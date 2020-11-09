0fc3380bce57c12a9453020ad305a1627fa00a57
Max Neuvians <max@neuvians.net>
2020-11-09 14:15:01 -0500

    feat: add RDS cluster definition

---
+++ aws/rds/rds.tf
@@ -0,0 +1,49 @@
+resource "random_string" "random" {
+  length  = 6
+  special = false
+  upper   = false
+}
+
+resource "aws_db_subnet_group" "notification-canada-ca" {
+  name       = "notification-canada-ca-${var.env}"
+  subnet_ids = var.vpc_private_subnets
+
+  tags = {
+    CostCenter = "notification-canada-ca-${var.env}"
+  }
+}
+
+resource "aws_rds_cluster_instance" "notification-canada-ca-server-instances" {
+  count                        = 3
+  identifier                   = "notification-canada-ca-${var.env}-instance-${count.index}"
+  cluster_identifier           = aws_rds_cluster.notification-canada-ca-server.id
+  instance_class               = var.rds_instance_type
+  db_subnet_group_name         = aws_db_subnet_group.notification-canada-ca.name
+  performance_insights_enabled = true
+
+  tags = {
+    CostCenter = "notification-canada-ca-${var.env}"
+  }
+}
+
+resource "aws_rds_cluster" "notification-canada-ca-server" {
+  cluster_identifier        = "notification-canada-ca-${var.env}-cluster"
+  engine                    = "aurora"
+  database_name             = "notification-canada-ca-${var.env}"
+  final_snapshot_identifier = "server-${random_string.random.result}"
+  master_username           = "postgres"
+  master_password           = var.rds_server_db_password
+  backup_retention_period   = 1
+  preferred_backup_window   = "07:00-09:00"
+  db_subnet_group_name      = aws_db_subnet_group.notification-canada-ca.name
+  storage_encrypted         = true
+
+
+  vpc_security_group_ids = [
+    var.eks_cluster_securitygroup
+  ]
+
+  tags = {
+    CostCenter = "notification-canada-ca-${var.env}"
+  }
+}
---
+++ aws/rds/variables.tf
@@ -0,0 +1,15 @@
+variable eks_cluster_securitygroup {
+  type = string
+}
+
+variable rds_instance_type {
+  type = string
+}
+
+variable rds_server_db_password {
+  type = string
+}
+
+variable vpc_private_subnets {
+  type = list
+}
