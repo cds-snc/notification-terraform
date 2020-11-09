207549293b1a05a2fc27da4d46f89af6eb1893f8
Max Neuvians <max@neuvians.net>
2020-11-09 11:38:15 -0500

    No tags on SES

--- aws/dns/ses.tf
+++ aws/dns/ses.tf
@@ -1,15 +1,7 @@
 resource "aws_ses_domain_identity" "notification-canada-ca" {
   domain = var.domain
-
-  tags = {
-    CostCenter = "notification-canada-ca-${var.env}"
-  }
 }
 
 resource "aws_ses_domain_dkim" "notification-canada-ca" {
   domain = var.domain
-
-  tags = {
-    CostCenter = "notification-canada-ca-${var.env}"
-  }
 }
