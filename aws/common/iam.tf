86bf955e8726b50d50cb128cfc9937d50f488826
Max Neuvians <max@neuvians.net>
2020-11-10 09:18:42 -0500

    feat: set up SNS channels with KMS encryption and no subscriptions (#22)
    * feat: set up SNS channels with KMS encryption and no subscriptions
    
    * Add SMS prefs and IAM role

---
+++ aws/common/iam.tf
@@ -0,0 +1,28 @@
+###
+# AWS SNS Cloudwatch IAM role
+###
+
+resource "aws_iam_role" "sns-delivery-role" {
+  name = "sns-delivery-role"
+
+  assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "logs:CreateLogGroup",
+        "logs:CreateLogStream",
+        "logs:PutLogEvents",
+        "logs:PutMetricFilter",
+        "logs:PutRetentionPolicy"
+      ],
+      "Resource": [
+        "*"
+      ]
+    }
+  ]
+}
+POLICY
+}
