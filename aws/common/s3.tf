8a8ee3f1ecc85d55e1935950f2b0c5a5b094b815
Antoine Augusti <antoine.augusti@cds-snc.ca>
2020-11-10 14:08:37 -0500

    Add alb_log_bucket

--- aws/common/s3.tf
+++ aws/common/s3.tf
@@ -148,3 +148,31 @@ resource "aws_s3_bucket_public_access_block" "document_bucket_logs" {
   ignore_public_acls      = true
   restrict_public_buckets = true
 }
+
+resource "aws_s3_bucket" "alb_log_bucket" {
+  bucket = "notification-canada-ca-${var.env}-alb-logs"
+  acl    = "private"
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+
+  logging {
+    target_bucket = aws_s3_bucket.document_bucket_logs.bucket
+  }
+
+  #tfsec:ignore:AWS002 - Ignore log of logs
+}
+
+resource "aws_s3_bucket_public_access_block" "alb_log_bucket" {
+  bucket = aws_s3_bucket.alb_log_bucket.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
