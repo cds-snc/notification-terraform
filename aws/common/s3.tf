45f0d0619ffcebc6ffc8de0fa33265eb7a0e330b
Antoine Augusti <antoine.augusti@cds-snc.ca>
2020-11-10 14:01:03 -0500

    Work on document bucket

--- aws/common/s3.tf
+++ aws/common/s3.tf
@@ -67,7 +67,7 @@ resource "aws_s3_bucket" "csv_bucket_logs" {
 }
 
 resource "aws_s3_bucket_public_access_block" "csv_bucket_logs" {
-  bucket = aws_s3_bucket.csv_bucket.id
+  bucket = aws_s3_bucket.csv_bucket_logs.id
 
   block_public_acls       = true
   block_public_policy     = true
@@ -86,4 +86,63 @@ resource "aws_s3_bucket" "asset_bucket" {
   #tfsec:ignore:AWS001 - Public read access
   #tfsec:ignore:AWS002 - No logging enabled
   #tfsec:ignore:AWS017 - Defines an unencrypted S3 bucket
+}
+
+resource "aws_s3_bucket" "document_bucket" {
+  bucket = "notification-canada-ca-${var.env}-document-download"
+  acl    = "private"
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+
+  logging {
+    target_bucket = aws_s3_bucket.document_bucket_logs.bucket
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "document_bucket" {
+  bucket = aws_s3_bucket.document_bucket.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket" "document_bucket_logs" {
+  bucket = "notification-canada-ca-${var.env}-document-download-logs"
+  acl    = "log-delivery-write"
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+
+  lifecycle_rule {
+    enabled = true
+
+    expiration {
+      days = 90
+    }
+  }
+
+  tags = {
+    CostCenter = "notification-canada-ca-${var.env}"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "document_bucket_logs" {
+  bucket = aws_s3_bucket.document_bucket_logs.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
