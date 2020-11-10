f55a1902d476a19ad621842e63e0224479eaee4b
Max Neuvians <max@neuvians.net>
2020-11-10 14:54:52 -0500

    feat: adds RDS module (#24)
    * feat: add RDS cluster definition
    
    * Add RDS password and instance sizing
    
    * Add CI fixes
    
    * Add fake password
    
    * Add number to fake password
    
    * Typechecking
    
    * Adjust backup retention period
    
    * Improve naming convention
    
    * Make postgres engine specific
    
    * Update aws/rds/rds.tf
    
    Co-authored-by: Antoine Augusti <antoine.augusti@cds-snc.ca>
    
    * Added preferred_maintenance_window
    
    Co-authored-by: Antoine Augusti <antoine.augusti@cds-snc.ca>

---
+++ aws/rds/rds.tf
@@ -0,0 +1,51 @@
+resource "random_string" "random" {
+  length  = 6
+  special = false
+  upper   = false
+}
+
+resource "aws_db_subnet_group" "notification-canada-ca" {
+  name       = "notification-canada-ca-${var.env}"
+  subnet_ids = var.vpc_private_subnets
+
+  tags = {
+    CostCenter = "notification-canada-ca-${var.env}"
+  }
+}
+
+resource "aws_rds_cluster_instance" "notification-canada-ca-instances" {
+  count                        = var.rds_instance_count
+  identifier                   = "notification-canada-ca-${var.env}-instance-${count.index}"
+  cluster_identifier           = aws_rds_cluster.notification-canada-ca.id
+  instance_class               = var.rds_instance_type
+  db_subnet_group_name         = aws_db_subnet_group.notification-canada-ca.name
+  performance_insights_enabled = true
+
+  tags = {
+    CostCenter = "notification-canada-ca-${var.env}"
+  }
+}
+
+resource "aws_rds_cluster" "notification-canada-ca" {
+  cluster_identifier           = "notification-canada-ca-${var.env}-cluster"
+  engine                       = "aurora-postgresql"
+  database_name                = "notification-canada-ca-${var.env}"
+  final_snapshot_identifier    = "server-${random_string.random.result}"
+  master_username              = "postgres"
+  master_password              = var.rds_cluster_password
+  backup_retention_period      = 8
+  preferred_backup_window      = "07:00-09:00"
+  preferred_maintenance_window = "wed:04:00-wed:04:30"
+  db_subnet_group_name         = aws_db_subnet_group.notification-canada-ca.name
+  storage_encrypted            = true
+  deletion_protection          = true
+
+
+  vpc_security_group_ids = [
+    var.eks_cluster_securitygroup
+  ]
+
+  tags = {
+    CostCenter = "notification-canada-ca-${var.env}"
+  }
+}
