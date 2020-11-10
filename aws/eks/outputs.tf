56cb82b3c68552569616ada857bcb7c6946bda2b
Max Neuvians <max@neuvians.net>
2020-11-10 11:15:07 -0500

    feat: add EKS SG as output for other modules (#25)

--- aws/eks/outputs.tf
+++ aws/eks/outputs.tf
@@ -5,3 +5,7 @@ output "api_target_group_arn" {
 output "admin_target_group_arn" {
   value = aws_alb_target_group.notification-canada-ca-admin.arn
 }
+
+output "eks-cluster-securitygroup" {
+  value = aws_eks_cluster.notification-canada-ca-eks-cluster.vpc_config[0].cluster_security_group_id
+}
