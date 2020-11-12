866487efa0c8ac286440d0156fad764ca26a118b
Antoine Augusti <antoine.augusti@cds-snc.ca>
2020-11-12 15:35:54 -0500

    Add sns_to_sqs_sms_callbacks

--- aws/common/lambda.tf
+++ aws/common/lambda.tf
@@ -14,3 +14,20 @@ resource "aws_lambda_function" "ses_to_sqs_email_callbacks" {
 
   runtime = "python3.8"
 }
+
+data "archive_file" "sns_to_sqs_sms_callbacks" {
+  type        = "zip"
+  source_file = "${path.module}/lambdas/sns_to_sqs_sms_callbacks.py"
+  output_path = "${path.module}/lambdas/sns_to_sqs_sms_callbacks.zip"
+}
+
+resource "aws_lambda_function" "sns_to_sqs_sms_callbacks" {
+  filename      = data.archive_file.sns_to_sqs_sms_callbacks.output_path
+  function_name = "sns-to-sqs-sms-callbacks"
+  role          = aws_iam_role.iam_lambda_to_sqs.arn
+  handler       = "sns_to_sqs_sms_callbacks.lambda_handler"
+
+  source_code_hash = data.archive_file.sns_to_sqs_sms_callbacks.output_base64sha256
+
+  runtime = "python3.8"
+}
