6f2fa461f5c2e9f9900e431fe6882fcd83651adc
Antoine Augusti <antoine.augusti@cds-snc.ca>
2020-11-12 11:42:21 -0500

    No dependency

--- aws/common/securityhub.tf
+++ aws/common/securityhub.tf
@@ -1,4 +1,3 @@
 resource "aws_securityhub_standards_subscription" "cis" {
-  depends_on    = [aws_securityhub_account.notification]
   standards_arn = "arn:aws:securityhub:::ruleset/cis-aws-foundations-benchmark/v/1.2.0"
 }
