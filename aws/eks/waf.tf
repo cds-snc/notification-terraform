960156c045d88cf24d264be35473a0ceaae5dae1
Antoine Augusti <antoine.augusti@cds-snc.ca>
2020-11-13 13:59:33 -0500

    Set up WAF

---
+++ aws/eks/waf.tf
@@ -0,0 +1,49 @@
+resource "aws_wafv2_web_acl" "notification-canada-ca" {
+  name        = "managed-rule-example"
+  scope       = "REGIONAL"
+
+  default_action {
+    allow {}
+  }
+
+  rule {
+    name     = "aws-managed-rules-common"
+    priority = 1
+
+    override_action {
+      count {}
+    }
+
+    statement {
+      # See https://docs.aws.amazon.com/waf/latest/developerguide/aws-managed-rule-groups-list.html
+      managed_rule_group_statement {
+        name        = "AWSManagedRulesCommonRuleSet"
+        vendor_name = "AWS"
+
+        excluded_rule {
+          name = "SizeRestrictions_QUERYSTRING"
+        }
+
+        excluded_rule {
+          name = "NoUserAgent_HEADER"
+        }
+      }
+    }
+
+    visibility_config {
+      cloudwatch_metrics_enabled = true
+      metric_name                = "wafv2-aws-managed-rules-common"
+      sampled_requests_enabled   = false
+    }
+  }
+
+  tags = {
+    CostCenter = "notification-canada-ca-${var.env}"
+  }
+
+  visibility_config {
+    cloudwatch_metrics_enabled = true
+    metric_name                = "wafv2"
+    sampled_requests_enabled   = false
+  }
+}
