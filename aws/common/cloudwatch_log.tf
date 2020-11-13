4f23ea1b93dda2e0a5655af394742f0eb0310cc6
Antoine Augusti <antoine.augusti@cds-snc.ca>
2020-11-13 10:37:12 -0500

    Create log groups for SNS deliveries (#38)
    * WIP
    
    * Fix
    
    * Reorganise
    
    * Add sns_to_sqs_sms_callbacks
    
    * Cloudwatch log to Lambda
    
    * Update tags
    
    * Tags for lambdas
    
    * Create log groups for SNS deliveries
    
    * Lambda

--- aws/common/cloudwatch_log.tf
+++ aws/common/cloudwatch_log.tf
@@ -1,13 +1,29 @@
+resource "aws_cloudwatch_log_group" "sns_deliveries" {
+  name = "sns/${var.region}/${var.account_id}/DirectPublishToPhoneNumber"
+
+  tags = {
+    CostCenter = "notification-canada-ca-${var.env}"
+  }
+}
+
+resource "aws_cloudwatch_log_group" "sns_deliveries_failures" {
+  name = "sns/${var.region}/${var.account_id}/DirectPublishToPhoneNumber/Failure"
+
+  tags = {
+    CostCenter = "notification-canada-ca-${var.env}"
+  }
+}
+
 resource "aws_cloudwatch_log_subscription_filter" "sns_to_lambda" {
   name            = "sns_to_lambda"
-  log_group_name  = "sns/${var.region}/${var.account_id}/DirectPublishToPhoneNumber"
+  log_group_name  = aws_cloudwatch_log_group.sns_deliveries.name
   filter_pattern  = ""
   destination_arn = aws_lambda_function.sns_to_sqs_sms_callbacks.arn
 }
 
 resource "aws_cloudwatch_log_subscription_filter" "sns_failures_to_lambda" {
   name            = "sns_failures_to_lambda"
-  log_group_name  = "sns/${var.region}/${var.account_id}/DirectPublishToPhoneNumber/Failure"
+  log_group_name  = aws_cloudwatch_log_group.sns_deliveries_failures.name
   filter_pattern  = ""
   destination_arn = aws_lambda_function.sns_to_sqs_sms_callbacks.arn
 }
