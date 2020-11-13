e65b0033b0dd94613632ef551657986a4035168c
Antoine Augusti <antoine.augusti@cds-snc.ca>
2020-11-13 11:46:23 -0500

    Lambda permission for SNS

--- aws/common/sns.tf
+++ aws/common/sns.tf
@@ -35,4 +35,6 @@ resource "aws_sns_topic_subscription" "ses_sns_to_lambda" {
   topic_arn = aws_sns_topic.notification-canada-ca-ses-callback.arn
   protocol  = "lambda"
   endpoint  = aws_lambda_function.ses_to_sqs_email_callbacks.arn
+
+  depends_on = [aws_lambda_permission.allow_sns]
 }
