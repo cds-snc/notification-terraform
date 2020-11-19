5cd1d468ac05e0b7f600ae89f3d95c07a8e6d572
Antoine Augusti <antoine.augusti@cds-snc.ca>
2020-11-19 09:24:54 -0500

    feat: create EKS container insights log group

--- aws/eks/cloudwatch_log.tf
+++ aws/eks/cloudwatch_log.tf
@@ -1,6 +1,10 @@
 ###
 # AWS EKS Cloudwatch groups
 ###
+resource "aws_cloudwatch_log_group" "eks_container_insights" {
+  name              = "/aws/containerinsights/${aws_eks_cluster.notification-canada-ca-eks-cluster.name}/application"
+  retention_in_days = 0
+}
 
 resource "aws_cloudwatch_log_group" "notification-canada-ca-eks-cluster-logs" {
   name              = "/aws/eks/notification-canada-ca-${var.env}/cluster"
@@ -13,7 +17,7 @@ resource "aws_cloudwatch_log_group" "notification-canada-ca-eks-cluster-logs" {
 resource "aws_cloudwatch_log_metric_filter" "web-500-errors" {
   name           = "web-500-errors"
   pattern        = "\"\\\" 500 \""
-  log_group_name = "/aws/containerinsights/${aws_eks_cluster.notification-canada-ca-eks-cluster.name}/application"
+  log_group_name = aws_cloudwatch_log_group.eks_container_insights.name
 
   metric_transformation {
     name      = "500-errors"
@@ -25,7 +29,7 @@ resource "aws_cloudwatch_log_metric_filter" "web-500-errors" {
 resource "aws_cloudwatch_log_metric_filter" "celery-error" {
   name           = "celery-error"
   pattern        = "\"ERROR/Worker\""
-  log_group_name = "/aws/containerinsights/${aws_eks_cluster.notification-canada-ca-eks-cluster.name}/application"
+  log_group_name = aws_cloudwatch_log_group.eks_container_insights.name
 
   metric_transformation {
     name      = "celery-error"
