e5f1b4214da594121e4b00619d27b46079c2830f
Antoine Augusti <antoine.augusti@cds-snc.ca>
2020-11-23 13:05:09 -0500

    feat: create bucket and role for Health Canada (#79)

--- aws/common/s3.tf
+++ aws/common/s3.tf
@@ -66,6 +66,70 @@ resource "aws_s3_bucket" "csv_bucket_logs" {
   #tfsec:ignore:AWS002 - Ignore log of logs
 }
 
+resource "aws_s3_bucket" "bulk_send" {
+  bucket = "notification-canada-ca-${var.env}-bulk-send"
+  acl    = "private"
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+
+  lifecycle_rule {
+    id      = "expire"
+    enabled = true
+
+    expiration {
+      days = 7
+    }
+  }
+
+  logging {
+    target_bucket = aws_s3_bucket.bulk_send_logs.bucket
+  }
+
+  tags = {
+    CostCenter = "notification-canada-ca-${var.env}"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "bulk_send" {
+  bucket = aws_s3_bucket.bulk_send.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket" "bulk_send_logs" {
+  bucket = "notification-canada-ca-${var.env}-bulk-send-logs"
+  acl    = "log-delivery-write"
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+
+  lifecycle_rule {
+    enabled = true
+
+    expiration {
+      days = 90
+    }
+  }
+
+  tags = {
+    CostCenter = "notification-canada-ca-${var.env}"
+  }
+
+  #tfsec:ignore:AWS002 - Ignore log of logs
+}
+
 resource "aws_s3_bucket_public_access_block" "csv_bucket_logs" {
   bucket = aws_s3_bucket.csv_bucket_logs.id
 
