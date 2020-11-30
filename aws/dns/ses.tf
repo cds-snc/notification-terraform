039e0b952f38d989810235d954653ae037ad5d86
Antoine Augusti <antoine.augusti@cds-snc.ca>
2020-11-30 08:47:56 -0500

    feat: set up additional sending domain for SES

--- aws/dns/ses.tf
+++ aws/dns/ses.tf
@@ -31,3 +31,42 @@ resource "aws_ses_domain_mail_from" "notification-canada-ca" {
   domain           = aws_ses_domain_identity.notification-canada-ca.domain
   mail_from_domain = "bounce.${aws_ses_domain_identity.notification-canada-ca.domain}"
 }
+
+
+###
+# Additional sending domains
+###
+
+resource "aws_ses_domain_identity" "cic-trvapply-vrtdemande" {
+  count  = var.env == "production" ? 1 : 0
+  domain = "trvapply-vrtdemande.apps.cic.gc.ca"
+}
+
+resource "aws_ses_domain_dkim" "cic-trvapply-vrtdemande" {
+  count  = var.env == "production" ? 1 : 0
+  domain = "trvapply-vrtdemande.apps.cic.gc.ca"
+}
+
+resource "aws_ses_identity_notification_topic" "cic-trvapply-vrtdemande-bounce-topic" {
+  count                    = var.env == "production" ? 1 : 0
+  topic_arn                = var.notification_canada_ca_ses_callback_arn
+  notification_type        = "Bounce"
+  identity                 = aws_ses_domain_identity.cic-trvapply-vrtdemande.domain
+  include_original_headers = false
+}
+
+resource "aws_ses_identity_notification_topic" "cic-trvapply-vrtdemande-delivery-topic" {
+  count                    = var.env == "production" ? 1 : 0
+  topic_arn                = var.notification_canada_ca_ses_callback_arn
+  notification_type        = "Delivery"
+  identity                 = aws_ses_domain_identity.cic-trvapply-vrtdemande.domain
+  include_original_headers = false
+}
+
+resource "aws_ses_identity_notification_topic" "cic-trvapply-vrtdemande-complaint-topic" {
+  count                    = var.env == "production" ? 1 : 0
+  topic_arn                = var.notification_canada_ca_ses_callback_arn
+  notification_type        = "Complaint"
+  identity                 = aws_ses_domain_identity.notification-canada-ca.domain
+  include_original_headers = false
+}
