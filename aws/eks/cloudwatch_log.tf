9be2da51eb926cfa9ddecc029415fb1ca07d4a4d
Jimmy Royer <jimleroyer@gmail.com>
2020-12-09 13:42:45 -0500

    feat: track number of services going over the rate limit (#101)
    * feat: track number of services going over the rate limit
    
    * fix: changed name from review

--- aws/eks/cloudwatch_log.tf
+++ aws/eks/cloudwatch_log.tf
@@ -33,3 +33,15 @@ resource "aws_cloudwatch_log_metric_filter" "celery-error" {
     value     = "1"
   }
 }
+
+resource "aws_cloudwatch_log_metric_filter" "over-daily-rate-limit" {
+  name           = "over-daily-rate-limit"
+  pattern        = "has been rate limited for daily use sent"
+  log_group_name = "/aws/containerinsights/${aws_eks_cluster.notification-canada-ca-eks-cluster.name}/application"
+
+  metric_transformation {
+    name      = "over-daily-rate-limit"
+    namespace = "LogMetrics"
+    value     = "1"
+  }
+}
