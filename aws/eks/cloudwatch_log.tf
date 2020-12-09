2c59c893228b88fdb2011e231339b8ba0607fa26
Jimmy Royer <jimleroyer@gmail.com>
2020-12-09 13:41:16 -0500

    Update aws/eks/cloudwatch_log.tf
    Co-authored-by: Antoine Augusti <antoine.augusti@cds-snc.ca>

--- aws/eks/cloudwatch_log.tf
+++ aws/eks/cloudwatch_log.tf
@@ -40,7 +40,7 @@ resource "aws_cloudwatch_log_metric_filter" "over-daily-rate-limit" {
   log_group_name = "/aws/containerinsights/${aws_eks_cluster.notification-canada-ca-eks-cluster.name}/application"
 
   metric_transformation {
-    name      = "over-rate-limit"
+    name      = "over-daily-rate-limit"
     namespace = "LogMetrics"
     value     = "1"
   }
