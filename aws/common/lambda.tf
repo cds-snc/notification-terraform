eee7eed741c3971445bec9efdb4a014e33194374
Antoine Augusti <antoine.augusti@cds-snc.ca>
2021-01-11 08:40:42 -0500

    fix: specific source ARNs for Lambda permissions

--- aws/common/lambda.tf
+++ aws/common/lambda.tf
@@ -40,27 +40,58 @@ resource "aws_lambda_function" "sns_to_sqs_sms_callbacks" {
   }
 }
 
-resource "aws_lambda_permission" "allow_cloudwatch_events" {
-  statement_id  = "AllowExecutionFromCloudWatch"
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.sns_to_sqs_sms_callbacks.function_name
-  principal     = "events.amazonaws.com"
-}
-
-resource "aws_lambda_permission" "allow_cloudwatch_logs" {
-  statement_id  = "AllowExecutionFromCloudWatchLogs"
+##
+# CloudWatch log groups for SNS deliveries in ca-central-1
+##
+resource "aws_lambda_permission" "allow_cloudwatch_logs_sns_successes" {
+  statement_id  = "allow_cloudwatch_logs_sns_successes"
   action        = "lambda:InvokeFunction"
   function_name = aws_lambda_function.sns_to_sqs_sms_callbacks.function_name
   principal     = "logs.amazonaws.com"
+  source_arn    = aws_cloudwatch_log_group.sns_deliveries.arn
 }
 
-resource "aws_lambda_permission" "allow_sns" {
-  statement_id  = "AllowExecutionFromSNS"
+resource "aws_lambda_permission" "allow_cloudwatch_logs_sns_failures" {
+  statement_id  = "allow_cloudwatch_logs_sns_failures"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.sns_to_sqs_sms_callbacks.function_name
+  principal     = "logs.amazonaws.com"
+  source_arn    = aws_cloudwatch_log_group.sns_deliveries_failures.arn
+}
+
+##
+# CloudWatch log groups for SNS deliveries in us-west-2
+##
+resource "aws_lambda_permission" "allow_cloudwatch_logs_sns_successes_us_west_2" {
+  statement_id  = "allow_cloudwatch_logs_sns_successes_us_west_2"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.sns_to_sqs_sms_callbacks.function_name
+  principal     = "logs.amazonaws.com"
+  source_arn    = aws_cloudwatch_log_group.sns_deliveries_us_west_2.arn
+}
+
+resource "aws_lambda_permission" "allow_cloudwatch_logs_sns_failures_us_west_2" {
+  statement_id  = "allow_cloudwatch_logs_sns_failures_us_west_2"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.sns_to_sqs_sms_callbacks.function_name
+  principal     = "logs.amazonaws.com"
+  source_arn    = aws_cloudwatch_log_group.sns_deliveries_failures_us_west_2.arn
+}
+
+##
+# SNS topic for SES deliveries
+##
+resource "aws_lambda_permission" "allow_sns_ses_callbacks" {
+  statement_id  = "allow_sns_ses_callbacks"
   action        = "lambda:InvokeFunction"
   function_name = aws_lambda_function.ses_to_sqs_email_callbacks.function_name
   principal     = "sns.amazonaws.com"
+  source_arn    = aws_sns_topic.notification-canada-ca-ses-callback.arn
 }
 
+##
+# SNS topics for CloudWatch alarms in us-west-2
+##
 resource "aws_lambda_permission" "sns_warning_us_west_2_to_slack_lambda" {
   statement_id  = "AllowExecutionFromSNSWarningUSWest2"
   action        = "lambda:InvokeFunction"
