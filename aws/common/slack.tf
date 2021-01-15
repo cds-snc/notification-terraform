39d7edd5c28f77de169d5193ec033fd133cc62b7
Dorian Pruski <dorian.pruski@cds-snc.ca>
2021-01-15 14:43:50 -0700

    requested changes to fix vars

--- aws/common/slack.tf
+++ aws/common/slack.tf
@@ -34,20 +34,3 @@ module "notify_slack_critical" {
 
   depends_on = [aws_sns_topic.notification-canada-ca-alert-critical]
 }
-
-module "notify_slack_general" {
-  source  = "terraform-aws-modules/notify-slack/aws"
-  version = "~> 4.0"
-
-  create_sns_topic = false
-  sns_topic_name   = aws_sns_topic.notification-canada-ca-alert-general.name
-
-  slack_webhook_url = var.cloudwatch_slack_webhook_general_topic
-  slack_channel     = var.slack_channel_general_topic
-  slack_username    = "[GENERAL] AWS Cloudwatch"
-  slack_emoji       = ":loudspeaker:"
-
-  lambda_function_name                   = "notify-slack-general"
-  cloudwatch_log_group_retention_in_days = 90
-
-}
