44e09dd8fa3ac32d89bbd3d286204047c851a5ff
Dorian Pruski <dorian.pruski@cds-snc.ca>
2021-01-14 23:46:48 -0700

    requested changes to fix vars

--- aws/common/slack.tf
+++ aws/common/slack.tf
@@ -50,4 +50,5 @@ module "notify_slack_general" {
   lambda_function_name                   = "notify-slack-general"
   cloudwatch_log_group_retention_in_days = 90
 
+  depends_on = [aws_sns_topic.notification-canada-ca-alert-general]
 }
