4253c59eaedc13f03748297a762f8b57e4dbdb59
Jimmy Royer <jimleroyer@gmail.com>
2021-01-18 18:57:01 -0500

    fix: adding server side encryption config to Athena S3 bucket

--- aws/common/athena.tf
+++ aws/common/athena.tf
@@ -4,6 +4,14 @@
 
 resource "aws_s3_bucket" "athena" {
   bucket = "athena"
+  acl    = "private"
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
 
   lifecycle_rule {
     id      = "expire"
