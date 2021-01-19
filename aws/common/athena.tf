a1bacf1c1775b1b8340c1885cf8a198e74f99398
Jimmy Royer <jimleroyer@gmail.com>
2021-01-19 13:35:05 -0500

    fix: Added primary workgroup configuration to have Athena working right in environments

--- aws/common/athena.tf
+++ aws/common/athena.tf
@@ -10,3 +10,24 @@ resource "aws_athena_database" "notification_athena" {
     encryption_option = "SSE_S3"
   }
 }
+
+resource "aws_athena_workgroup" "primary" {
+  name       = "primary"
+
+  configuration {
+    enforce_workgroup_configuration    = true
+    publish_cloudwatch_metrics_enabled = true
+
+    result_configuration {
+      output_location = "s3://${aws_s3_bucket.athena_bucket.bucket}"
+
+      encryption_configuration {
+        encryption_option = "SSE_S3"
+      }
+    }
+  }
+
+  tags = {
+    CostCenter = "notification-canada-ca-${var.env}"
+  }
+}
