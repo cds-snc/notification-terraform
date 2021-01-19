682540743debf69b8a91d1783a00621feace5867
Jimmy Royer <jimleroyer@gmail.com>
2021-01-18 19:26:18 -0500

    fix: Adding a log bucket for a ELB log bucket..

--- aws/common/athena.tf
+++ aws/common/athena.tf
@@ -2,8 +2,8 @@
 # Athena deployment configuration for the Notification application
 ###
 
-resource "aws_s3_bucket" "athena" {
-  bucket = "athena"
+resource "aws_s3_bucket" "athena_bucket" {
+  bucket = "notification-canada-ca-${var.env}-athena"
   acl    = "private"
   server_side_encryption_configuration {
     rule {
@@ -22,9 +22,48 @@ resource "aws_s3_bucket" "athena" {
     }
   }
 
+  logging {
+    target_bucket = aws_s3_bucket.athena_bucket_logs.bucket
+  }
+
   tags = {
     CostCenter = "notification-canada-ca-${var.env}"
   }
+}
+
+resource "aws_s3_bucket_public_access_block" "athena_bucket" {
+  bucket = aws_s3_bucket.athena_bucket.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket" "athena_bucket_logs" {
+  bucket = "notification-canada-ca-${var.env}-athena-logs"
+  acl    = "log-delivery-write"
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+
+  lifecycle_rule {
+    enabled = true
+
+    expiration {
+      days = 90
+    }
+  }
+
+  tags = {
+    CostCenter = "notification-canada-ca-${var.env}"
+  }
+
+  #tfsec:ignore:AWS002 - Ignore log of logs
 }
 
 resource "aws_athena_workgroup" "notification_athena_workgroup" {
@@ -35,7 +74,7 @@ resource "aws_athena_workgroup" "notification_athena_workgroup" {
     publish_cloudwatch_metrics_enabled = true
 
     result_configuration {
-      output_location = "s3://${aws_s3_bucket.athena.bucket}/output/"
+      output_location = "s3://${aws_s3_bucket.athena_bucket.bucket}/output/"
 
       encryption_configuration {
         encryption_option = "SSE_S3"
@@ -50,5 +89,5 @@ resource "aws_athena_workgroup" "notification_athena_workgroup" {
 
 resource "aws_athena_database" "notification_athena" {
   name   = "notification_athena"
-  bucket = aws_s3_bucket.athena.bucket
+  bucket = aws_s3_bucket.athena_bucket.bucket
 }
