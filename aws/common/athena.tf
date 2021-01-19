0b119b101d535c84a6f4767db858b77ff3a65be9
Jimmy Royer <jimmy.royer@cds-snc.ca>
2021-01-19 11:40:10 -0500

    Enabled Athena with a dedicated S3 bucket (#156)
    * feat: Enabled Athena with a dedicated S3 bucket
    
    * fix: adding server side encryption config to Athena S3 bucket
    
    * fix: Adding a log bucket for a ELB log bucket..
    
    * fix: moves s3 config into the s3 config file & removed workgroup config
    
    * Update aws/common/athena.tf
    
    Co-authored-by: Antoine Augusti <antoine.augusti@cds-snc.ca>
    
    Co-authored-by: Antoine Augusti <antoine.augusti@cds-snc.ca>

---
+++ aws/common/athena.tf
@@ -0,0 +1,12 @@
+###
+# Athena deployment configuration for the Notification application
+###
+
+resource "aws_athena_database" "notification_athena" {
+  name   = "notification_athena"
+  bucket = aws_s3_bucket.athena_bucket.bucket
+
+  encryption_configuration {
+    encryption_option = "SSE_S3"
+  }
+}
--- aws/common/s3.tf
+++ aws/common/s3.tf
@@ -366,3 +366,67 @@ resource "aws_s3_bucket_policy" "alb_log_bucket_allow_elb_account" {
 }
 POLICY
 }
+
+resource "aws_s3_bucket" "athena_bucket" {
+  bucket = "notification-canada-ca-${var.env}-athena"
+  acl    = "private"
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+
+  lifecycle_rule {
+    id      = "expire"
+    enabled = true
+
+    expiration {
+      days = 7
+    }
+  }
+
+  logging {
+    target_bucket = aws_s3_bucket.athena_bucket_logs.bucket
+  }
+
+  tags = {
+    CostCenter = "notification-canada-ca-${var.env}"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "athena_bucket" {
+  bucket = aws_s3_bucket.athena_bucket.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket" "athena_bucket_logs" {
+  bucket = "notification-canada-ca-${var.env}-athena-logs"
+  acl    = "log-delivery-write"
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+
+  lifecycle_rule {
+    enabled = true
+
+    expiration {
+      days = 90
+    }
+  }
+
+  tags = {
+    CostCenter = "notification-canada-ca-${var.env}"
+  }
+
+  #tfsec:ignore:AWS002 - Ignore log of logs
+}
