8902d22314f9f0ecf95f683903e3f182c083a6a0
Jimmy Royer <jimmy.royer@cds-snc.ca>
2021-01-20 11:26:37 -0500

    Update aws/common/athena.tf
    Co-authored-by: Antoine Augusti <antoine.augusti@cds-snc.ca>

--- aws/common/athena.tf
+++ aws/common/athena.tf
@@ -33,7 +33,7 @@ resource "aws_athena_workgroup" "primary" {
 }
 
 resource "aws_athena_named_query" "create_table_alb_log" {
-  name      = "alb_log_create_table"
+  name      = "create_table_alb_logs"
   workgroup = aws_athena_workgroup.primary.name
   database  = aws_athena_database.notification_athena.name
   query = templatefile("${path.module}/sql/alb_log_create_table.sql.tmpl",
