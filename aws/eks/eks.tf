af1f7e5226c4a5a82d29a219bdcc776a9de168b9
Antoine Augusti <antoine.augusti@cds-snc.ca>
2021-02-24 10:10:50 -0500

    Remove blank line

--- aws/eks/eks.tf
+++ aws/eks/eks.tf
@@ -21,7 +21,6 @@ resource "aws_eks_cluster" "notification-canada-ca-eks-cluster" {
   # tfsec:ignore:AWS066 EKS should have the encryption of secrets enabled
   # Will be tackled in the future https://github.com/cds-snc/notification-terraform/issues/202
 
-
   # Ensure that IAM Role permissions are created before and deleted after EKS Cluster handling.
   # Otherwise, EKS will not be able to properly delete EKS managed EC2 infrastructure such as Security Groups.
   depends_on = [
