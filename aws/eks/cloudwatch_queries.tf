8f912f92d3a8e54718a45d25bfe7f3fa7f00cc5a
Antoine Augusti <antoine.augusti@cds-snc.ca>
2021-04-06 09:41:28 -0400

    Update names

--- aws/eks/cloudwatch_queries.tf
+++ aws/eks/cloudwatch_queries.tf
@@ -1,11 +1,11 @@
 resource "aws_cloudwatch_query_definition" "admin-api-500-errors" {
   name = "ADMIN & API - 500 errors"
 
-  log_groups = [
+  log_group_names = [
     local.eks_application_log_group
   ]
 
-  query = <<EOF
+  query_string = <<EOF
 fields @timestamp, log, kubernetes.labels.app as app, kubernetes.pod_name as pod_name, @logStream
 | filter kubernetes.labels.app like /admin|api/
 | filter strcontains(@message, 'HTTP/1.1\" 500')
