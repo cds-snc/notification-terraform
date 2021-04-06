2561cf0e00dfb468cc3b0cc41e196a5dbe9f4c3d
Antoine Augusti <antoine.augusti@cds-snc.ca>
2021-04-06 09:47:06 -0400

    Add new queries

--- aws/eks/cloudwatch_queries.tf
+++ aws/eks/cloudwatch_queries.tf
@@ -1,15 +1,46 @@
-resource "aws_cloudwatch_query_definition" "admin-api-500-errors" {
-  name = "ADMIN & API - 500 errors"
+resource "aws_cloudwatch_query_definition" "admin-api-50X-errors" {
+  name = "ADMIN & API - 50X errors"
 
   log_group_names = [
     local.eks_application_log_group
   ]
 
-  query_string = <<EOF
+  query_string = <<QUERY
 fields @timestamp, log, kubernetes.labels.app as app, kubernetes.pod_name as pod_name, @logStream
 | filter kubernetes.labels.app like /admin|api/
-| filter strcontains(@message, 'HTTP/1.1\" 500')
+| filter @message like /HTTP\/\d+\.\d+\\" 50\d/
 | sort @timestamp desc
 | limit 20
-EOF
+QUERY
+}
+
+resource "aws_cloudwatch_query_definition" "celery-errors" {
+  name = "Celery errors"
+
+  log_group_names = [
+    local.eks_application_log_group
+  ]
+
+  query_string = <<QUERY
+fields @timestamp, log, kubernetes.labels.app as app, kubernetes.pod_name as pod_name, @logStream
+| filter kubernetes.labels.app like /^celery/
+| filter strcontains(@message, 'ERROR/Worker')
+| sort @timestamp desc
+| limit 20
+QUERY
+}
+
+resource "aws_cloudwatch_query_definition" "services-over-daily-rate-limit" {
+  name = "Services going over daily rate limits"
+
+  log_group_names = [
+    local.eks_application_log_group
+  ]
+
+  query_string = <<QUERY
+fields @timestamp, log
+| filter strcontains(@message, 'has been rate limited for daily use sent')
+| sort @timestamp desc
+| limit 20
+QUERY
 }
