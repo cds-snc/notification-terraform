a8eb8807ad3f976e71373af52567b7558cd178ab
Antoine Augusti <antoine.augusti@cds-snc.ca>
2021-04-21 14:46:56 -0400

    Update to ELBSecurityPolicy-2016-08

--- aws/eks/alb.tf
+++ aws/eks/alb.tf
@@ -32,7 +32,7 @@ resource "aws_alb_listener" "notification-canada-ca" {
   certificate_arn   = var.aws_acm_notification_canada_ca_arn
   # See https://docs.aws.amazon.com/elasticloadbalancing/latest/network/create-tls-listener.html#describe-ssl-policies
   # And https://cyber.gc.ca/en/guidance/guidance-securely-configuring-network-protocols-itsp40062
-  ssl_policy        = "ELBSecurityPolicy-FS-1-1-2019-08"
+  ssl_policy        = "ELBSecurityPolicy-2016-08"
 
   default_action {
     type             = "forward"
