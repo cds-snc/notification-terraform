7bf969cdc36d4d3585847907860eee0c449568a0
Jimmy Royer <jimleroyer@gmail.com>
2021-05-18 18:53:28 -0400

    Fixed region in dns_query_log_config configuration

--- aws/common/route53.tf
+++ aws/common/route53.tf
@@ -3,7 +3,7 @@
 ###
 
 resource "aws_route53_resolver_query_log_config" "dns_query_log_config" {
-  name            = "us-west-2/${var.account_id}/dns_query_log_config"
+  name            = "us-east-1/${var.account_id}/dns_query_log_config"
   destination_arn = [aws_cloudwatch_log_group.route53_resolver_query_log.arn]
 
   tags = {
