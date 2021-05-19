08033585396ade5ee6f8f2f98c755341ea5aa208
Jimmy Royer <jimleroyer@gmail.com>
2021-05-19 11:43:06 -0400

    fix: Removed illegal characters in the dns query log config name

--- aws/common/route53.tf
+++ aws/common/route53.tf
@@ -3,7 +3,7 @@
 ###
 
 resource "aws_route53_resolver_query_log_config" "dns_query_log_config" {
-  name            = "${var.region}/${var.account_id}/dns_query_log_config"
+  name            = "${var.region}_${var.account_id}_dns_query_log_config"
   destination_arn = aws_cloudwatch_log_group.route53_resolver_query_log.arn
 
   tags = {
