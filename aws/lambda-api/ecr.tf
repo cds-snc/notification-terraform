44d635b604233a0532642030441f3fe4d91401b6
Stephen McMurtry <smcmurtry@gmail.com>
2021-08-19 15:18:24 -0400

    ufixed ignore comment

--- aws/lambda-api/ecr.tf
+++ aws/lambda-api/ecr.tf
@@ -1,5 +1,6 @@
 resource "aws_ecr_repository" "api" {
-  # checkov:skip=CKV_AWS_51:The :latest tag is used in Staging
+  # The :latest tag is used in Staging
+  #tfsec:ignore:AWS078
 
   name                 = "${var.product_name}/api"
   image_tag_mutability = "MUTABLE"
