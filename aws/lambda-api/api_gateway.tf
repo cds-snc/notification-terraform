25679f781ba5997043370b9b42d17d3e3bbbd71e
Stephen McMurtry <smcmurtry@gmail.com>
2021-08-19 15:40:14 -0400

    added custom API gateway domain

--- aws/lambda-api/api_gateway.tf
+++ aws/lambda-api/api_gateway.tf
@@ -72,3 +72,19 @@ resource "aws_api_gateway_stage" "api" {
   rest_api_id   = aws_api_gateway_rest_api.api.id
   stage_name    = "v1"
 }
+
+resource "aws_api_gateway_domain_name" "api" {
+  regional_certificate_arn = data.aws_acm_certificate.notification-canada-ca.arn
+  domain_name              = "api-lambda.${var.notify_email_domain}"
+  security_policy          = "TLS_1_2"
+
+  endpoint_configuration {
+    types = ["REGIONAL"]
+  }
+}
+
+resource "aws_api_gateway_base_path_mapping" "api" {
+  api_id      = aws_api_gateway_rest_api.api.id
+  stage_name  = aws_api_gateway_stage.api.stage_name
+  domain_name = aws_api_gateway_domain_name.api.domain_name
+}
