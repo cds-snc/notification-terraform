72b5a3741e7b3be13b2023988736f6fcbae79790
Stephen McMurtry <smcmurtry@gmail.com>
2021-08-19 14:59:48 -0400

    moved files into new lambda-api folder to keep everything self contained

---
+++ aws/lambda-api/variables.tf
@@ -0,0 +1,80 @@
+variable "admin_client_secret" {
+  type = string
+  sensitive = true
+}
+
+variable "admin_client_user_name" {
+  type = string
+  sensitive = true
+}
+
+
+variable "asset_domain" {
+  type = string
+}
+
+variable "asset_upload_bucket_name" {
+  type = string
+}
+
+variable "auth_tokens" {
+  type = string
+  sensitive = true
+}
+
+// check if this already exists
+variable "aws_pinpoint_region" {
+  type = string
+}
+
+// check if this already exists
+variable "base_domain" {
+  type = string
+}
+
+variable "csv_upload_bucket_name" {
+  type = string
+  sensitive = true
+}
+
+variable "dangerous_salt" {
+  type = string
+  sensitive = true
+}
+
+variable "documents_bucket" {
+  type = string
+}
+
+variable "mlwr_host" {
+  type = string
+}
+
+variable "notification_queue_prefix" {
+  type = string
+}
+
+variable "redis_enabled" {
+  type = string
+}
+
+variable "redis_url" {
+  type = string
+}
+
+variable "secret_key" {
+  type = string
+  sensitive = true
+}
+
+variable "sqlalchemy_database_reader_uri" {
+  type = string
+}
+
+variable "sqlalchemy_database_uri" {
+  type = string
+}
+
+variable "sqlalchemy_pool_size" {
+  type = string
+}
