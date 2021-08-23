ee2f82dc4baf5359bfcc993d9ffc5644b2ca485a
Stephen McMurtry <smcmurtry@gmail.com>
2021-08-23 16:59:50 -0400

    added iam file, hard code notify as product name

--- aws/lambda-api/ecr.tf
+++ aws/lambda-api/ecr.tf
@@ -2,7 +2,7 @@ resource "aws_ecr_repository" "api" {
   # The :latest tag is used in Staging
   #tfsec:ignore:AWS078
 
-  name                 = "${var.product_name}/api"
+  name                 = "notify/api"
   image_tag_mutability = "MUTABLE"
 
   image_scanning_configuration {
---
+++ aws/lambda-api/iam.tf
@@ -0,0 +1,80 @@
+data "aws_iam_policy_document" "service_principal" {
+  statement {
+    effect = "Allow"
+
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role" "api" {
+  name               = "notify-api"
+  assume_role_policy = data.aws_iam_policy_document.service_principal.json
+}
+
+data "aws_iam_policy" "lambda_insights" {
+  name = "CloudWatchLambdaInsightsExecutionRolePolicy"
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_insights" {
+  role       = aws_iam_role.api.name
+  policy_arn = data.aws_iam_policy.lambda_insights.arn
+}
+
+data "aws_iam_policy_document" "api_policies" {
+
+  statement {
+
+    effect = "Allow"
+
+    actions = [
+      "logs:CreateLogGroup",
+      "logs:CreateLogStream",
+      "logs:PutLogEvents"
+    ]
+
+    resources = [
+      "*"
+    ]
+  }
+
+  statement {
+
+    effect = "Allow"
+
+    actions = [
+      "ecr:GetDownloadUrlForlayer",
+      "ecr:BatchGetImage"
+    ]
+    resources = [
+      aws_ecr_repository.api.arn
+    ]
+  }
+
+}
+
+resource "aws_iam_policy" "api" {
+  name   = "notify-api"
+  path   = "/"
+  policy = data.aws_iam_policy_document.api_policies.json
+}
+
+resource "aws_iam_role_policy_attachment" "api" {
+  role       = aws_iam_role.api.name
+  policy_arn = aws_iam_policy.api.arn
+}
+
+# Use AWS managed IAM policy
+####
+# Provides minimum permissions for a Lambda function to execute while
+# accessing a resource within a VPC - create, describe, delete network
+# interfaces and write permissions to CloudWatch Logs.
+####
+resource "aws_iam_role_policy_attachment" "AWSLambdaVPCAccessExecutionRole" {
+  role       = aws_iam_role.api.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaVPCAccessExecutionRole"
+}
