2a4f35133ec3eb411cb3c5095da78b115ab5d04a
Stephen McMurtry <smcmurtry@gmail.com>
2021-08-26 14:10:46 -0400

    hooked up root GET method to lambda function

--- aws/lambda-api/api_gateway.tf
+++ aws/lambda-api/api_gateway.tf
@@ -33,6 +33,20 @@ resource "aws_api_gateway_method" "root_method" {
   }
 }
 
+resource "aws_api_gateway_integration" "root_integration" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  resource_id = aws_api_gateway_rest_api.api.root_resource_id
+  http_method = aws_api_gateway_method.root_method.http_method
+
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.api.invoke_arn
+
+  request_parameters = {
+    "integration.request.path.proxy" = "method.request.path.proxy"
+  }
+}
+
 resource "aws_api_gateway_method_response" "api_response_200" {
   rest_api_id = aws_api_gateway_rest_api.api.id
   resource_id = aws_api_gateway_resource.resource.id
