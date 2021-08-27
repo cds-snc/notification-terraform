ce1424450b3f4c4be1a21c68d7cde9612154788c
Stephen McMurtry <smcmurtry@gmail.com>
2021-08-27 12:31:13 -0400

    updated names of env variables, changed ecr repo name to avoid conflict with existing infra

--- aws/lambda-api/ecr.tf
+++ aws/lambda-api/ecr.tf
@@ -2,7 +2,7 @@ resource "aws_ecr_repository" "api" {
   # The :latest tag is used in Staging
   #tfsec:ignore:AWS078
 
-  name                 = "notify/api"
+  name                 = "notify/api-${var.env}"
   image_tag_mutability = "MUTABLE"
 
   image_scanning_configuration {
