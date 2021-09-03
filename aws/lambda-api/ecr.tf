eb5a657ed0bad0718ef9094532700bdca7a7c18f
Stephen Astels <sastels@gmail.com>
2021-09-03 15:26:53 -0400

    maybe fix failing test

--- aws/lambda-api/ecr.tf
+++ aws/lambda-api/ecr.tf
@@ -3,7 +3,7 @@ resource "aws_ecr_repository" "api" {
   #tfsec:ignore:AWS078
 
   name                 = "notify/api-${var.env}"
-  image_tag_mutability = "MUTABLE"
+  image_tag_mutability = "IMMUTABLE"
 
   image_scanning_configuration {
     scan_on_push = true
