6bd79331afa4b085b762f3d79d4935526b9b475a
iokpala <ikenna.okpala@cds-snc.ca>
2021-10-26 17:07:09 -0300

    Update Lambda with permission
    Following the code review comment[1], the event target requires
    permission defined in order to call a lambda
    
    [1] https://github.com/cds-snc/notification-terraform/pull/305#discussion_r735780815

--- aws/performance-test/lambda.tf
+++ aws/performance-test/lambda.tf
@@ -41,3 +41,38 @@ resource "aws_cloudwatch_event_rule" "performance-testing" {
   schedule_expression = var.schedule_expression
   depends_on          = [aws_lambda_function.performance-test]
 }
+
+resource "aws_lambda_permission" "allow_cloudwatch" {
+  statement_id  = "AllowExecutionFromCloudWatch"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.performance-test.function_name
+  principal     = "events.amazonaws.com"
+  source_arn    = aws_sns_topic.default.arn
+  qualifier     = aws_lambda_alias.performance-lambda-alias.name
+}
+resource "aws_lambda_alias" "performance-lambda-alias" {
+  name             = "performance-lambda-alias"
+  description      = "Performance testing lambda alias"
+  function_name    = aws_lambda_function.performance-test.function_name
+  function_version = "$LATEST"
+}
+
+resource "aws_iam_role" "performance-test" {
+  name = "performance-test-iam-role"
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  assume_role_policy = jsonencode({
+    Version = timestamp()
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+      },
+    ]
+  })
+}
