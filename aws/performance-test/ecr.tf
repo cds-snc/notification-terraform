c2986692fa06448027a549c6ba77471bdcc9a53e
iokpala <ikenna.okpala@cds-snc.ca>
2021-10-27 12:10:31 -0300

    Ignore image tag mutability
    This updates the with and ignore tfsec

--- aws/performance-test/ecr.tf
+++ aws/performance-test/ecr.tf
@@ -3,7 +3,7 @@ resource "aws_ecr_repository" "performance-test" {
   #tfsec:ignore:AWS078
 
   name                 = "notify/performance-test"
-  image_tag_mutability = "MUTABLE"
+  image_tag_mutability = "MUTABLE" #tfsec:ignore:AWS078
 
   image_scanning_configuration {
     scan_on_push = true
