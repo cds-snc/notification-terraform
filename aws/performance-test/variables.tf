e066fe20f0e8c808d1f1da68a4152d0c0d98c8fd
iokpala <ikenna.okpala@cds-snc.ca>
2021-11-10 16:24:18 -0400

    FIXUP: Switch narrative of variable deescription
    This updates the variable description to start with
    "Identifies" rather than "This helps identify"

--- aws/performance-test/variables.tf
+++ aws/performance-test/variables.tf
@@ -6,55 +6,55 @@ variable "schedule_expression" {
 variable "load_test_phone_number" {
   type        = string
   default     = "16132532222"
-  description = "This helps identify delivery phone number."
+  description = "Identifies delivery phone number."
 }
 variable "load_test_email" {
   type        = string
   default     = "success@simulator.amazonses.com"
-  description = "This helps identify delivery email address."
+  description = "Identifies delivery email address."
 }
 variable "load_test_aws_s3_bucket" {
   type        = string
   default     = "notify-performance-test-results-staging"
-  description = "This helps identify aws s3 bucket."
+  description = "Identifies aws s3 bucket."
 }
 variable "load_test_csv_directory_path" {
   type        = string
   default     = "/tmp/notify_performance_test"
-  description = "This helps identify the csv directory."
+  description = "Identifies the csv directory."
 }
 variable "load_test_domain" {
   type        = string
   default     = "https://api.staging.notification.cdssandbox.xyz"
-  description = "This helps identify performance test domain."
+  description = "Identifies performance test domain."
 }
 variable "load_test_sms_template_id" {
   type        = string
   default     = "83d01f06-a818-4134-bd69-ce90a2949280"
-  description = "This helps identify sms template id."
+  description = "Identifies sms template id."
 }
 variable "load_test_bulk_email_template_id" {
   type        = string
   default     = "5ebee3b7-63c0-4052-a8cb-387b818df627"
-  description = "This helps identify bulk email template id."
+  description = "Identifies bulk email template id."
 }
 variable "load_test_email_template_id" {
   type        = string
   default     = "a59b313d-8de2-4973-ac2f-66de7ec0b239"
-  description = "This helps identify email template id."
+  description = "Identifies email template id."
 }
 variable "load_test_email_with_attachment_template_id" {
   type        = string
   default     = "a59b313d-8de2-4973-ac2f-66de7ec0b239"
-  description = "This helps identify email with attachment template id."
+  description = "Identifies email with attachment template id."
 }
 variable "load_test_email_with_link_template_id" {
   type        = string
   default     = "5ebee3b7-63c0-4052-a8cb-387b818df627"
-  description = "This helps identify email with link template id."
+  description = "Identifies email with link template id."
 }
 variable "test_auth_header" {
   type        = string
   default     = "apikey-v1 c55039fc-c0e1-44db-a14e-b6a669148ec6"
-  description = "This helps identify api auth header."
+  description = "Identifies api auth header."
 }
