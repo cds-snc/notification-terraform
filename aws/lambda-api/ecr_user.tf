f3efd987f5e09672f0fc344ed35e300f943797cf
Stephen Astels <sastels@gmail.com>
2021-11-10 13:12:40 -0500

    add update-lambda permission

--- aws/lambda-api/ecr_user.tf
+++ aws/lambda-api/ecr_user.tf
@@ -52,4 +52,14 @@ data "aws_iam_policy_document" "ecr" {
     ]
     resources = [aws_ecr_repository.api-lambda.arn]
   }
+  statement {
+    sid       = "PermissionsToUpdateFunction"
+      effect  = "Allow"
+      actions = [
+        "lambda:UpdateFunctionCode"
+      ]
+    resources = [
+      aws_lambda_function.api.arn
+    ]
+  }
 }
