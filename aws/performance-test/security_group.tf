b4176b3b4e396a8d3b4953538b5edd007e3a3de5
Stephen Astels <sastels@gmail.com>
2021-11-25 11:52:59 -0500

    restrict egress

--- aws/performance-test/security_group.tf
+++ aws/performance-test/security_group.tf
@@ -3,8 +3,8 @@ resource "aws_security_group" "perf_test" {
   description = "Performance Test Security Group"
 
   egress {
-    from_port   = 0
-    to_port     = 0
+    from_port   = 443
+    to_port     = 443
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
