a7a3df5d778c31d342c05f0eaf15139f4302a1cb
Steve Astels <sastels@users.noreply.github.com>
2021-11-29 14:16:12 -0500

    fix security group protocol (#327)

--- aws/performance-test/security_group.tf
+++ aws/performance-test/security_group.tf
@@ -6,7 +6,7 @@ resource "aws_security_group" "perf_test" {
   egress {
     from_port = 443
     to_port   = 443
-    protocol  = "-1"
+    protocol  = "tcp"
     # We need to connect to the staging api server (not managed in terraform)
     # tfsec:ignore:AWS009
     cidr_blocks = ["0.0.0.0/0"]
