d7b8be4e9872832665441af808a22630e6ec4c5f
Steve Astels <sastels@users.noreply.github.com>
2021-11-30 15:10:38 -0500

    move iam rule to container role (#332)

--- aws/performance-test/container_execution_role.tf
+++ aws/performance-test/container_execution_role.tf
@@ -14,6 +14,11 @@ resource "aws_iam_role_policy_attachment" "ce_cs" {
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceforEC2Role"
 }
 
+resource "aws_iam_role_policy_attachment" "perf_test_secretsmanager" {
+  role       = aws_iam_role.container_execution_role.name
+  policy_arn = aws_iam_policy.perf_test_secretsmanager.arn
+}
+
 ###
 # Policy Documents
 ###
@@ -33,5 +38,26 @@ data "aws_iam_policy_document" "container_execution_role" {
       type        = "Service"
       identifiers = ["ecs-tasks.amazonaws.com"]
     }
+  }
+}
+
+resource "aws_iam_policy" "perf_test_secretsmanager" {
+  name   = "PerfTestEcsTaskGetSecretValue"
+  path   = "/"
+  policy = data.aws_iam_policy_document.perf_test_secretsmanager.json
+}
+
+data "aws_iam_policy_document" "perf_test_secretsmanager" {
+  statement {
+    effect = "Allow"
+    actions = [
+      "secretsmanager:GetSecretValue",
+    ]
+    resources = [
+      aws_secretsmanager_secret_version.perf_test_phone_number.arn,
+      aws_secretsmanager_secret_version.perf_test_email.arn,
+      aws_secretsmanager_secret_version.perf_test_domain.arn,
+      aws_secretsmanager_secret_version.perf_test_auth_header.arn
+    ]
   }
 }
