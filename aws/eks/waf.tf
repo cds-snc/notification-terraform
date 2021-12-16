d433021b7950ba9f509bfb53565909e478c882ff
Pat Heard <patrick.heard@cds-snc.ca>
2021-12-16 17:03:14 -0500

    feat: add WAF Bot control managed rules (#366)
    Add AWS's Bot control managed rules to the EKS WAF ACL.  These will
    be initially set to `count` to determine impact, and then switched over
    to `block`.
    
    Co-authored-by: Jimmy Royer <jimleroyer@gmail.com>

--- aws/eks/waf.tf
+++ aws/eks/waf.tf
@@ -119,6 +119,28 @@ resource "aws_wafv2_web_acl" "notification-canada-ca" {
   }
 
   rule {
+    name     = "AWSManagedRulesBotControlRuleSet"
+    priority = 6
+
+    override_action {
+      count {}
+    }
+
+    statement {
+      managed_rule_group_statement {
+        name        = "AWSManagedRulesBotControlRuleSet"
+        vendor_name = "AWS"
+      }
+    }
+
+    visibility_config {
+      cloudwatch_metrics_enabled = true
+      metric_name                = "AWSManagedRulesBotControlRuleSet"
+      sampled_requests_enabled   = true
+    }
+  }
+
+  rule {
     name     = "document_download_invalid_path"
     priority = 10
 
