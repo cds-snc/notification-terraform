7a6325909496aedaac6062ef019798097e3b4374
Jimmy Royer <jimleroyer@gmail.com>
2021-12-16 13:58:43 -0500

    WAF rules for valid endpoints of notification-document-download-api (#341)
    * Added regex rules to allow valid doc-api endpoints
    
    * Trying out proper escape character for regex
    
    * Format terraform waf file
    
    * Allow api and admin to go through the new WAF rules
    
    * Align host match conditions with the matching HTTP header.
    
    * Moved variable definition for NEW_RELIC_APP_NAME
    
    * Let's try this out
    
    * Reworked WAF rules against scan attacked to split into 2 rules.

--- aws/eks/waf.tf
+++ aws/eks/waf.tf
@@ -118,6 +118,108 @@ resource "aws_wafv2_web_acl" "notification-canada-ca" {
     }
   }
 
+  rule {
+    name     = "document_download_invalid_path"
+    priority = 10
+
+    action {
+      block {
+        custom_response {
+          response_code = 204
+        }
+      }
+    }
+
+    statement {
+      and_statement {
+        statement {
+          byte_match_statement {
+            positional_constraint = "STARTS_WITH"
+            field_to_match {
+              single_header {
+                name = "Host"
+              }
+            }
+            search_string = "api.document"
+            text_transformation {
+              priority = 1
+              type     = "COMPRESS_WHITE_SPACE"
+            }
+            text_transformation {
+              priority = 2
+              type     = "LOWERCASE"
+            }
+          }
+        }
+
+        statement {
+          not_statement {
+            statement {
+              regex_pattern_set_reference_statement {
+                arn = aws_wafv2_regex_pattern_set.re_document_download.arn
+                field_to_match {
+                  uri_path {}
+                }
+                text_transformation {
+                  priority = 1
+                  type     = "COMPRESS_WHITE_SPACE"
+                }
+                text_transformation {
+                  priority = 2
+                  type     = "LOWERCASE"
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+
+    visibility_config {
+      cloudwatch_metrics_enabled = true
+      metric_name                = "document_download_api_invalid_path"
+      sampled_requests_enabled   = true
+    }
+  }
+
+  rule {
+    name     = "api_invalid_path"
+    priority = 11
+
+    action {
+      block {
+        custom_response {
+          response_code = 204
+        }
+      }
+    }
+
+    statement {
+      regex_pattern_set_reference_statement {
+        arn = aws_wafv2_regex_pattern_set.re_no_api_nor_domain.arn
+        field_to_match {
+          single_header {
+            name = "Host"
+          }
+        }
+        text_transformation {
+          priority = 1
+          type     = "COMPRESS_WHITE_SPACE"
+        }
+        text_transformation {
+          priority = 2
+          type     = "LOWERCASE"
+        }
+      }
+    }
+
+    visibility_config {
+      cloudwatch_metrics_enabled = true
+      metric_name                = "api_invalid_path"
+      sampled_requests_enabled   = true
+    }
+  }
+
   tags = {
     CostCenter = "notification-canada-ca-${var.env}"
   }
@@ -126,5 +228,58 @@ resource "aws_wafv2_web_acl" "notification-canada-ca" {
     cloudwatch_metrics_enabled = true
     metric_name                = "wafv2"
     sampled_requests_enabled   = false
+  }
+}
+
+resource "aws_wafv2_regex_pattern_set" "re_document_download" {
+  name        = "re_document_download"
+  description = "Regex matching valid document download endpoints"
+  scope       = "REGIONAL"
+
+  # WAF Regex blocks are combined with OR logic. 
+  # Regex support is limited, please see: 
+  # https://docs.aws.amazon.com/waf/latest/developerguide/waf-regex-pattern-set-managing.html
+
+  # GET /_status
+  regular_expression {
+    regex_string = "/_status"
+  }
+
+  # GET /services/<uuid:service_id>/documents/<uuid:document_id>
+  regular_expression {
+    regex_string = "/services/[\\w]{8}-[\\w]{4}-[\\w]{4}-[\\w]{4}-[\\w]{12}/documents/[\\w]{8}-[\\w]{4}-[\\w]{4}-[\\w]{4}-[\\w]{12}"
+  }
+
+  # POST /services/<uuid:service_id>/documents
+  regular_expression {
+    regex_string = "/services/[\\w]{8}-[\\w]{4}-[\\w]{4}-[\\w]{4}-[\\w]{12}/documents"
+  }
+
+  tags = {
+    CostCenter = "notification-canada-ca-${var.env}"
+  }
+}
+
+resource "aws_wafv2_regex_pattern_set" "re_no_api_nor_domain" {
+  name        = "re_no_api"
+  description = "Regex discarding invalid API endpoints"
+  scope       = "REGIONAL"
+
+  # WAF Regex blocks are combined with OR logic. 
+  # Regex support is limited, please see: 
+  # https://docs.aws.amazon.com/waf/latest/developerguide/waf-regex-pattern-set-managing.html
+
+  # Don't start with 'api' domain.
+  regular_expression {
+    regex_string = "^(?!api).*$"
+  }
+
+  # Don't start with a supported domain.
+  regular_expression {
+    regex_string = "^(?!${var.domain}).*$"
+  }
+
+  tags = {
+    CostCenter = "notification-canada-ca-${var.env}"
   }
 }
