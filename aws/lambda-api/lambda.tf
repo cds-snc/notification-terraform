897dc80ea643d52140033877dbd7f86757fa0ae6
Pat Heard <patrick.heard@cds-snc.ca>
2022-02-23 09:05:01 -0500

    fix: FF_NOTIFICATION_CELERY_PERSISTENCE to "True" (#392)
    With the API change in cds-snc/notification-api#1474, the feature flag
    value needs to be updated to a recognized True boolean value.

--- aws/lambda-api/lambda.tf
+++ aws/lambda-api/lambda.tf
@@ -50,7 +50,7 @@ resource "aws_lambda_function" "api" {
       SQLALCHEMY_DATABASE_URI               = var.sqlalchemy_database_uri
       SQLALCHEMY_POOL_SIZE                  = var.sqlalchemy_pool_size
       DOCUMENT_DOWNLOAD_API_HOST            = var.document_download_api_host
-      FF_NOTIFICATION_CELERY_PERSISTENCE    = "1"
+      FF_NOTIFICATION_CELERY_PERSISTENCE    = "True"
     }
   }
 
