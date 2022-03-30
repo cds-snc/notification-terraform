d3226c3fbd833361a19a689289b2e58ac336efb9
Mohamed Nur <mohamed.nur@cds-snc.ca>
2022-03-30 15:15:53 -0400

    feat: give lambda access to retrieve new relic license key secret

--- aws/lambda-api/secrets_manager.tf
+++ aws/lambda-api/secrets_manager.tf
@@ -6,3 +6,12 @@ resource "aws_secretsmanager_secret_version" "ecr-user-access-key" {
   secret_id     = aws_secretsmanager_secret.ecr-user-access-key.id
   secret_string = aws_iam_access_key.ecr-user.secret
 }
+
+# Created outside terraform when New Relic was integrated
+data "aws_secretsmanager_secret" "new-relic-license-key-secret" {
+  name = "NEW_RELIC_LICENSE_KEY"
+}
+
+data "aws_secretsmanager_secret_version" "new-relic-license-key" {
+  secret_id = data.aws_secretsmanager_secret.new-relic-license-key.id
+}
