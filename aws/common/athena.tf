1da3678f5612c583064bd10b7b7a1cf62ec8dd98
Pat Heard <patrick.heard@cds-snc.ca>
2022-04-06 14:52:32 +0000

    feat: add Athena CREATE TABLE for WAF logs

--- aws/common/athena.tf
+++ aws/common/athena.tf
@@ -43,3 +43,15 @@ resource "aws_athena_named_query" "create_table_alb_logs" {
       bucket_location = "s3://${var.cbs_satellite_bucket_name}/lb_logs/AWSLogs/${var.account_id}/elasticloadbalancing/${var.region}/"
   })
 }
+
+resource "aws_athena_named_query" "create_table_waf_logs" {
+  name      = "create_table_waf_logs"
+  workgroup = aws_athena_workgroup.primary.name
+  database  = aws_athena_database.notification_athena.name
+  query = templatefile("${path.module}/sql/waf_log_create_table.sql.tmpl",
+    {
+      database_name   = aws_athena_database.notification_athena.name
+      table_name      = "waf_logs"
+      bucket_location = "s3://${var.cbs_satellite_bucket_name}/waf_acl_logs/AWSLogs/${var.account_id}/lb/${var.region}/"
+  })
+}
