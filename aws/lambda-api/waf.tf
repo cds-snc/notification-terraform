01d0d704f59551a51add6e2ab2db78b750a00a17
Pat Heard <patrick.heard@cds-snc.ca>
2022-04-06 15:38:49 +0000

    chore: trigger re-create of WAF ACL logging config
    During the apply of #438, the WAF ACL logging config was removed
    and not recreated.  This will cause the logging config for Firehose
    to be recreated.
    
    The reason this happened is because there was a misconfiguration
    where two logging destinations had been set for a WAF ACL and
    only one is supported.

--- aws/lambda-api/waf.tf
+++ aws/lambda-api/waf.tf
@@ -182,6 +182,7 @@ resource "aws_kinesis_firehose_delivery_stream" "firehose-api-lambda-waf-logs" {
 resource "aws_wafv2_web_acl_logging_configuration" "firehose-api-lambda-waf-logs" {
   log_destination_configs = [aws_kinesis_firehose_delivery_stream.firehose-api-lambda-waf-logs.arn]
   resource_arn            = aws_wafv2_web_acl.api_lambda.arn
+
   redacted_fields {
     single_header {
       name = "authorization"
