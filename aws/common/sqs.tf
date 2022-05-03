d2c606ee4f9a9273b2eea6cff478cd38bfd3dab6
Stephen Astels <sastels@gmail.com>
2022-05-03 16:03:41 -0400

    turn on server side encryption

--- aws/common/sqs.tf
+++ aws/common/sqs.tf
@@ -1,7 +1,8 @@
 
 resource "aws_sqs_queue" "priority_db_tasks_queue" {
-  name                  = "${var.celery_queue_prefix}${var.sqs_priority_db_tasks_queue_name}"
-  fifo_queue            = true
-  deduplication_scope   = "messageGroup"
-  fifo_throughput_limit = "perMessageGroupId"
+  name                    = "${var.celery_queue_prefix}${var.sqs_priority_db_tasks_queue_name}"
+  fifo_queue              = true
+  deduplication_scope     = "messageGroup"
+  fifo_throughput_limit   = "perMessageGroupId"
+  sqs_managed_sse_enabled = true
 }
