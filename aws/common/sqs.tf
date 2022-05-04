576879b4dd5bf645e473f712c6dcf13573142d86
Stephen Astels <sastels@gmail.com>
2022-05-04 15:11:06 -0400

    add a hyphen between prefix and queue name

--- aws/common/sqs.tf
+++ aws/common/sqs.tf
@@ -1,5 +1,5 @@
 resource "aws_sqs_queue" "priority_db_tasks_queue" {
-  name                    = "${var.celery_queue_prefix}${var.sqs_priority_db_tasks_queue_name}"
+  name                    = "${var.celery_queue_prefix}-${var.sqs_priority_db_tasks_queue_name}"
   fifo_queue              = true
   deduplication_scope     = "messageGroup"
   fifo_throughput_limit   = "perMessageGroupId"
@@ -9,14 +9,14 @@ resource "aws_sqs_queue" "priority_db_tasks_queue" {
 }
 
 resource "aws_sqs_queue" "normal_db_tasks_queue" {
-  name                    = "${var.celery_queue_prefix}${var.sqs_normal_db_tasks_queue_name}"
+  name                    = "${var.celery_queue_prefix}-${var.sqs_normal_db_tasks_queue_name}"
   sqs_managed_sse_enabled = true
   # tfsec:ignore:AWS015 - Queues should be encrypted with customer managed KMS keys
   # AWS managed encryption is good enough for us
 }
 
 resource "aws_sqs_queue" "bulk_db_tasks_queue" {
-  name                    = "${var.celery_queue_prefix}${var.sqs_bulk_db_tasks_queue_name}"
+  name                    = "${var.celery_queue_prefix}-${var.sqs_bulk_db_tasks_queue_name}"
   sqs_managed_sse_enabled = true
   # tfsec:ignore:AWS015 - Queues should be encrypted with customer managed KMS keys
   # AWS managed encryption is good enough for us
