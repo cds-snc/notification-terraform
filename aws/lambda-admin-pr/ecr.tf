66f59335311d42e202df11fb815a534f626ffd4f
Pat Heard <patrick.heard@cds-snc.ca>
2022-05-19 13:40:55 +0000

    chore: tfsec suppression comment

--- aws/lambda-admin-pr/ecr.tf
+++ aws/lambda-admin-pr/ecr.tf
@@ -1,8 +1,6 @@
 resource "aws_ecr_repository" "notify_admin" {
-  #tfsec:ignore:AWS078
-
   name                 = "notify/admin"
-  image_tag_mutability = "MUTABLE"
+  image_tag_mutability = "MUTABLE" #tfsec:ignore:AWS078
 
   image_scanning_configuration {
     scan_on_push = true
