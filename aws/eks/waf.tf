0dbb7f292e7723bd9dccc329dd183dd32bc51b16
Jimmy Royer <jimleroyer@gmail.com>
2022-08-03 18:09:55 -0400

    Block anon ip ruleset by default

--- aws/eks/waf.tf
+++ aws/eks/waf.tf
@@ -101,13 +101,21 @@ resource "aws_wafv2_web_acl" "notification-canada-ca" {
     priority = 5
 
     override_action {
-      count {}
+      none {}
     }
 
     statement {
       managed_rule_group_statement {
         name        = "AWSManagedRulesAnonymousIpList"
         vendor_name = "AWS"
+      }
+
+      excluded_rule {
+        name = "AnonymousIPList"
+      }
+
+      excluded_rule {
+        name = "HostingProviderIPList"
       }
     }
 
