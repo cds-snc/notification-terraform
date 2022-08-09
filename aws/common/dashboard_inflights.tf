e9d0ce62539959b4948f3e64e467ff26a0c0728b
Andrew Leith <andrew.leith@cds-snc.ca>
2022-08-09 15:27:27 +0000

    Add text description widget to redis batch savings dashboard

--- aws/common/dashboard_inflights.tf
+++ aws/common/dashboard_inflights.tf
@@ -3,7 +3,7 @@ resource "aws_cloudwatch_dashboard" "redis_batch_saving" {
   dashboard_body = <<EOF
 {
     "widgets": [
-          {
+        {
             "height": 6,
             "width": 9,
             "y": 0,
@@ -161,6 +161,16 @@ resource "aws_cloudwatch_dashboard" "redis_batch_saving" {
                 "stat": "Sum",
                 "period": 300,
                 "title": "Bulk sms inflights"
+            }
+        },
+        {
+            "height": 13,
+            "width": 6,
+            "y": 0,
+            "x": 18,
+            "type": "text",
+            "properties": {
+                "markdown": "# Description\n- Overview of notifications entering the system. This dashboard is meant to show the internal of the redis queues.\n- This dashboard shows us notifications which have not yet been saved to the database\n- NOTE: only notifications sent using the API are included here (`/sms` or `/email`).  \n- NOTE: Admin requests and posting to `/bulk` are not here.\n\n# Definitions\n- Inbox: a redis list that is the first entry of notifications into the system\n- Inflight: a subset of the inbox list that is about to be added to the database\n   - created: a group of messages taken out of the inbox and put together\n   - acknowledged: inflight notification has been added to our database and scheduled to be processed\n   - expired:  something went wrong and the inflight was never acknowledged (notification goes back to inbox)\n\n# Additional information\n- [Priority lanes adr](https://github.com/cds-snc/notification-adr/blob/main/records/2022-03-08.priority-lanes.md)\n\n# Todo\n- keeping track of oldest message in a particular queue (when it was added the first time vs. bouncing back and forth from inbox/inflights)"
             }
         }
     ]
