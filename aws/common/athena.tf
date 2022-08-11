0485edc22d60179e14b8222c822058e2a1a5c01f
Stephen Astels <sastels@gmail.com>
2022-08-11 12:14:20 -0400

    add another query for lb and lambda together

--- aws/common/athena.tf
+++ aws/common/athena.tf
@@ -45,7 +45,7 @@ resource "aws_athena_named_query" "create_table_alb_logs" {
 }
 
 resource "aws_athena_named_query" "create_table_waf_logs" {
-  name      = "create_table_waf_logs_lb"
+  name      = "WAF: create table waf_logs_lb"
   workgroup = aws_athena_workgroup.primary.name
   database  = aws_athena_database.notification_athena.name
   query = templatefile("${path.module}/sql/waf_log_create_table.sql.tmpl",
@@ -57,7 +57,7 @@ resource "aws_athena_named_query" "create_table_waf_logs" {
 }
 
 resource "aws_athena_named_query" "create_table_waf_logs_api_lambda" {
-  name      = "create_table_waf_logs_api_lambda"
+  name      = "WAF: create table waf_logs_api_lambda"
   workgroup = aws_athena_workgroup.primary.name
   database  = aws_athena_database.notification_athena.name
   query = templatefile("${path.module}/sql/waf_log_create_table.sql.tmpl",
@@ -65,5 +65,17 @@ resource "aws_athena_named_query" "create_table_waf_logs_api_lambda" {
       database_name   = aws_athena_database.notification_athena.name
       table_name      = "waf_logs_api_lambda"
       bucket_location = "s3://${var.cbs_satellite_bucket_name}/waf_acl_logs/AWSLogs/${var.account_id}/lambda/"
+  })
+}
+
+resource "aws_athena_named_query" "create_table_waf_logs_api_lambda" {
+  name      = "WAF: create table waf_logs"
+  workgroup = aws_athena_workgroup.primary.name
+  database  = aws_athena_database.notification_athena.name
+  query = templatefile("${path.module}/sql/waf_log_create_table.sql.tmpl",
+    {
+      database_name   = aws_athena_database.notification_athena.name
+      table_name      = "waf_logs"
+      bucket_location = "s3://${var.cbs_satellite_bucket_name}/waf_acl_logs/AWSLogs/${var.account_id}/"
   })
 }
