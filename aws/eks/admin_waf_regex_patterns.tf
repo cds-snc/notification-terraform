a47981cca31b230aadeb39af1cc251e1eb935c1f
Stephen Astels <sastels@gmail.com>
2022-08-12 16:02:18 -0400

    Five regex then

--- aws/eks/admin_waf_regex_patterns.tf
+++ aws/eks/admin_waf_regex_patterns.tf
@@ -9,7 +9,25 @@ resource "aws_wafv2_regex_pattern_set" "re_admin" {
 
 
   regular_expression {
-    regex_string = "/.well-known|/_email|/_letter|/_status|/_styleguide|/a11y|/accounts|/accounts-or-dashboard|/activity|/add-service|/agreement|/callbacks|/contact|/design-patterns-content-guidance|/documentation|/email|/email-branding|/email-not-received|/error|/features|/find-services-by-name|/find-users-by-email|/forced-password-reset|/forgot-password|/format|/inbound-sms-admin|/invitation|/letter-branding|/letters|/messages-status|/new-password|/organisation-invitation|/organisations|/personalise|/platform-admin|/preview|/pricing|/provider|/providers|/register|/register-from-invite|/register-from-org-invite|/registration-continue|/resend-email-verification|/roadmap|/robots.txt|/send-new-code|/send-new-email-token|/services|/services-or-dashboard|/set-lang|/sign-in|/sign-out|/sms|/static|/support|/templates|/text-not-received|/two-factor-email-sent|/two-factor-sms-sent|/user-profile|/users|/verify|/verify-email|/verify-mobile|/welcome|/why-notify"
+    regex_string = "/.well-known|/_email|/_letter|/_status|/_styleguide|/a11y|/accounts|/accounts-or-dashboard|/activity|/add-service|/agreement|/callbacks|/contact|/design-patterns-content-guidance|/documentation|/email"
+  }
+
+  regular_expression {
+    regex_string = "/email-branding|/email-not-received|/error|/features|/find-services-by-name|/find-users-by-email|/forced-password-reset|/forgot-password|/format|/inbound-sms-admin|/invitation|/letter-branding"
+  }
+
+  regular_expression {
+    regex_string = "/letters|/messages-status|/new-password|/organisation-invitation|/organisations|/personalise|/platform-admin|/preview|/pricing|/provider|/providers|/register|/register-from-invite"
+
+  }
+
+  regular_expression {
+    regex_string = "/register-from-org-invite|/registration-continue|/resend-email-verification|/roadmap|/robots.txt|/send-new-code|/send-new-email-token|/services|/services-or-dashboard|/set-lang|/sign-in|/sign-out|/sms"
+
+  }
+
+  regular_expression {
+    regex_string = "/static|/support|/templates|/text-not-received|/two-factor-email-sent|/two-factor-sms-sent|/user-profile|/users|/verify|/verify-email|/verify-mobile|/welcome|/why-notify"
   }
 
   tags = {
