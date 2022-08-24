669c0bcd055834e7fe37c87cd3c1775d1f4e4c1a
Jumana Bahrainwala <jzbahrai@uwaterloo.ca>
2022-08-24 16:05:59 -0400

    api regex

---
+++ aws/eks/api_waf_regex_patterns.tf
@@ -0,0 +1,13 @@
+resource "aws_wafv2_regex_pattern_set" "re_api" {
+  name        = "re_api"
+  description = "Regex matching valid api endpoints"
+  scope       = "REGIONAL"
+
+  regular_expression {
+    regex_string = "_status|/api-key|/complaint|/email-branding|/events|/inbound-number|/invite|/letter-branding|/letters|/notifications|/organisation|/organisations|/platform-stats|/provider-details|/service|/static|/user|/v2"
+  }
+
+  tags = {
+    CostCenter = "notification-canada-ca-${var.env}"
+  }
+}
