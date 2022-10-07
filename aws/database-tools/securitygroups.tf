5095cb5baf644d8c10831dfe4b6761c71d2eb1ac
Jumana B <jzbahrai@users.noreply.github.com>
2022-10-07 16:23:48 -0400

    Initial blazer commit (#546)
    * Initial blazer commit
    
    * add plan to workflow
    
    * Database tools
    
    * Add bill tag key and value
    
    * use secret from paramter store
    
    * add workflow to merge to staging
    
    * try
    
    * try 2
    
    * try3
    
    * try4
    
    * try5
    
    * fix
    
    * try again
    
    * fixes

---
+++ aws/database-tools/securitygroups.tf
@@ -0,0 +1,42 @@
+data "aws_subnet" "private_subnet" {
+  for_each = toset(var.vpc_private_subnets)
+  id       = each.value
+}
+
+resource "aws_security_group" "blazer" {
+  name        = "blazer"
+  description = "Allow inbound traffic to internal service"
+  vpc_id      = var.vpc_id
+
+  egress {
+    description = "Access to internet"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Access to internal service"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    self        = true
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  dynamic "egress" {
+    for_each = [for s in data.aws_subnet.private_subnet : {
+      cidr = s.cidr_block
+      zone = s.availability_zone
+    }]
+
+    content {
+      protocol    = "tcp"
+      from_port   = 5432
+      to_port     = 5432
+      cidr_blocks = [egress.value.cidr]
+      description = "Traffic to ECS cluster"
+    }
+  }
+}
