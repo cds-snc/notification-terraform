1358187458fff79a7d0e1c89f9ee2416db731446
Jumana B <jzbahrai@users.noreply.github.com>
2022-10-21 11:56:04 -0400

    Initial blazer commit (#564)
    * Initial blazer commit
    
    * Reduce size of description as tf doesnt allow more than 250 characters
    
    * fix ssm paramter + role permission

---
+++ aws/database-tools/task_role.tf
@@ -0,0 +1,62 @@
+resource "aws_iam_role" "blazer_ecs_task" {
+  name               = "${aws_ecs_cluster.blazer.name}-ecs-task"
+  assume_role_policy = data.aws_iam_policy_document.ecs_task_assume.json
+  tags = {
+    (var.billing_tag_key) = var.billing_tag_value
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "blazer_ecs_task_policy_attach" {
+  role       = aws_iam_role.blazer_ecs_task.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+data "aws_iam_policy_document" "ecs_task_assume" {
+  statement {
+    actions = ["sts:AssumeRole"]
+    principals {
+      type        = "Service"
+      identifiers = ["ecs-tasks.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_policy" "blazer_task_role_policy" {
+  name   = "blazer_task_role_policy"
+  path   = "/"
+  policy = data.aws_iam_policy_document.blazer_task_role_actions.json
+}
+
+resource "aws_iam_role_policy_attachment" "blazer_task_role_actions" {
+  role       = aws_iam_role.blazer_ecs_task.name
+  policy_arn = aws_iam_policy.blazer_task_role_policy.arn
+}
+
+data "aws_iam_policy_document" "blazer_task_role_actions" {
+  statement {
+
+    effect = "Allow"
+
+    actions = [
+      "ssmmessages:CreateControlChannel",
+      "ssmmessages:CreateDataChannel",
+      "ssmmessages:OpenControlChannel",
+      "ssmmessages:OpenDataChannel"
+    ]
+    resources = ["*"]
+  }
+
+  statement {
+
+    effect = "Allow"
+
+    actions = [
+      "ssm:DescribeParameters",
+      "ssm:GetParameters",
+    ]
+    resources = [
+      aws_ssm_parameter.sqlalchemy_database_reader_uri.arn,
+      aws_ssm_parameter.db_tools_environment_variables.arn
+    ]
+  }
+}
