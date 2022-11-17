0f0fea6985b69e96559202dcc88159df3a50e0b0
Jumana Bahrainwala <jzbahrai@uwaterloo.ca>
2022-11-17 15:09:00 -0500

    Lambda for google cidr

---
+++ aws/lambda-google-cidr/lambda.tf
@@ -0,0 +1,40 @@
+module "lambda-google-cidr" {
+  source                 = "github.com/cds-snc/terraform-modules?ref=v0.0.45//lambda"
+  name                   = "google-cidr"
+  billing_tag_value      = var.billing_tag_value
+  ecr_arn                = aws_ecr_repository.google-cidr.arn
+  enable_lambda_insights = true
+  image_uri              = "${aws_ecr_repository.google-cidr.repository_url}:latest"
+  timeout                = 60
+  memory                 = 1024
+
+  environment_variables = {
+    PREFIX_LIST_ID = var.google_cidr_prefix_list_id
+  }
+}
+
+resource "aws_lambda_function_event_invoke_config" "google_cidr_invoke_config" {
+  function_name                = module.lambda-google-cidr.function_name
+  maximum_event_age_in_seconds = 60
+  maximum_retry_attempts       = 0
+}
+
+resource "aws_cloudwatch_event_target" "google_cidr" {
+  arn  = module.lambda-google-cidr.function_arn
+  rule = aws_cloudwatch_event_rule.google_cidr.id
+}
+
+resource "aws_cloudwatch_event_rule" "google_cidr" {
+  name                = "google_cidr_testing"
+  description         = "google_cidr_testing event rule"
+  schedule_expression = var.google_cidr_schedule_expression
+  depends_on          = [module.lambda-google-cidr]
+}
+
+resource "aws_lambda_permission" "allow_cloudwatch" {
+  statement_id  = "AllowExecutionFromCloudWatch"
+  action        = "lambda:InvokeFunction"
+  function_name = module.lambda-google-cidr.function_name
+  principal     = "events.amazonaws.com"
+  source_arn    = aws_cloudwatch_event_rule.google_cidr.arn
+}
---
+++ env/staging/lambda-google-cidr/terragrunt.hcl
@@ -0,0 +1,33 @@
+dependencies {
+  paths = ["../common"]
+}
+
+dependency "common" {
+  config_path = "../common"
+}
+
+dependency "eks" {
+  config_path = "../eks"
+
+  # Configure mock outputs for the `validate` command that are returned when there are no outputs available (e.g the
+  # module hasn't been applied yet.
+  mock_outputs_allowed_terraform_commands = ["validate", "plan", "init", "fmt", "show"]
+  mock_outputs_merge_with_state           = true
+  mock_outputs = {
+    google_cidr_prefix_list_id      = ""
+  }
+}
+
+include {
+  path = find_in_parent_folders()
+}
+
+inputs = {
+  billing_tag_value                  = "notification-canada-ca-staging"
+  google_cidr_schedule_expression    = "rate(5 minutes)"
+  google_cidr_prefix_list_id         = dependency.eks.outputs.google_cidr_prefix_list_id
+}
+
+terraform {
+  source = "../../../aws//lambda-google-cidr"
+}
