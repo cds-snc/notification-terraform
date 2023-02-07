edb10e12b3d3c3eef4f332c4bdc00ed181b21f26
Jumana B <jzbahrai@users.noreply.github.com>
2023-02-07 15:52:24 -0500

    Add ses_receiving_emails as an image (#636)

---
+++ aws/ses_to_sqs_email_callbacks/lambda.tf
@@ -0,0 +1,18 @@
+module "ses_to_sqs_email_callbacks" {
+  source                 = "github.com/cds-snc/terraform-modules?ref=v4.0.3//lambda"
+  name                   = "ses_to_sqs_email_callbacks"
+  billing_tag_value      = var.billing_tag_value
+  ecr_arn                = aws_ecr_repository.ses_to_sqs_email_callbacks.arn
+  enable_lambda_insights = true
+  image_uri              = "${aws_ecr_repository.ses_to_sqs_email_callbacks.repository_url}:latest"
+  timeout                = 60
+  memory                 = 1024
+
+}
+
+resource "aws_lambda_permission" "allow_sns_ses_callbacks" {
+  action        = "lambda:InvokeFunction"
+  function_name = module.ses_to_sqs_email_callbacks.function_name
+  principal     = "sns.amazonaws.com"
+  source_arn    = var.notification_canada_ca_ses_callback_arn
+}
---
+++ aws/ses_to_sqs_email_callbacks/variables.tf
@@ -0,0 +1,15 @@
+variable "billing_tag_value" {
+  type        = string
+  description = "Identifies the billing code."
+}
+variable "sns_alert_warning_arn" {
+  type = string
+}
+
+variable "sns_alert_critical_arn" {
+  type = string
+}
+
+variable "notification_canada_ca_ses_callback_arn" {
+  type = string
+}
