2404e37c44e653eed113b31922069ade06d39464
Stephen Astels <sastels@gmail.com>
2023-02-24 16:38:43 -0500

    just use port 5432 for ingress rule

--- aws/eks/securitygroups.tf
+++ aws/eks/securitygroups.tf
@@ -110,8 +110,8 @@ resource "aws_security_group_rule" "quicksight-db-ingress" {
   description              = "Connect RDS to Quicksight"
   type                     = "ingress"
   protocol                 = "tcp"
-  from_port                = 0
-  to_port                  = 65535
+  from_port                = 5432
+  to_port                  = 5432
   source_security_group_id = data.aws_security_group.eks-securitygroup-rds.id
   security_group_id        = aws_security_group.quicksight.id
 }
