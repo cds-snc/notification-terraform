90ba0a8a7997efd577e3aab03ddd48ed65c9b153
Stephen Astels <sastels@gmail.com>
2023-02-28 12:53:31 -0500

    delete bulk send s3 buckets and iam user

--- aws/common/iam.tf
+++ aws/common/iam.tf
@@ -128,48 +128,6 @@ resource "aws_iam_role_policy_attachment" "lambda_sqs" {
 }
 
 ###
-# IAM role for S3 bulk send
-###
-resource "aws_iam_user" "bulk_send" {
-  name = "bulk-send"
-}
-
-resource "aws_iam_access_key" "bulk_send" {
-  user = aws_iam_user.bulk_send.name
-}
-
-resource "aws_iam_user_policy" "write_s3_bulk_send" {
-  name = "WriteS3BulkSend"
-  user = aws_iam_user.bulk_send.name
-
-  policy = <<EOF
-{
-   "Version":"2012-10-17",
-   "Statement":[
-      {
-         "Sid":"ListObjectsInBucket",
-         "Effect":"Allow",
-         "Action":[
-            "s3:ListBucket"
-         ],
-         "Resource":[
-            "${aws_s3_bucket.bulk_send.arn}"
-         ]
-      },
-      {
-         "Sid":"AllObjectActions",
-         "Effect":"Allow",
-         "Action":"s3:*Object",
-         "Resource":[
-            "${aws_s3_bucket.bulk_send.arn}/*"
-         ]
-      }
-   ]
-}
-EOF
-}
-
-###
 # IAM role for Continuous guardrail scanning
 # https://github.com/cds-snc/continuous-guardrail-scanning
 # Group + User + Credentials + Policy
