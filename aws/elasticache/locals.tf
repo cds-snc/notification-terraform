70abf6d7fe36290fbaae1b5620356bafa7582328
Pat Heard <patrick.heard@cds-snc.ca>
2023-03-07 20:50:28 +0000

    fix: cluster security group ID list

--- aws/elasticache/locals.tf
+++ aws/elasticache/locals.tf
@@ -1,4 +1,4 @@
 locals {
-  # Lambda PR review env security group is only included in Staging
-  cluster_security_group_ids = var.env == "staging" ? [var.eks_cluster_securitygroup, aws_security_group.lambda_admin_pr_review[0].id] : [var.eks_cluster_securitygroup]
+  # In Staging we add a Redis cluster specific security group for things like the lambda admin PR review env
+  cluster_security_group_ids = var.env == "staging" ? [var.eks_cluster_securitygroup, aws_security_group.redis_cluster[0].id] : [var.eks_cluster_securitygroup]
 }
