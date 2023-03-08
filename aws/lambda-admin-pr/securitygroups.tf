f7beddd8ed6d253832bac6058faf2991b993cb73
Pat Heard <patrick.heard@cds-snc.ca>
2023-03-08 10:18:49 -0500

    fix: allow admin PR review env egress to internet (#682)
    Update the lambda admin PR review to allow egress to the
    internet on 443 TCP so that it can communicate with the
    Notify API.

--- aws/lambda-admin-pr/securitygroups.tf
+++ aws/lambda-admin-pr/securitygroups.tf
@@ -78,3 +78,14 @@ resource "aws_security_group_rule" "internet_ingress_to_lambda" {
   security_group_id = aws_security_group.lambda_admin_pr_review[0].id
   cidr_blocks       = ["0.0.0.0/0"]
 }
+
+resource "aws_security_group_rule" "lambda_egress_to_internet" {
+  count             = var.env == "staging" ? 1 : 0
+  description       = "Allow outbound connections from lambda admin PR review env to the internet"
+  type              = "egress"
+  from_port         = 443
+  to_port           = 443
+  protocol          = "tcp"
+  security_group_id = aws_security_group.lambda_admin_pr_review[0].id
+  cidr_blocks       = ["0.0.0.0/0"]
+}
