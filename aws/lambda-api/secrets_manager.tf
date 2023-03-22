9cc500cbd7f56704f29062e740aca263510c71e5
Ben Larabie <ben.larabie@cds-snc.ca>
2023-03-22 13:34:55 -0400

    Removing secret data

--- aws/lambda-api/secrets_manager.tf
+++ aws/lambda-api/secrets_manager.tf
@@ -8,10 +8,3 @@ resource "aws_secretsmanager_secret_version" "new-relic-license-key" {
   secret_id     = aws_secretsmanager_secret.new-relic-license-key.id
   secret_string = var.new_relic_license_key
 }
-data "aws_secretsmanager_secret_version" "new-relic-license-key" {
-  secret_id = data.aws_secretsmanager_secret.new-relic-license-key.id
-}
-
-data "aws_secretsmanager_secret" "new-relic-license-key" {
-  name = aws_secretsmanager_secret.new-relic-license-key.name
-}
