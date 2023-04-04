5da330286e6e3011870c7dc6a53824b465b13bf0
Stephen Astels <sastels@gmail.com>
2023-04-04 16:43:39 -0400

    comment out some heartbeat

--- aws/heartbeat/cloudwatch_logs.tf
+++ aws/heartbeat/cloudwatch_logs.tf
@@ -12,14 +12,14 @@ resource "aws_cloudwatch_log_group" "heartbeat_log_group" {
   }
 }
 
-resource "aws_cloudwatch_log_metric_filter" "heartbeat-500-errors-api" {
-  name           = "heartbeat-500-errors-api"
-  pattern        = "\"\\\"levelname\\\": \\\"ERROR\\\"\""
-  log_group_name = "/aws/lambda/${module.heartbeat.function_name}"
+# resource "aws_cloudwatch_log_metric_filter" "heartbeat-500-errors-api" {
+#   name           = "heartbeat-500-errors-api"
+#   pattern        = "\"\\\"levelname\\\": \\\"ERROR\\\"\""
+#   log_group_name = "/aws/lambda/${module.heartbeat.function_name}"
 
-  metric_transformation {
-    name      = "500-errors-heartbeat-api"
-    namespace = "LogMetrics"
-    value     = "1"
-  }
-}
+#   metric_transformation {
+#     name      = "500-errors-heartbeat-api"
+#     namespace = "LogMetrics"
+#     value     = "1"
+#   }
+# }
