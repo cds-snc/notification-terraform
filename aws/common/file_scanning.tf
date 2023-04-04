f292655cc403b5ffcc66df264b1759cd1cf56fc6
Stephen Astels <sastels@gmail.com>
2023-04-04 14:38:23 -0400

    add another default tag

--- aws/common/file_scanning.tf
+++ aws/common/file_scanning.tf
@@ -1,8 +1,8 @@
-# module "s3_scan_objects" {
-#   source = "github.com/cds-snc/terraform-modules?ref=v5.0.0//S3_scan_object"
+module "s3_scan_objects" {
+  source = "github.com/cds-snc/terraform-modules?ref=v5.0.0//S3_scan_object"
 
-#   product_name          = "gc-notify"
-#   s3_upload_bucket_name = "notification-canada-ca-${var.env}-document-download-scan-files"
+  product_name          = "gc-notify"
+  s3_upload_bucket_name = "notification-canada-ca-${var.env}-document-download-scan-files"
 
-#   billing_tag_value = var.billing_tag_value
-# }
+  billing_tag_value = var.billing_tag_value
+}
