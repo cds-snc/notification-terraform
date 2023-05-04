5f941b484ee6de7d58cb16a3d8fd12daa198cd13
Ben Larabie <ben.larabie@cds-snc.ca>
2023-05-04 13:13:43 -0400

    Upping the retention period for elasticache logs to 90 days

--- aws/elasticache/cloudwatch_log.tf
+++ aws/elasticache/cloudwatch_log.tf
@@ -1,9 +1,9 @@
 resource "aws_cloudwatch_log_group" "notification-canada-ca-elasticache-slow-logs" {
   name              = "/aws/elasticache/notify-${var.env}-cluster-cache-az/slow-logs"
-  retention_in_days = 14
+  retention_in_days = 90
 }
 
 resource "aws_cloudwatch_log_group" "notification-canada-ca-elasticache-engine-logs" {
   name              = "/aws/elasticache/notify-${var.env}-cluster-cache-az/engine-logs"
-  retention_in_days = 14
+  retention_in_days = 90
 }
