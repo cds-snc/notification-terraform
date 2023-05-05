d21aa76df1a17970d4abc608fa9494e47799484d
Ben Larabie <ben.larabie@cds-snc.ca>
2023-05-05 14:52:22 -0400

    Fixed region for ses_receiving_email and got dns to deploy

--- aws/ecr/ecr.tf
+++ aws/ecr/ecr.tf
@@ -61,6 +61,7 @@ resource "aws_ecr_repository" "performance-test" {
 resource "aws_ecr_repository" "ses_receiving_emails" {
   # The :latest tag is used in Staging
 
+  provider             = aws.us-east-1
   name                 = "notify/ses_receiving_emails"
   image_tag_mutability = "MUTABLE" #tfsec:ignore:AWS078
   force_delete         = var.force_delete_ecr
