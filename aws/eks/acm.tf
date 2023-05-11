6fc609adea2a994276cb224eca7f5b51f95de91c
Ben Larabie <ben.larabie@cds-snc.ca>
2023-05-11 09:31:06 -0400

    Re-enabling ACM validation for staging

--- aws/eks/acm.tf
+++ aws/eks/acm.tf
@@ -49,14 +49,14 @@ resource "aws_route53_record" "notification-canada-ca" {
   zone_id         = var.route_53_zone_arn
   ttl             = 60
 }
-/*
+
 resource "aws_acm_certificate_validation" "notification-canada-ca" {
   count = var.env != "production" ? 1 : 0
 
   certificate_arn         = aws_acm_certificate.notification-canada-ca.arn
   validation_record_fqdns = [aws_route53_record.notification-canada-ca[0].fqdn]
 }
-*/
+
 resource "aws_route53_record" "notification-canada-ca-alt" {
   count    = var.env != "production" ? 1 : 0
   provider = aws.staging
@@ -68,7 +68,6 @@ resource "aws_route53_record" "notification-canada-ca-alt" {
   zone_id         = var.route_53_zone_arn
   ttl             = 60
 }
-/*
 
 resource "aws_acm_certificate_validation" "notification-canada-ca-alt" {
   count = var.env != "production" ? 1 : 0
@@ -76,4 +75,3 @@ resource "aws_acm_certificate_validation" "notification-canada-ca-alt" {
   certificate_arn         = aws_acm_certificate.notification-canada-ca-alt[0].arn
   validation_record_fqdns = [aws_route53_record.notification-canada-ca-alt[0].fqdn]
 }
-*/
