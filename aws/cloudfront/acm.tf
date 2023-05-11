963c1e09963c61df6eeaca2b090fedb1abbbe293
Ben Larabie <ben.larabie@cds-snc.ca>
2023-05-11 14:01:53 -0400

    Fixing ACM validation by iterating over SANs and creating route53 records (#747)

--- aws/cloudfront/acm.tf
+++ aws/cloudfront/acm.tf
@@ -15,19 +15,30 @@ resource "aws_acm_certificate" "assets-notification-canada-ca" {
 }
 
 resource "aws_route53_record" "assets-notification-canada-ca" {
-  count           = var.env != "production" ? 1 : 0
-  provider        = aws.staging
+
+  provider = aws.staging
+
+  for_each = {
+    for dvo in aws_acm_certificate.assets-notification-canada-ca.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
   allow_overwrite = true
-  name            = tolist(aws_acm_certificate.assets-notification-canada-ca.domain_validation_options)[0].resource_record_name
-  records         = [tolist(aws_acm_certificate.assets-notification-canada-ca.domain_validation_options)[0].resource_record_value]
-  type            = tolist(aws_acm_certificate.assets-notification-canada-ca.domain_validation_options)[0].resource_record_type
-  zone_id         = var.route_53_zone_arn
+  name            = each.value.name
+  records         = [each.value.record]
+  type            = each.value.type
   ttl             = 60
+  zone_id         = var.route_53_zone_arn
+
 }
 
 
 resource "aws_acm_certificate_validation" "assets-notification-canada-ca" {
-  count                   = var.env != "production" ? 1 : 0
+  for_each = aws_route53_record.assets-notification-canada-ca
+
   certificate_arn         = aws_acm_certificate.assets-notification-canada-ca.arn
-  validation_record_fqdns = [aws_route53_record.assets-notification-canada-ca[0].fqdn]
+  validation_record_fqdns = [each.value.fqdn]
 }
