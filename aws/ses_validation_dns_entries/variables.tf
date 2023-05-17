c07329a86dea38c2e6e8e6f9cff6ee9f7ebf3f1a
Ben Larabie <ben.larabie@cds-snc.ca>
2023-05-17 14:19:26 -0400

    fixing route53 variable name

--- aws/ses_validation_dns_entries/variables.tf
+++ aws/ses_validation_dns_entries/variables.tf
@@ -4,7 +4,7 @@ variable "scratch_account_ids" {
   default     = "\"AWS\": \"419291849580\", \"AWS\": \"239043911459\", \"AWS\": \"296255494825\""
 }
 
-variable "route_53_zone_arn" {
+variable "route53_zone_arn" {
   type        = string
   description = "Used by the scratch environment to reference cdssandbox in staging"
   default     = "/hostedzone/Z04028033PLSHVOO9ZJ1Z"
