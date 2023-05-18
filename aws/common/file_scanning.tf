070cac4b6d7d4d1312117a77a23cef8a6e09490d
Pat Heard <patrick.heard@cds-snc.ca>
2023-05-18 08:41:25 -0400

    fix: increase s3_scan_object function timeout (#756)
    Update to the latest module release which increases the
    transport lambda's timeout to 120s.  This is to fix the cases
    where the transport lambda must wait for a new Scan Files
    lambda function cold start.

--- aws/common/file_scanning.tf
+++ aws/common/file_scanning.tf
@@ -1,5 +1,5 @@
 module "s3_scan_objects" {
-  source = "github.com/cds-snc/terraform-modules?ref=v5.0.0//S3_scan_object"
+  source = "github.com/cds-snc/terraform-modules?ref=v5.1.9//S3_scan_object"
 
   product_name          = "gc-notify"
   s3_upload_bucket_name = "notification-canada-ca-${var.env}-document-download-scan-files"
