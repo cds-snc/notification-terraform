f3a530291f7b9c56a9aa10de1020e0514ef1ad6a
Jimmy Royer <jimleroyer@gmail.com>
2023-07-20 08:48:53 -0400

    Revert "scan object to reference s3 bucket by ID (#823)"
    This reverts commit ee08a2e4db3a00a410ac6741ef1b1afa2ceb3dc4.

--- aws/common/file_scanning.tf
+++ aws/common/file_scanning.tf
@@ -1,6 +1,6 @@
 module "s3_scan_objects" {
   source = "github.com/cds-snc/terraform-modules//S3_scan_object?ref=v6.0.1"
 
-  s3_upload_bucket_name = aws_s3_bucket.scan_files_document_bucket.id
+  s3_upload_bucket_name = "notification-canada-ca-${var.env}-document-download-scan-files"
   billing_tag_value     = var.billing_tag_value
 }
