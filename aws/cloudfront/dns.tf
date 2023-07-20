f85d744b5ce24ec101819fe739d0cc335faf4cce
Ben Larabie <ben.larabie@cds-snc.ca>
2023-07-20 12:18:29 -0400

    Removing aws.staging references

--- aws/cloudfront/dns.tf
+++ aws/cloudfront/dns.tf
@@ -1,7 +1,7 @@
 resource "aws_route53_record" "assets-notification-CNAME" {
   count = var.env != "production" ? 1 : 0
 
-  provider = aws.staging
+  provider = aws.dns
   zone_id  = var.route_53_zone_arn
   name     = "assets.${var.domain}"
   type     = "CNAME"
