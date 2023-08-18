53886045ef100aba193abddcb19cd56c7ce12732
Stephen Astels <sastels@gmail.com>
2023-08-18 14:17:15 -0400

    all your columns belong to us

--- aws/quicksight/datasets.tf
+++ aws/quicksight/datasets.tf
@@ -1,3 +1,5 @@
+# valid column types are [STRING INTEGER DECIMAL DATETIME BIT BOOLEAN JSON]
+
 resource "aws_quicksight_data_set" "notifications" {
   data_set_id = "notifications"
   name        = "Notifications"
@@ -10,50 +12,48 @@ resource "aws_quicksight_data_set" "notifications" {
       name            = "notifications"
       input_columns {
         name = "id"
-        type = "STRING"  # [STRING INTEGER DECIMAL DATETIME BIT BOOLEAN JSON]
+        type = "STRING"
       }
-      #   input_columns = [
-      #     {
-      #       name = "id"
-      #       type = "string"
-      #     },
-      #     {
-      #       name = "template_id"
-      #       type = "string"
-      #     },
-      #     {
-      #       name = "template_version"
-      #       type = "string"
-      #     },
-      #     {
-      #       name = "service_id"
-      #       type = "string"
-      #     },
-      #     {
-      #       name = "job_id"
-      #       type = "string"
-      #     },
-      #     {
-      #       name = "notification_type"
-      #       type = "string"
-      #     },
-      #     {
-      #       name = "notification_status"
-      #       type = "string"
-      #     },
-      #     {
-      #       name = "created_at"
-      #       type = "date"
-      #     },
-      #     {
-      #       name = "sent_at"
-      #       type = "date"
-      #     },
-      #     {
-      #       name = "updated_at"
-      #       type = "date"
-      #     }
-      #   ]
+      input_columns {
+        name = "id"
+        type = "STRING"
+      }
+      input_columns {
+        name = "template_id"
+        type = "STRING"
+      }
+      input_columns {
+        name = "template_version"
+        type = "STRING"
+      }
+      input_columns {
+        name = "service_id"
+        type = "STRING"
+      }
+      input_columns {
+        name = "job_id"
+        type = "STRING"
+      }
+      input_columns {
+        name = "notification_type"
+        type = "STRING"
+      }
+      input_columns {
+        name = "notification_status"
+        type = "STRING"
+      }
+      input_columns {
+        name = "created_at"
+        type = "DATETIME"
+      }
+      input_columns {
+        name = "sent_at"
+        type = "DATETIME"
+      }
+      input_columns {
+        name = "upDATETIMEd_at"
+        type = "DATETIME"
+      }
     }
   }
 }
