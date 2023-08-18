dbdfb438a4cb4aaa62b2de44aa734a1fab44436e
Stephen Astels <sastels@gmail.com>
2023-08-18 13:55:13 -0400

    ok input_columns has to be a block

--- aws/quicksight/datasets.tf
+++ aws/quicksight/datasets.tf
@@ -8,48 +8,52 @@ resource "aws_quicksight_data_set" "notifications" {
     relational_table {
       data_source_arn = aws_quicksight_data_source.rds.arn
       name            = "notifications"
-      input_columns = [
-        {
-          name = "id"
-          type = "string"
-        },
-        {
-          name = "template_id"
-          type = "string"
-        },
-        {
-          name = "template_version"
-          type = "string"
-        },
-        {
-          name = "service_id"
-          type = "string"
-        },
-        {
-          name = "job_id"
-          type = "string"
-        },
-        {
-          name = "notification_type"
-          type = "string"
-        },
-        {
-          name = "notification_status"
-          type = "string"
-        },
-        {
-          name = "created_at"
-          type = "date"
-        },
-        {
-          name = "sent_at"
-          type = "date"
-        },
-        {
-          name = "updated_at"
-          type = "date"
-        }
-      ]
+      input_columns {
+        name = "id"
+        type = "STRING"  # [STRING INTEGER DECIMAL DATETIME BIT BOOLEAN JSON]
+      }
+      #   input_columns = [
+      #     {
+      #       name = "id"
+      #       type = "string"
+      #     },
+      #     {
+      #       name = "template_id"
+      #       type = "string"
+      #     },
+      #     {
+      #       name = "template_version"
+      #       type = "string"
+      #     },
+      #     {
+      #       name = "service_id"
+      #       type = "string"
+      #     },
+      #     {
+      #       name = "job_id"
+      #       type = "string"
+      #     },
+      #     {
+      #       name = "notification_type"
+      #       type = "string"
+      #     },
+      #     {
+      #       name = "notification_status"
+      #       type = "string"
+      #     },
+      #     {
+      #       name = "created_at"
+      #       type = "date"
+      #     },
+      #     {
+      #       name = "sent_at"
+      #       type = "date"
+      #     },
+      #     {
+      #       name = "updated_at"
+      #       type = "date"
+      #     }
+      #   ]
     }
   }
 }
