af184a6dad72a9024c1bbb6f8ef31f58bb825c97
Stephen Astels <sastels@gmail.com>
2023-08-31 13:15:00 -0400

    add an alarm to test PR

--- aws/eks/cloudwatch_alarms.tf
+++ aws/eks/cloudwatch_alarms.tf
@@ -4,6 +4,24 @@
 #
 # There are also alarms defined in aws/common/cloudwatch_alarms.tf
 
+
+resource "aws_cloudwatch_metric_alarm" "Steve-alarm" {
+  alarm_name          = "steve-warning"
+  alarm_description   = "Too many Steves"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  evaluation_periods  = "1"
+  metric_name         = "HTTPCode_ELB_500_Count"
+  namespace           = "AWS/ApplicationELB"
+  period              = 60
+  statistic           = "Sum"
+  threshold           = 1
+  alarm_actions       = [var.sns_alert_warning_arn]
+  treat_missing_data  = "notBreaching"
+  dimensions = {
+    LoadBalancer = aws_alb.notification-canada-ca.arn_suffix
+  }
+}
+
 resource "aws_cloudwatch_metric_alarm" "load-balancer-1-500-error-1-minute-warning" {
   alarm_name          = "load-balancer-1-500-error-1-minute-warning"
   alarm_description   = "One 500 error in 1 minute"
