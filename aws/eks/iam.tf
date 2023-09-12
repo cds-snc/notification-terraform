bb9ffe3c1ce3bf0381e066b709411996ece22aef
Ben Larabie <ben.larabie@cds-snc.ca>
2023-09-12 14:59:20 -0400

    Removing the commit sha reference attempt

--- aws/eks/iam.tf
+++ aws/eks/iam.tf
@@ -226,8 +226,8 @@ data "aws_iam_policy" "ssm_managed_instance" {
 
 
 module "iam_assumable_role_karpenter" {
-  source = "terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc?ref=8af6d28"
-  #version                       = "5.28.0"
+  source                        = "terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc"
+  version                       = "5.28.0"
   create_role                   = true
   role_name                     = "karpenter-controller-${var.eks_cluster_name}"
   provider_url                  = data.aws_eks_cluster.notify_cluster.identity[0].oidc[0].issuer
