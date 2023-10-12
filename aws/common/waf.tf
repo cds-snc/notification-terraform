77155e991a4121f58850f1f6dea56ee8dae44307
wbanks <william.banks@cds-snc.ca>
2023-10-12 08:05:17 -0400

    formatting

--- aws/common/waf.tf
+++ aws/common/waf.tf
@@ -2,7 +2,7 @@ resource "aws_wafv2_ip_set" "ip_blocklist" {
   name               = "ip_blocklist"
   scope              = "REGIONAL"
   ip_address_version = "IPV4"
-  addresses          = [
+  addresses = [
     "40.50.60.70/32",
     "103.14.26.118/32"
   ]
