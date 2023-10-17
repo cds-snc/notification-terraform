661b727af05560bead487cf8d42795e76acb180e
Jimmy Royer <jimleroyer@gmail.com>
2023-10-17 15:30:49 -0400

    Putting in some vars for the SQS visibility timeouts

--- aws/common/sqs.tf
+++ aws/common/sqs.tf
@@ -26,8 +26,8 @@ resource "aws_sqs_queue" "notify_internal_tasks_queue" {
   name = "${var.celery_queue_prefix}notify-internal-tasks"
   # This queue was created outside of terraform and has this value set to false in staging and production.
   sqs_managed_sse_enabled = false
-  # This queue was created outside of terraform and has this value set to 310 in staging and production.
-  visibility_timeout_seconds = 310
+  # This queue was created outside of terraform and has this value set to default in staging and production.
+  visibility_timeout_seconds = ${var.sqs_visibility_timeout_default}
 }
 
 resource "aws_sqs_queue" "eks_notification_canada_ca_sms_high_queue" {
@@ -37,7 +37,7 @@ resource "aws_sqs_queue" "eks_notification_canada_ca_sms_high_queue" {
   # AWS managed encryption is good enough for us
 
   # Visibility timeout is set to 26 seconds for an application side retry set to 25 seconds.
-  visibility_timeout_seconds = 26
+  visibility_timeout_seconds = {$var.sqs_visibility_timeout_priority_high}
 }
 
 resource "aws_sqs_queue" "eks_notification_canada_cadelivery_receipts" {
@@ -47,7 +47,7 @@ resource "aws_sqs_queue" "eks_notification_canada_cadelivery_receipts" {
   #4 Days
   message_retention_seconds  = 345600
   receive_wait_time_seconds  = 0
-  visibility_timeout_seconds = 310
+  visibility_timeout_seconds = ${var.sqs_visibility_timeout_default}
 
   tags = {
     Environment = var.env
