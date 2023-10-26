4fafa563aee96f0a9a3b56afc232b0e82be0c893
Ben Larabie <ben.larabie@cds-snc.ca>
2023-10-26 13:22:33 -0400

    Setting visibility timeout for high priority email sqs

--- aws/common/sqs.tf
+++ aws/common/sqs.tf
@@ -40,6 +40,16 @@ resource "aws_sqs_queue" "eks_notification_canada_ca_sms_high_queue" {
   visibility_timeout_seconds = var.sqs_visibility_timeout_priority_high
 }
 
+resource "aws_sqs_queue" "eks_notification_canada_ca_priority_task_queue" {
+  name                    = "${var.celery_queue_prefix}priority-tasks"
+  sqs_managed_sse_enabled = true
+  # tfsec:ignore:AWS015 - Queues should be encrypted with customer managed KMS keys
+  # AWS managed encryption is good enough for us
+
+  # Visibility timeout is set to 26 seconds for an application side retry set to 25 seconds.
+  visibility_timeout_seconds = var.sqs_visibility_timeout_priority_high
+}
+
 resource "aws_sqs_queue" "eks_notification_canada_cadelivery_receipts" {
   name             = "eks-notification-canada-cadelivery-receipts"
   delay_seconds    = 0
