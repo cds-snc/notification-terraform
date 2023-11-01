912420b50bf81010d38fec9e44796178cc6dff70
Ben Larabie <ben.larabie@cds-snc.ca>
2023-11-01 13:13:44 -0400

    Added comment on reasoning around this change

--- aws/common/uploadS3Assets.tf
+++ aws/common/uploadS3Assets.tf
@@ -1,3 +1,6 @@
+# This is a bit of a hack in that we're cloning an external repo into a temp directory so that we can populate cloudfront.
+# We will have to review a better way of doing this in the future, but this will get us
+
 resource "aws_s3_object" "assets" {
 
   bucket   = aws_s3_bucket.asset_bucket.id
