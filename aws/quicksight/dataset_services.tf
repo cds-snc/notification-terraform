7c93b0cac74555c1ee6103b5895cc95f2abcb758
Stephen Astels <sastels@gmail.com>
2023-11-10 15:57:49 -0500

    rename fields in services and templates

--- aws/quicksight/dataset_services.tf
+++ aws/quicksight/dataset_services.tf
@@ -5,6 +5,15 @@ resource "aws_quicksight_data_set" "services" {
   name        = "Services"
   import_mode = "SPICE"
 
+  permissions {
+    actions   = local.dataset_viewer_permissions
+    principal = aws_quicksight_group.dataset_viewer.arn
+  }
+  permissions {
+    actions   = local.dataset_owner_permissions
+    principal = aws_quicksight_group.dataset_owner.arn
+  }
+
   physical_table_map {
     physical_table_map_id = "services"
     relational_table {
@@ -52,12 +61,72 @@ resource "aws_quicksight_data_set" "services" {
       }
     }
   }
-  permissions {
-    actions   = local.dataset_viewer_permissions
-    principal = aws_quicksight_group.dataset_viewer.arn
-  }
-  permissions {
-    actions   = local.dataset_owner_permissions
-    principal = aws_quicksight_group.dataset_owner.arn
+
+  logical_table_map {
+    alias                = "services"
+    logical_table_map_id = "services"
+    source {
+      physical_table_id = "services"
+    }
+    data_transforms {
+      rename_column_operation {
+        column_name     = "id"
+        new_column_name = "service_id"
+      }
+    }
+    data_transforms {
+      rename_column_operation {
+        column_name     = "updated_at"
+        new_column_name = "service_updated_at"
+      }
+    }
+    data_transforms {
+      rename_column_operation {
+        column_name     = "created_at"
+        new_column_name = "service_created_at"
+      }
+    }
+    data_transforms {
+      rename_column_operation {
+        column_name     = "active"
+        new_column_name = "service_active"
+      }
+    }
+    data_transforms {
+      rename_column_operation {
+        column_name     = "count_as_live"
+        new_column_name = "service_count_as_live"
+      }
+    }
+    data_transforms {
+      rename_column_operation {
+        column_name     = "go_live_at"
+        new_column_name = "service_go_live_at"
+      }
+    }
+    data_transforms {
+      rename_column_operation {
+        column_name     = "name"
+        new_column_name = "service_name"
+      }
+    }
+    data_transforms {
+      rename_column_operation {
+        column_name     = "message_limit"
+        new_column_name = "service_message_limit"
+      }
+    }
+    data_transforms {
+      rename_column_operation {
+        column_name     = "rate_limit"
+        new_column_name = "service_rate_limit"
+      }
+    }
+    data_transforms {
+      rename_column_operation {
+        column_name     = "sms_daily_limit"
+        new_column_name = "service_sms_daily_limit"
+      }
+    }
   }
 }
