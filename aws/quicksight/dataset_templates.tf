90afc380eee978b5e5075222a0aa67772fc73c2e
Steve Astels <sastels@users.noreply.github.com>
2023-11-14 13:02:16 -0500

    Rename QuickSight fields (#1020)

--- aws/quicksight/dataset_templates.tf
+++ aws/quicksight/dataset_templates.tf
@@ -5,8 +5,17 @@ resource "aws_quicksight_data_set" "templates" {
   name        = "TemplatesV2"
   import_mode = "SPICE"
 
+  permissions {
+    actions   = local.dataset_viewer_permissions
+    principal = aws_quicksight_group.dataset_viewer.arn
+  }
+  permissions {
+    actions   = local.dataset_owner_permissions
+    principal = aws_quicksight_group.dataset_owner.arn
+  }
+
   physical_table_map {
-    physical_table_map_id = "templatesv2"
+    physical_table_map_id = "templates"
     relational_table {
       data_source_arn = aws_quicksight_data_source.rds.arn
       name            = "templates"
@@ -44,12 +53,48 @@ resource "aws_quicksight_data_set" "templates" {
       }
     }
   }
-  permissions {
-    actions   = local.dataset_viewer_permissions
-    principal = aws_quicksight_group.dataset_viewer.arn
-  }
-  permissions {
-    actions   = local.dataset_owner_permissions
-    principal = aws_quicksight_group.dataset_owner.arn
+
+  logical_table_map {
+    alias                = "templates"
+    logical_table_map_id = "templates"
+    source {
+      physical_table_id = "templates"
+    }
+    data_transforms {
+      rename_column_operation {
+        column_name     = "id"
+        new_column_name = "template_id"
+      }
+    }
+    data_transforms {
+      rename_column_operation {
+        column_name     = "updated_at"
+        new_column_name = "template_updated_at"
+      }
+    }
+    data_transforms {
+      rename_column_operation {
+        column_name     = "created_at"
+        new_column_name = "template_created_at"
+      }
+    }
+    data_transforms {
+      rename_column_operation {
+        column_name     = "name"
+        new_column_name = "template_name"
+      }
+    }
+    data_transforms {
+      rename_column_operation {
+        column_name     = "process_type"
+        new_column_name = "template_process_type"
+      }
+    }
+    data_transforms {
+      rename_column_operation {
+        column_name     = "version"
+        new_column_name = "template_version"
+      }
+    }
   }
 }
