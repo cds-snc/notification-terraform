d496a64a6af12e60edc22195dfd85ecc97974a97
Ben Larabie <ben.larabie@cds-snc.ca>
2023-11-15 11:02:54 -0500

    formatting

--- aws/common/cloudwatch_log.tf
+++ aws/common/cloudwatch_log.tf
@@ -2,8 +2,8 @@
 # AWS CloudWatch Log Groups
 ###
 resource "aws_cloudwatch_log_group" "sns_deliveries" {
-  count = var.cloudwatch_enabled ? 1 : 0
-  name  = "sns/${var.region}/${var.account_id}/DirectPublishToPhoneNumber"
+  count             = var.cloudwatch_enabled ? 1 : 0
+  name              = "sns/${var.region}/${var.account_id}/DirectPublishToPhoneNumber"
   retention_in_days = var.env == "production" ? 0 : 365
 
   tags = {
@@ -12,8 +12,8 @@ resource "aws_cloudwatch_log_group" "sns_deliveries" {
 }
 
 resource "aws_cloudwatch_log_group" "sns_deliveries_failures" {
-  count = var.cloudwatch_enabled ? 1 : 0
-  name  = "sns/${var.region}/${var.account_id}/DirectPublishToPhoneNumber/Failure"
+  count             = var.cloudwatch_enabled ? 1 : 0
+  name              = "sns/${var.region}/${var.account_id}/DirectPublishToPhoneNumber/Failure"
   retention_in_days = var.env == "production" ? 0 : 365
 
   tags = {
@@ -24,8 +24,8 @@ resource "aws_cloudwatch_log_group" "sns_deliveries_failures" {
 resource "aws_cloudwatch_log_group" "sns_deliveries_us_west_2" {
   provider = aws.us-west-2
 
-  count = var.cloudwatch_enabled ? 1 : 0
-  name  = "sns/us-west-2/${var.account_id}/DirectPublishToPhoneNumber"
+  count             = var.cloudwatch_enabled ? 1 : 0
+  name              = "sns/us-west-2/${var.account_id}/DirectPublishToPhoneNumber"
   retention_in_days = var.env == "production" ? 0 : 365
 
   tags = {
@@ -36,8 +36,8 @@ resource "aws_cloudwatch_log_group" "sns_deliveries_us_west_2" {
 resource "aws_cloudwatch_log_group" "sns_deliveries_failures_us_west_2" {
   provider = aws.us-west-2
 
-  count = var.cloudwatch_enabled ? 1 : 0
-  name  = "sns/us-west-2/${var.account_id}/DirectPublishToPhoneNumber/Failure"
+  count             = var.cloudwatch_enabled ? 1 : 0
+  name              = "sns/us-west-2/${var.account_id}/DirectPublishToPhoneNumber/Failure"
   retention_in_days = var.env == "production" ? 0 : 365
 
   tags = {
@@ -46,8 +46,8 @@ resource "aws_cloudwatch_log_group" "sns_deliveries_failures_us_west_2" {
 }
 
 resource "aws_cloudwatch_log_group" "route53_resolver_query_log" {
-  count = var.cloudwatch_enabled ? 1 : 0
-  name  = "route53/${var.region}/${var.account_id}/DNS/logs"
+  count             = var.cloudwatch_enabled ? 1 : 0
+  name              = "route53/${var.region}/${var.account_id}/DNS/logs"
   retention_in_days = var.env == "production" ? 0 : 365
 
   tags = {
