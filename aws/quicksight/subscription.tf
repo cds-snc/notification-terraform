3049e1620aef3ff7361c5b8642a813cf36f194f6
Stephen Astels <sastels@gmail.com>
2023-11-20 15:00:51 -0500

    fix subscription name

--- aws/quicksight/subscription.tf
+++ aws/quicksight/subscription.tf
@@ -1,5 +1,5 @@
 resource "aws_quicksight_account_subscription" "subscription" {
-  account_name          = "quicksight-test-${var.env}-${var.account_id}"
+  account_name          = "quicksight-${var.env}-${var.account_id}"
   authentication_method = "IAM_ONLY"
   edition               = "ENTERPRISE"
   notification_email    = "helpdesk@cds-snc.ca"
