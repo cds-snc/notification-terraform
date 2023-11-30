81fec7d25a9061e527643fb372a0cba6978a04de
Ben Larabie <ben.larabie@cds-snc.ca>
2023-11-30 15:22:13 -0500

    Explicitly setting kms for new log grou

--- aws/elasticache/cloudwatch_log.tf
+++ aws/elasticache/cloudwatch_log.tf
@@ -11,4 +11,5 @@ resource "aws_cloudwatch_log_group" "notification-canada-ca-elasticache-engine-l
 resource "aws_cloudwatch_log_group" "redis-batch-saving" {
   name              = "BatchSaving"
   retention_in_days = var.env == "production" ? 0 : 365
+  kms_key_id        = var.kms_arn
 }
