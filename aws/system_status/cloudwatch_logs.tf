0602374d248b307dce9b57d3250aece3936dca43
Jumana Bahrainwala <jzbahrai@uwaterloo.ca>
2023-12-07 15:32:27 -0500

    Basic scaffolding for system status lambda

---
+++ aws/system_status/cloudwatch_logs.tf
@@ -0,0 +1,27 @@
+#
+# System Status CloudWatch logging
+#
+
+resource "aws_cloudwatch_log_group" "system_status_log_group" {
+  count             = var.cloudwatch_enabled ? 1 : 0
+  name              = "system_status_log_group"
+  retention_in_days = var.log_retention_period_days
+  tags = {
+    CostCenter  = "notification-canada-ca-${var.env}"
+    Environment = var.env
+    Application = "lambda"
+  }
+}
+
+resource "aws_cloudwatch_log_metric_filter" "system_status-500-errors-api" {
+  count          = var.cloudwatch_enabled ? 1 : 0
+  name           = "system_status-500-errors-api"
+  pattern        = "\"\\\"levelname\\\": \\\"ERROR\\\"\""
+  log_group_name = "/aws/lambda/${module.system_status.function_name}"
+
+  metric_transformation {
+    name      = "500-errors-system_status-api"
+    namespace = "LogMetrics"
+    value     = "1"
+  }
+}
