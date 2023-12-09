404926abebb72efa974742d84a034c61e729342e
Pat Heard <patrick.heard@cds-snc.ca>
2023-12-09 01:08:46 +0000

    fix: only create self-service portal in prod

--- aws/common/client_vpn.tf
+++ aws/common/client_vpn.tf
@@ -3,19 +3,21 @@
 # access to the private subnets.
 #
 module "vpn" {
-  source = "github.com/cds-snc/terraform-modules//client_vpn?ref=v7.4.1"
+  source = "github.com/cds-snc/terraform-modules//client_vpn?ref=v7.4.3"
 
   endpoint_name   = "private-subnets"
   access_group_id = var.client_vpn_access_group_id
 
   vpc_id              = aws_vpc.notification-canada-ca.id
   vpc_cidr_block      = aws_vpc.notification-canada-ca.cidr_block
-  subnet_ids          = var.env == "production" ? aws_subnet.notification-canada-ca-private.*.id : [] # Only associate subnets in production to save costs
   subnet_cidr_blocks  = aws_subnet.notification-canada-ca-private.*.cidr_block
   acm_certificate_arn = aws_acm_certificate.client_vpn.arn
 
+  # Only create a self-service portal in prod  
+  # The client config can still be downloaded from the AWS console
+  self_service_portal                            = var.env == "production" ? "enabled" : "disabled"
+  client_vpn_self_service_saml_metadata_document = var.env == "production" ? var.client_vpn_self_service_saml_metadata : null
   client_vpn_saml_metadata_document              = var.client_vpn_saml_metadata
-  client_vpn_self_service_saml_metadata_document = var.client_vpn_self_service_saml_metadata
 
   billing_tag_value = "notification-canada-ca-${var.env}"
 }
