9021a8795ecf4f4f8aa2567ebf76af6c177849b7
Stephen Astels <sastels@gmail.com>
2023-12-11 12:00:56 -0500

    add refresh schedule

--- aws/quicksight/dataset_login_events.tf
+++ aws/quicksight/dataset_login_events.tf
@@ -1,7 +1,7 @@
 # valid column types are [STRING INTEGER DECIMAL DATETIME BIT BOOLEAN JSON]
 
 resource "aws_quicksight_data_set" "login_events" {
-  data_set_id = "login_events"
+  data_set_id = "login-events"
   name        = "Login Events"
   import_mode = "SPICE"
 
@@ -31,5 +31,32 @@ resource "aws_quicksight_data_set" "login_events" {
   permissions {
     actions   = local.dataset_owner_permissions
     principal = aws_quicksight_group.dataset_owner.arn
+  }
+
+  refresh_properties {
+    refresh_configuration {
+      incremental_refresh {
+        lookback_window {
+          column_name = "created_at"
+          size        = 1
+          size_unit   = "DAY"
+        }
+      }
+    }
+  }
+}
+
+resource "aws_quicksight_refresh_schedule" "login_events" {
+  data_set_id = "login-events"
+  schedule_id = "schedule-login-events"
+  depends_on  = [aws_quicksight_data_set.login_events]
+
+  schedule {
+    refresh_type = "INCREMENTAL_REFRESH"
+
+    schedule_frequency {
+      interval        = "DAILY"
+      time_of_the_day = "07:10"
+    }
   }
 }
