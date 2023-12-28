1beb23495729cec50099dbd14fa44ebbea21eebe
Ben Larabie <ben.larabie@cds-snc.ca>
2023-12-28 08:58:35 -0500

    fixing naming as this is not a per service alert

--- aws/eks/cloudwatch_alarms.tf
+++ aws/eks/cloudwatch_alarms.tf
@@ -415,10 +415,10 @@ resource "aws_cloudwatch_metric_alarm" "logs-1-scanfiles-timeout-1-minute-warnin
   alarm_actions       = [var.sns_alert_warning_arn]
 }
 
-resource "aws_cloudwatch_metric_alarm" "logs-1-bounce-rate-per-service-critical" {
+resource "aws_cloudwatch_metric_alarm" "logs-1-bounce-rate-critical" {
   count               = var.cloudwatch_enabled ? 1 : 0
-  alarm_name          = "logs-1-bounce-rate-per-service-critical"
-  alarm_description   = "One service exceeding 10% bounce rate in a 12 hour period"
+  alarm_name          = "logs-1-bounce-rate-critical"
+  alarm_description   = "Bounce rate exceeding 10% in a 12 hour period"
   comparison_operator = "GreaterThanOrEqualToThreshold"
   evaluation_periods  = "1"
   metric_name         = aws_cloudwatch_log_metric_filter.bounce-rate-critical[0].metric_transformation[0].name
