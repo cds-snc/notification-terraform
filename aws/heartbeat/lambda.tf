7d533f9cb35a60827c1c7ea915a5b64b69e903df
Andrew <andrew.leith@cds-snc.ca>
2024-01-08 13:36:43 -0400

    Remove heartbeat template id env var  (#1088)
    * chore(heartbeat lambda): remove template env var
    
    * chore(hearbeat): add sms number to env vars
    
    ---------
    
    Co-authored-by: Jumana B <jzbahrai@users.noreply.github.com>

--- aws/heartbeat/lambda.tf
+++ aws/heartbeat/lambda.tf
@@ -13,9 +13,9 @@ module "heartbeat" {
   memory                 = 1024
 
   environment_variables = {
-    heartbeat_api_key     = var.heartbeat_api_key
-    heartbeat_base_url    = "['https://api-lambda.${var.base_domain}', 'https://api-k8s.${var.base_domain}']"
-    heartbeat_template_id = var.heartbeat_template_id
+    heartbeat_api_key    = var.heartbeat_api_key
+    heartbeat_base_url   = "['https://api-lambda.${var.base_domain}', 'https://api-k8s.${var.base_domain}']"
+    heartbeat_sms_number = var.heartbeat_sms_number
   }
 }
 
