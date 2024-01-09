16f772ec44e6fe25d48a95ca06ae927bb4f476b6
Ben Larabie <ben.larabie@cds-snc.ca>
2024-01-09 12:57:28 -0500

    Setting DNS certs

--- aws/system_status_static_site/variables.tf
+++ aws/system_status_static_site/variables.tf
@@ -3,3 +3,11 @@ variable "route_53_zone_arn" {
   description = "Used by the scratch environment to reference cdssandbox in staging"
   default     = "/hostedzone/Z04028033PLSHVOO9ZJ1Z"
 }
+
+variable "status_cert_created" {
+  type        = bool
+  description = "This flag must be set to false on a new environment creation in production, so that we can generate the certificate first."
+  default     = false
+}
+
+
