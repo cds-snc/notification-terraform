36b0e6a4a1ac30b5c7e7c413c5c2a3fdfc3a8a16
Ben Larabie <ben.larabie@cds-snc.ca>
2024-01-10 10:56:15 -0500

    update

---
+++ aws/dns/dev.notification.internal.tf
@@ -0,0 +1,20 @@
+resource "aws_route53_zone" "dev_internal_dns" {
+  count = var.env == "dev" ? 1 : 0
+  name  = "dev.notification.internal"
+
+  vpc {
+    vpc_id = var.vpc_id
+  }
+}
+
+# Dev Tools DNS
+
+resource "aws_route53_record" "pgadmin_internal_CNAME" {
+  count   = var.env == "dev" ? 1 : 0
+  zone_id = aws_route53_zone.dev_internal_dns[0].zone_id
+  name    = "pgadmin.dev.notification.internal"
+  type    = "CNAME"
+  ttl     = "60"
+  records = ["internal-a83839c0acb264ff7b00f69e94dc3ca3-2136659269.ca-central-1.elb.amazonaws.com"]
+}
+
