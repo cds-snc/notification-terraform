53ce8855893aa1a88e9531f2897a62a0e078be19
Pat Heard <patrick.heard@cds-snc.ca>
2024-01-10 11:32:44 -0500

    fix: send client VPN connection logs to Sentinel
    Update the Sentinel Forwarder subscriptions so that all
    client VPN connection logs are sent to Sentinel.

--- aws/common/vpn.tf
+++ aws/common/vpn.tf
@@ -3,7 +3,7 @@
 # access to the private subnets.
 #
 module "vpn" {
-  source = "github.com/cds-snc/terraform-modules//client_vpn?ref=v7.4.3"
+  source = "github.com/cds-snc/terraform-modules//client_vpn?ref=v9.0.3"
 
   endpoint_name   = "private-subnets"
   access_group_id = var.client_vpn_access_group_id
