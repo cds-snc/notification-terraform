8d3ab615e50c277d869a9915c829ceefe9489946
Ben Larabie <ben.larabie@cds-snc.ca>
2024-01-11 08:59:50 -0500

    graylog dns

--- aws/dns/dev.notification.internal.tf
+++ aws/dns/dev.notification.internal.tf
@@ -18,3 +18,12 @@ resource "aws_route53_record" "pgadmin_internal_CNAME" {
   records = ["internal-a83839c0acb264ff7b00f69e94dc3ca3-2136659269.ca-central-1.elb.amazonaws.com"]
 }
 
+resource "aws_route53_record" "graylog_internal_CNAME" {
+  count   = var.env == "dev" ? 1 : 0
+  zone_id = aws_route53_zone.dev_internal_dns[0].zone_id
+  name    = "graylog.dev.notification.internal"
+  type    = "CNAME"
+  ttl     = "60"
+  records = ["internal-a4ba5e6079eb74b2398bbe87b08e6346-596142408.ca-central-1.elb.amazonaws.com"]
+}
+
