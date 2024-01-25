102998ae785dd5f70e46073b5cd6809d7fc85762
Ben Larabie <ben.larabie@cds-snc.ca>
2024-01-25 14:39:47 -0500

    Dev internal DNS w/nginx

--- aws/dns/secrets.tf
+++ aws/dns/secrets.tf
@@ -4,7 +4,7 @@ resource "aws_secretsmanager_secret" "internal_dns_cert_base64" {
 
 resource "aws_secretsmanager_secret_version" "internal_dns_cert_base64" {
   secret_id     = aws_secretsmanager_secret.internal_dns_cert_base64.id
-  secret_string = base64encode(tls_self_signed_cert.internal_dns.cert_pem)
+  secret_string = tls_self_signed_cert.internal_dns.cert_pem
 }
 
 resource "aws_secretsmanager_secret" "internal_dns_key_base64" {
@@ -13,5 +13,5 @@ resource "aws_secretsmanager_secret" "internal_dns_key_base64" {
 
 resource "aws_secretsmanager_secret_version" "internal_dns_key_base64" {
   secret_id     = aws_secretsmanager_secret.internal_dns_key_base64.id
-  secret_string = base64encode(tls_private_key.internal_dns.private_key_pem)
+  secret_string = tls_private_key.internal_dns.private_key_pem
 }
--- aws/eks/outputs.tf
+++ aws/eks/outputs.tf
@@ -84,3 +84,8 @@ output "karpenter_instance_profile" {
 output "quicksight_security_group_id" {
   value = aws_security_group.quicksight.id
 }
+
+# EKS CSI
+output "eks_csi_role_arn" {
+  value = aws_iam_role.secrets_csi.arn
+}
