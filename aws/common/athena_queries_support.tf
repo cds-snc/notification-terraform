328d8c2e242f20af647549059e93d2113d1a3bd3
Stephen Astels <sastels@gmail.com>
2024-02-26 16:02:06 -0500

    fix

--- aws/common/athena_queries_support.tf
+++ aws/common/athena_queries_support.tf
@@ -3,5 +3,5 @@ resource "aws_athena_named_query" "find_blocked_ips" {
   description = "TF: find ips that Notify has recently blocked"
   workgroup   = aws_athena_workgroup.primary.name
   database    = aws_athena_database.notification_athena.name
-  query       = templatefile("${path.module}/sql/find_blocked_ips.sql.tmpl")
+  query       = templatefile("${path.module}/sql/find_blocked_ips.sql.tmpl", {})
 }
