68ba53e025f234fb5774eb3c1bfdc1d6a7590a86
Stephen Astels <sastels@gmail.com>
2024-02-26 15:25:51 -0500

    add a new query on WAF logs

---
+++ aws/common/athena_queries_support.tf
@@ -0,0 +1,7 @@
+resource "aws_athena_named_query" "find_blocked_ips" {
+  name        = "WAF: find blocked ips"
+  description = "TF: find ips that Notify has recently blocked"
+  workgroup   = aws_athena_workgroup.primary.name
+  database    = aws_athena_database.notification_athena.name
+  query       = templatefile("${path.module}/sql/find_blocked_ips.sql.tmpl")
+}
