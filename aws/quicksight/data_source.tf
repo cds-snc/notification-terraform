3a05c8ec8d582aaca70077a91801ed3853b5c4ca
Jimmy Royer <jimleroyer@gmail.com>
2024-03-07 11:12:27 -0500

    Define an etag on the manifest file so it updates on changes

--- aws/quicksight/data_source.tf
+++ aws/quicksight/data_source.tf
@@ -39,6 +39,7 @@ resource "aws_s3_object" "manifest_file" {
   key    = "quicksight/s3-manifest-sms-usage.json"                  # replace with desired object key
   source = "aws/quicksight/s3-manifests.s3-manifest-sms-usage.json" # replace with path to local file
   acl    = "private"
+  etag   = filemd5("aws/quicksight/s3-manifests.s3-manifest-sms-usage.json")
 }
 
 resource "aws_quicksight_data_source" "s3_sms_usage" {
