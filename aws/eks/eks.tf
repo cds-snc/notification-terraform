9da77f88e1dfaf10dc52fd088ecaee88fdb640ff
Ben Larabie <ben.larabie@cds-snc.ca>
2024-03-15 10:28:05 -0400

    Switching github arc to PR BOT app

--- aws/eks/eks.tf
+++ aws/eks/eks.tf
@@ -12,8 +12,8 @@ resource "aws_eks_cluster" "notification-canada-ca-eks-cluster" {
   vpc_config {
 
     # Setting this explicitly for now, until manifests release is in
-    endpoint_private_access = false
-    endpoint_public_access  = true
+    endpoint_private_access = true
+    endpoint_public_access  = false
 
     # tfsec:ignore:AWS068 EKS cluster should not have open CIDR range for public access
     # Will be tackled in the future https://github.com/cds-snc/notification-terraform/issues/203
--- aws/eks/secrets.tf
+++ aws/eks/secrets.tf
@@ -7,11 +7,30 @@ resource "aws_secretsmanager_secret_version" "nginx_target_group_arn" {
   secret_string = aws_alb_target_group.internal_nginx_http.arn
 }
 
-resource "aws_secretsmanager_secret" "gha_arc_pat" {
-  name = "GITHUB_ARC_PAT"
+resource "aws_secretsmanager_secret" "pr_bot_app_id" {
+  name = "PR_BOT_APP_ID"
 }
 
-resource "aws_secretsmanager_secret_version" "gha_arc_pat" {
-  secret_id     = aws_secretsmanager_secret.gha_arc_pat.id
-  secret_string = var.gha_arc_pat
+resource "aws_secretsmanager_secret_version" "pr_bot_app_id" {
+  secret_id     = aws_secretsmanager_secret.pr_bot_app_id.id
+  secret_string = var.pr_bot_app_id
 }
+
+resource "aws_secretsmanager_secret" "pr_bot_private_key" {
+  name = "PR_BOT_PRIVATE_KEY"
+}
+
+resource "aws_secretsmanager_secret_version" "pr_bot_private_key" {
+  secret_id     = aws_secretsmanager_secret.pr_bot_private_key.id
+  secret_string = var.pr_bot_private_key
+}
+
+resource "aws_secretsmanager_secret" "pr_bot_installation_id" {
+  name = "PR_BOT_INSTALLATION_ID"
+}
+
+resource "aws_secretsmanager_secret_version" "pr_bot_installation_id" {
+  secret_id     = aws_secretsmanager_secret.pr_bot_installation_id.id
+  secret_string = var.pr_bot_installation_id
+}
+
