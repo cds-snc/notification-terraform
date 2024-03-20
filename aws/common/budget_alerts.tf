928ba954c019b8c5a24bf47e58b32ae613caf564
Michael Pond <michael.pond@cds-snc.ca>
2024-03-20 15:10:41 -0400

    Adjusting budget alerts TF code to just use slack rather than emails -- and added the webhooks for DEV so that notifications show up on dev as well

--- aws/common/budget_alerts.tf
+++ aws/common/budget_alerts.tf
@@ -1,6 +1,5 @@
 
 resource "aws_budgets_budget" "notify_global" {
-  count        = var.env != "dev" ? 1 : 0
   name         = "notify-global-budget"
   budget_type  = "COST"
   limit_amount = var.account_budget_limit
@@ -21,32 +20,5 @@ resource "aws_budgets_budget" "notify_global" {
     threshold_type            = "PERCENTAGE"
     notification_type         = "ACTUAL"
     subscriber_sns_topic_arns = [aws_sns_topic.notification-canada-ca-alert-general.arn]
-  }
-}
-
-# Dev is not connected to slack, so we still need to have an email notification set up on dev
-
-resource "aws_budgets_budget" "notify_global_dev" {
-  count        = var.env == "dev" ? 1 : 0
-  name         = "notify-global-dev-budget"
-  budget_type  = "COST"
-  limit_amount = var.account_budget_limit
-  limit_unit   = "USD"
-  time_unit    = "MONTHLY"
-
-  notification {
-    comparison_operator        = "GREATER_THAN"
-    threshold                  = 100
-    threshold_type             = "PERCENTAGE"
-    notification_type          = "FORECASTED"
-    subscriber_email_addresses = var.account_budget_alert_emails
-  }
-
-  notification {
-    comparison_operator        = "GREATER_THAN"
-    threshold                  = 80
-    threshold_type             = "PERCENTAGE"
-    notification_type          = "ACTUAL"
-    subscriber_email_addresses = var.account_budget_alert_emails
   }
 }
