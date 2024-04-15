9af788576f10ce24b8cff99f265b0ab8efec6373
Ben Larabie <ben.larabie@cds-snc.ca>
2024-04-15 15:04:33 -0400

    Switching to module

--- aws/ecr/iam.tf
+++ aws/ecr/iam.tf
@@ -1,34 +1,24 @@
-resource "aws_iam_role" "github_docker_push" {
-  name = "github_docker_push"
-
-  # Terraform's "jsonencode" function converts a
-  # Terraform expression result to valid JSON syntax.
-  assume_role_policy = <<ROLE
-{
-  "Version": "2012-10-17",
-  "Statement": [
-      {
-          "Sid": "",
-          "Effect": "Allow",
-          "Principal": {
-              "Federated": "arn:aws:iam::283582579564:oidc-provider/token.actions.githubusercontent.com"
-          },
-          "Action": "sts:AssumeRoleWithWebIdentity",
-          "Condition": {
-              "StringLike": {
-                  "token.actions.githubusercontent.com:sub": "repo:cds-snc/notification-terraform:ref:refs/heads/main"
-              }
-          }
-      }
+module "oidc" {
+  source            = "github.com/cds-snc/terraform-modules?ref=v2.0.1//gh_oidc_role"
+  billing_tag_key   = "CostCentre"
+  billing_tag_value = "notification-canada-ca-${var.env}"
+  oidc_exists       = true
+  roles = [
+    {
+      name : "github_docker_push"
+      repo_name : "notification-terraform"
+      claim : "ref:refs/heads/main"
+    }
   ]
 }
-ROLE
 
-}
 
 resource "aws_iam_role_policy" "github_docker_push" {
+
+  depends_on = [module.oidc]
+
   name = "github_docker_push"
-  role = aws_iam_role.github_docker_push.id
+  role = "github_docker_push"
 
   # Terraform's "jsonencode" function converts a
   # Terraform expression result to valid JSON syntax.
@@ -57,3 +47,5 @@ resource "aws_iam_role_policy" "github_docker_push" {
 }
 POLICY
 }
+
+
