6a6295a7bf60622496045261569e4a56251efbe2
Ben Larabie <ben.larabie@cds-snc.ca>
2024-04-19 13:56:03 -0400

    test

--- scripts/pullEnvFile.sh
+++ scripts/pullEnvFile.sh
@@ -3,6 +3,9 @@
 #   Used by GitHub Actions To Pull the Environment File from 1Password                            #  
 ###################################################################################################
 
+curl -o 1pass.deb https://downloads.1password.com/linux/debian/amd64/stable/1password-cli-amd64-latest.deb
+dpkg -i 1pass.deb
+
 echo "reading 1Password for TFVars - Staging"
 op read op://4eyyuwddp6w4vxlabrr2i2duxm/"TFVars - Staging"/notesPlain > .env
 echo "done."
