0a46a61d4b86c032d5df2b244153e9b52836cf15
Ben Larabie <ben.larabie@cds-snc.ca>
2024-04-19 14:26:27 -0400

    full thing maybe

--- scripts/pullEnvFile.sh
+++ scripts/pullEnvFile.sh
@@ -7,5 +7,5 @@ TARGET=$1
 
 pwd
 echo "reading 1Password for TFVars - Staging"
-op read op://4eyyuwddp6w4vxlabrr2i2duxm/"TFVars - Staging"/notesPlain > staging.tfvars
+op read op://4eyyuwddp6w4vxlabrr2i2duxm/"TFVars - Staging"/notesPlain > TARGET=$1
 echo "done."
