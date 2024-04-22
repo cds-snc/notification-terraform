9b27183aac0595b22bc355902a32d10d2ab526d5
Stephen Astels <sastels@gmail.com>
2024-04-22 11:46:51 -0400

    fix copypasta

--- aws/pinpoint_to_sqs_sms_callbacks/pools.tf
+++ aws/pinpoint_to_sqs_sms_callbacks/pools.tf
@@ -1,6 +1,6 @@
 resource "null_resource" "create_pools" {
   count      = var.bootstrap ? 1 : 0
-  depends_on = [aws_iam_role.pinpoint_logs.arn, aws_cloudwatch_log_group.pinpoint_deliveries, aws_cloudwatch_log_group.pinpoint_deliveries_failures]
+  depends_on = [aws_iam_role.pinpoint_logs, aws_cloudwatch_log_group.pinpoint_deliveries, aws_cloudwatch_log_group.pinpoint_deliveries_failures]
 
   provisioner "local-exec" {
     command = "scripts/create_pools.sh ${aws_iam_role.pinpoint_logs.arn} ${aws_cloudwatch_log_group.pinpoint_deliveries.arn} ${aws_cloudwatch_log_group.pinpoint_deliveries_failures.arn}"
