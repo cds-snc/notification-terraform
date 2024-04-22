5da89f3168b2673e2777c1c48302013f2bae1620
Stephen Astels <sastels@gmail.com>
2024-04-22 14:38:36 -0400

    move script

--- aws/pinpoint_to_sqs_sms_callbacks/pools.tf
+++ aws/pinpoint_to_sqs_sms_callbacks/pools.tf
@@ -1,8 +1,7 @@
-# TODO figure out where the script is located during CI
-# resource "null_resource" "create_pools" {
-#   depends_on = [aws_iam_role.pinpoint_logs, aws_cloudwatch_log_group.pinpoint_deliveries, aws_cloudwatch_log_group.pinpoint_deliveries_failures]
+resource "null_resource" "create_pools" {
+  depends_on = [aws_iam_role.pinpoint_logs, aws_cloudwatch_log_group.pinpoint_deliveries, aws_cloudwatch_log_group.pinpoint_deliveries_failures]
 
-#   provisioner "local-exec" {
-#     command = "${path.root}/scripts/create_pinpoint_pools.sh ${aws_iam_role.pinpoint_logs.arn} ${aws_cloudwatch_log_group.pinpoint_deliveries.arn} ${aws_cloudwatch_log_group.pinpoint_deliveries_failures.arn}"
-#   }
-# }
+  provisioner "local-exec" {
+    command = "./create_pinpoint_pools.sh ${aws_iam_role.pinpoint_logs.arn} ${aws_cloudwatch_log_group.pinpoint_deliveries.arn} ${aws_cloudwatch_log_group.pinpoint_deliveries_failures.arn}"
+  }
+}
