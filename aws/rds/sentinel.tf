cf92b5ab00ae6e8fcc8b564019f62f35049ab5f7
Pat Heard <patrick.heard@cds-snc.ca>
2024-04-24 19:33:32 +0000

    fix: resource reference

--- aws/rds/sentinel.tf
+++ aws/rds/sentinel.tf
@@ -9,7 +9,7 @@ resource "aws_lambda_permission" "sentinel_forwarder_cloudwatch_log_subscription
   action        = "lambda:InvokeFunction"
   function_name = var.sentinel_forwarder_cloudwatch_lambda_name
   principal     = "logs.${var.region}.amazonaws.com"
-  source_arn    = "${aws_cloudwatch_log_group.logs_exports.arn}:*"
+  source_arn    = "${aws_cloudwatch_log_group.logs_exports[0].arn}:*"
   statement_id  = "AllowExecutionFromCloudWatchLogs-sentinel-cloud-watch-forwarder-postgres"
 }
 
@@ -17,7 +17,7 @@ resource "aws_cloudwatch_log_subscription_filter" "postgresql_dangerous_queries"
   count = var.sentinel_forwarder_cloudwatch_lambda_name != null && var.env != "production" ? 1 : 0
 
   name            = "Postgresql dangerous queries"
-  log_group_name  = aws_cloudwatch_log_group.logs_exports.arn
+  log_group_name  = aws_cloudwatch_log_group.logs_exports[0].arn
   filter_pattern  = "[(w1=\"*${join("*\" || w1=\"*", concat(local.postgres_dangerous_queries, local.postgres_dangerous_queries_lower))}*\")]"
   destination_arn = var.sentinel_forwarder_cloudwatch_lambda_arn
   distribution    = "Random"
