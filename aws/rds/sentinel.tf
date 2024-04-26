e837f61ddd59c84f0dc211cdfcd5388ee93a5833
Pat Heard <patrick.heard@cds-snc.ca>
2024-04-26 08:21:34 -0400

    feat: send potentially dangerous queries to Sentinel (#1264)
    Update the Sentinel forwarder to send potentially dangerous SQL
    queries.  These will be used to create alarms.

---
+++ aws/rds/sentinel.tf
@@ -0,0 +1,24 @@
+locals {
+  postgres_dangerous_queries       = ["ALTER", "CREATE", "DELETE", "DROP", "GRANT", "REVOKE", "TRUNCATE"]
+  postgres_dangerous_queries_lower = [for sql in local.postgres_dangerous_queries : lower(sql)]
+}
+
+resource "aws_lambda_permission" "sentinel_forwarder_cloudwatch_log_subscription" {
+  count = var.sentinel_forwarder_cloudwatch_lambda_name != null && var.env != "production" ? 1 : 0
+
+  action        = "lambda:InvokeFunction"
+  function_name = var.sentinel_forwarder_cloudwatch_lambda_name
+  principal     = "logs.${var.region}.amazonaws.com"
+  source_arn    = "${aws_cloudwatch_log_group.logs_exports[0].arn}:*"
+  statement_id  = "AllowExecutionFromCloudWatchLogs-sentinel-cloud-watch-forwarder-postgres"
+}
+
+resource "aws_cloudwatch_log_subscription_filter" "postgresql_dangerous_queries" {
+  count = var.sentinel_forwarder_cloudwatch_lambda_name != null && var.env != "production" ? 1 : 0
+
+  name            = "Postgresql dangerous queries"
+  log_group_name  = aws_cloudwatch_log_group.logs_exports[0].arn
+  filter_pattern  = "[(w1=\"*${join("*\" || w1=\"*", concat(local.postgres_dangerous_queries, local.postgres_dangerous_queries_lower))}*\")]"
+  destination_arn = var.sentinel_forwarder_cloudwatch_lambda_arn
+  distribution    = "Random"
+}
--- env/production/rds/terragrunt.hcl
+++ env/production/rds/terragrunt.hcl
@@ -26,4 +26,7 @@ inputs = {
   vpc_private_subnets       = dependency.common.outputs.vpc_private_subnets
   sns_alert_general_arn     = dependency.common.outputs.sns_alert_general_arn
   rds_database_name         = "NotificationCanadaCaproduction"
+
+  sentinel_forwarder_cloudwatch_lambda_arn  = dependency.eks.outputs.sentinel_forwarder_cloudwatch_lambda_arn
+  sentinel_forwarder_cloudwatch_lambda_name = dependency.eks.outputs.sentinel_forwarder_cloudwatch_lambda_name
 }
