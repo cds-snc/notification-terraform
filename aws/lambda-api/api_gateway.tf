fce5488f1832c5932ad90055151f34783a6e8694
Ben Larabie <ben.larabie@cds-snc.ca>
2024-05-07 13:11:14 -0400

    Removing the duplication of code

--- aws/lambda-api/api_gateway.tf
+++ aws/lambda-api/api_gateway.tf
@@ -191,33 +191,3 @@ resource "aws_api_gateway_method_settings" "api_settings" {
     cache_data_encrypted = true
   }
 }
-
-# Allow API Gateway to push logs to CloudWatch
-resource "aws_api_gateway_account" "main" {
-  cloudwatch_role_arn = aws_iam_role.main.arn
-}
-
-resource "aws_iam_role" "main" {
-  name               = "api-gateway-logs-role"
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Sid": "",
-      "Effect": "Allow",
-      "Principal": {
-        "Service": "apigateway.amazonaws.com"
-      },
-      "Action": "sts:AssumeRole"
-    }
-  ]
-}
-EOF
-
-}
-
-resource "aws_iam_role_policy_attachment" "main" {
-  role       = aws_iam_role.main.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonAPIGatewayPushToCloudWatchLogs"
-}
