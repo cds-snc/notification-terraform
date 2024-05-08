995cc8a821c390561399a80cb30980ac1aaa76a1
Ben Larabie <ben.larabie@cds-snc.ca>
2024-05-08 11:02:12 -0400

    Fixing prod sentinel rds forwarding

--- aws/rds/rds.tf
+++ aws/rds/rds.tf
@@ -168,7 +168,7 @@ resource "aws_rds_cluster" "notification-canada-ca" {
 
 # Holds the exported postgresql logs
 resource "aws_cloudwatch_log_group" "logs_exports" {
-  count = var.env != "production" ? 1 : 0
+  count = var.enable_sentinel_forwarding
   name  = "/aws/rds/cluster/notification-canada-ca-${var.env}-cluster/postgresql"
   #checkov:skip=CKV_AWS_338:The short retention is required to respect Notify's privacy policy
   retention_in_days = 3
