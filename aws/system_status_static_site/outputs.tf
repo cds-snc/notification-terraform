8b4f8cd79ce325fa3466d948568c9938022a9c71
Ben Larabie <ben.larabie@cds-snc.ca>
2024-06-06 13:30:27 -0400

    First step of restoring old static site

--- aws/system_status_static_site/outputs.tf
+++ aws/system_status_static_site/outputs.tf
@@ -1,14 +1,14 @@
 output "system_status_static_site_bucket_name" {
-  value       = module.system_status_static_site.s3_bucket_arn
+  value       = var.status_cert_created ? module.system_status_static_site[0].s3_bucket_arn : ""
   description = "name of the s3 bucket for the system status static site"
 }
 
 output "system_status_static_site_bucket_id" {
-  value       = module.system_status_static_site.s3_bucket_id
+  value       = var.status_cert_created ? module.system_status_static_site[0].s3_bucket_id : ""
   description = "id of the s3 bucket for the system status static site"
 }
 
 output "system_status_static_site_bucket_region" {
-  value       = module.system_status_static_site.s3_bucket_region
+  value       = var.status_cert_created ? module.system_status_static_site[0].s3_bucket_region : ""
   description = "aws region of the s3 bucket for the system status static site"
 }
