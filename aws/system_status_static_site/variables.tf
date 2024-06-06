cef68dae373df74211935cc06f385dbbefb14f9a
Ben Larabie <ben.larabie@cds-snc.ca>
2024-06-06 13:45:17 -0400

    First step of restoring old static site (#1356)

--- aws/system_status_static_site/variables.tf
+++ aws/system_status_static_site/variables.tf
@@ -4,6 +4,12 @@ variable "route_53_zone_arn" {
   default     = "/hostedzone/Z04028033PLSHVOO9ZJ1Z"
 }
 
+variable "status_cert_created" {
+  type        = bool
+  description = "This flag must be set to false on a new environment creation in production, so that we can generate the certificate first."
+  default     = false
+}
+
 variable "force_destroy_s3" {
   type        = bool
   description = "Force destroy the s3 bucket. Not advised for production."
