06b2cd31a516cf2e4fe9a3ce245bdae736c40b66
Jimmy Royer <jimleroyer@gmail.com>
2024-06-12 11:42:47 -0400

    Split the admin+api 500 errors query in 2

--- aws/eks/cloudwatch_queries.tf
+++ aws/eks/cloudwatch_queries.tf
@@ -186,9 +186,9 @@ QUERY
 
 ################################ UNSORTED YET #################################
 
-resource "aws_cloudwatch_query_definition" "admin-api-50X-errors" {
+resource "aws_cloudwatch_query_definition" "admin-50X-errors" {
   count = var.cloudwatch_enabled ? 1 : 0
-  name  = "API / ADMIN & API - 50X errors"
+  name  = "Admin / 50X errors"
 
   log_group_names = [
     local.eks_application_log_group
@@ -196,7 +196,24 @@ resource "aws_cloudwatch_query_definition" "admin-api-50X-errors" {
 
   query_string = <<QUERY
 fields @timestamp, log, kubernetes.container_name as app, kubernetes.pod_name as pod_name, @logStream
-| filter kubernetes.container_name like /admin|api/
+| filter kubernetes.container_name like /admin/
+| filter @message like /HTTP\/\d+\.\d+\\" 50\d/
+| sort @timestamp desc
+| limit 20
+QUERY
+}
+
+resource "aws_cloudwatch_query_definition" "api-50X-errors" {
+  count = var.cloudwatch_enabled ? 1 : 0
+  name  = "API / 50X errors"
+
+  log_group_names = [
+    local.eks_application_log_group
+  ]
+
+  query_string = <<QUERY
+fields @timestamp, log, kubernetes.container_name as app, kubernetes.pod_name as pod_name, @logStream
+| filter kubernetes.container_name like /api/
 | filter @message like /HTTP\/\d+\.\d+\\" 50\d/
 | sort @timestamp desc
 | limit 20
@@ -276,7 +293,7 @@ QUERY
 
 resource "aws_cloudwatch_query_definition" "gh-arc-errors" {
   count = var.cloudwatch_enabled ? 1 : 0
-  name  = "CICD / ARC Errors"
+  name  = "CICD / GitHub ARC Errors"
 
   log_group_names = [
     local.eks_application_log_group
