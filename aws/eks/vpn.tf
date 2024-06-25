88913030e8f0bd83e301a3cd068a305c82613291
Ben Larabie <ben.larabie@cds-snc.ca>
2024-06-25 14:56:12 -0400

    bumping version on vpn and simple website module

--- aws/eks/vpn.tf
+++ aws/eks/vpn.tf
@@ -3,7 +3,7 @@
 # access to the private subnets.
 #
 module "vpn" {
-  source = "github.com/cds-snc/terraform-modules//client_vpn?ref=v9.5.0"
+  source = "github.com/cds-snc/terraform-modules//client_vpn?ref=v9.5.3"
 
   endpoint_name         = "private-subnets"
   access_group_id       = var.client_vpn_access_group_id
@@ -29,7 +29,7 @@ module "vpn" {
 
 # GHA VPN
 module "gha_vpn" {
-  source = "github.com/cds-snc/terraform-modules//client_vpn?ref=v9.5.0"
+  source = "github.com/cds-snc/terraform-modules//client_vpn?ref=v9.5.3"
 
   endpoint_name   = "gha-vpn"
   access_group_id = var.client_vpn_access_group_id
