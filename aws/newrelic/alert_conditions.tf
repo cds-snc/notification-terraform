655e3f8dbf51c2a82baa1888c10bdbdd308ac5dc
Michael Pond <michael.pond@cds-snc.ca>
2024-07-03 13:02:59 -0400

    adding aert condition

--- aws/newrelic/alert_conditions.tf
+++ aws/newrelic/alert_conditions.tf
@@ -1,9 +1,9 @@
 resource "newrelic_nrql_alert_condition" "tf_lambda_api_errors_count_anomaly_unexpected_errors" {
-  account_id = 2691974
-  policy_id = 2801728
-  type = "baseline"
-  name = "Terraform - [Lambda API] Errors count anomaly (Unexpected Errors)"
-  enabled = true
+  account_id                   = 2691974
+  policy_id                    = 2801728
+  type                         = "baseline"
+  name                         = "Terraform - [Lambda API] Errors count anomaly (Unexpected Errors)"
+  enabled                      = true
   violation_time_limit_seconds = 86400
 
   nrql {
@@ -11,21 +11,21 @@ resource "newrelic_nrql_alert_condition" "tf_lambda_api_errors_count_anomaly_une
   }
 
   critical {
-    operator = "above"
-    threshold = 6
-    threshold_duration = 300
+    operator              = "above"
+    threshold             = 6
+    threshold_duration    = 300
     threshold_occurrences = "all"
   }
 
   warning {
-    operator = "above"
-    threshold = 3
-    threshold_duration = 300
+    operator              = "above"
+    threshold             = 3
+    threshold_duration    = 300
     threshold_occurrences = "all"
   }
-  fill_option = "none"
+  fill_option        = "none"
   aggregation_window = 60
   aggregation_method = "event_flow"
-  aggregation_delay = 300
+  aggregation_delay  = 300
   baseline_direction = "upper_and_lower"
 }
