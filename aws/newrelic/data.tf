48a73ba396f556b83d2285fb0bb0aa64df62b791
Michael Pond <michael.pond@cds-snc.ca>
2024-07-11 16:03:33 -0400

    Adding all Lambda Alerts and Entity Data

--- aws/newrelic/data.tf
+++
@@ -1,7 +0,0 @@
-# data "newrelic_entity" "notification-admin-script-staging" {
-#   name = "notification-admin-script"
-#   provider = newrelic
-#   domain = "BROWSER" # or BROWSER, INFRA, MOBILE, SYNTH, depending on your entity's domain
-#   type = "APPLICATION"
-# }
-
