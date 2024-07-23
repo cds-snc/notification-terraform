1af144c1796b5eeea1875a48ae9d84152725dfee
Ben Larabie <ben.larabie@cds-snc.ca>
2024-07-23 09:47:25 -0400

    formatting

--- aws/newrelic/aws_integration.tf
+++ aws/newrelic/aws_integration.tf
@@ -1,6 +1,6 @@
 module "newrelic-aws-cloud-integrations" {
-  count   = var.env == "staging" ? 1 : 0 
-  source  = "github.com/newrelic/terraform-provider-newrelic//examples/modules/cloud-integrations/aws"
+  count  = var.env == "staging" ? 1 : 0
+  source = "github.com/newrelic/terraform-provider-newrelic//examples/modules/cloud-integrations/aws"
 
   newrelic_account_id     = var.new_relic_account_id
   newrelic_account_region = "US"
