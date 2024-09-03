b9c4c66342ccd0bba28a7a8189b62b039d280782
Stephen Astels <sastels@gmail.com>
2024-09-03 13:53:58 -0400

    fix more alarms

--- aws/pinpoint_to_sqs_sms_callbacks/cloudwatch_alarms.tf
+++ aws/pinpoint_to_sqs_sms_callbacks/cloudwatch_alarms.tf
@@ -97,7 +97,6 @@ resource "aws_cloudwatch_metric_alarm" "total-sms-spending-warning" {
       namespace   = "AWS/SNS"
       period      = 300
       stat        = "Maximum"
-      unit        = "Count"
     }
   }
 
@@ -108,7 +107,6 @@ resource "aws_cloudwatch_metric_alarm" "total-sms-spending-warning" {
       namespace   = "AWS/SMSVoice"
       period      = 300
       stat        = "Maximum"
-      unit        = "Count"
     }
   }
 }
@@ -137,7 +135,6 @@ resource "aws_cloudwatch_metric_alarm" "total-sms-spending-critical" {
       namespace   = "AWS/SNS"
       period      = 300
       stat        = "Maximum"
-      unit        = "Count"
     }
   }
 
@@ -148,7 +145,6 @@ resource "aws_cloudwatch_metric_alarm" "total-sms-spending-critical" {
       namespace   = "AWS/SMSVoice"
       period      = 300
       stat        = "Maximum"
-      unit        = "Count"
     }
   }
 }
@@ -218,7 +214,6 @@ resource "aws_cloudwatch_metric_alarm" "pinpoint-sms-success-rate-critical" {
       namespace   = aws_cloudwatch_log_metric_filter.pinpoint-sms-successes[0].metric_transformation[0].namespace
       period      = 60 * 60 * 12
       stat        = "Sum"
-      unit        = "Count"
     }
   }
 
@@ -229,7 +224,6 @@ resource "aws_cloudwatch_metric_alarm" "pinpoint-sms-success-rate-critical" {
       namespace   = aws_cloudwatch_log_metric_filter.pinpoint-sms-failures[0].metric_transformation[0].namespace
       period      = 60 * 60 * 12
       stat        = "Sum"
-      unit        = "Count"
     }
   }
 }
