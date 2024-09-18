69f9ad6f4f3f7e861ffd95d959117bea35666101
Michael Pond <michael.pond@cds-snc.ca>
2024-09-18 10:51:38 -0400

    fixing newrelic vars

--- aws/newrelic/variables.tf
+++ aws/newrelic/variables.tf
@@ -1,15 +1,3 @@
-variable "new_relic_api_key" {
-  type        = string
-  description = "New Relic API Key"
-  sensitive   = true
-}
-
-variable "new_relic_slack_webhook_url" {
-  type        = string
-  description = "Slack Webhook URL"
-  sensitive   = true
-}
-
 variable "newrelic_account_region" {
   type    = string
   default = "US"
@@ -24,10 +12,4 @@ variable "aws_config_recorder_name" {
   type        = string
   description = "The name of the AWS Configuration Recorder"
   default     = "aws-controltower-BaselineConfigRecorder"
-}
-
-variable "enable_new_relic" {
-  type        = bool
-  description = "Set whether or not newrelic is enabled"
-  default     = true
 }
