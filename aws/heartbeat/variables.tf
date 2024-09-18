2ac3beca37519d9e708f8ea3a1a17d0fed9d6fe3
Michael Pond <michael.pond@cds-snc.ca>
2024-09-18 10:33:55 -0400

    fixing vars for heartbeat

--- aws/heartbeat/variables.tf
+++ aws/heartbeat/variables.tf
@@ -1,30 +1,3 @@
-variable "billing_tag_value" {
-  type        = string
-  description = "Identifies the billing code."
-}
-
-variable "heartbeat_api_key" {
-  sensitive   = true
-  type        = string
-  description = "Identifies the heartbeat api key."
-}
-
-variable "base_domain" {
-  sensitive   = true
-  type        = string
-  description = "Identifies the base url to trigger the heartbeat function with. This is a string in the secrets and parsed in the lambda"
-}
-
-variable "heartbeat_sms_number" {
-  type        = string
-  description = "Identifies the sms number to send hearbeats to"
-}
-
-variable "schedule_expression" {
-  type        = string
-  description = "This aws cloudwatch event rule scheule expression that specifies when the scheduler runs."
-}
-
 variable "sns_alert_warning_arn" {
   type = string
 }
@@ -44,11 +17,4 @@ variable "heartbeat_docker_tag" {
   type        = string
   description = "Set this to specify the image version"
   default     = "bootstrap"
-}
-
-variable "bootstrap" {
-  type        = bool
-  description = "Set this to true to use the bootstrap image"
-  default     = false
-
 }
