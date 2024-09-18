bb5526314b3e404832903f46d1dfcb790c035eda
Michael Pond <michael.pond@cds-snc.ca>
2024-09-18 10:45:41 -0400

    fixing vars for lambda-api

--- aws/lambda-api/variables.tf
+++ aws/lambda-api/variables.tf
@@ -2,11 +2,6 @@ variable "csv_upload_bucket_arn" {
   type = string
 }
 
-variable "new_relic_license_key" {
-  type      = string
-  sensitive = true
-}
-
 variable "vpc_private_subnets" {
   type = list(any)
 }
@@ -16,10 +11,6 @@ variable "eks_cluster_securitygroup" {
 }
 
 variable "firehose_waf_logs_iam_role_arn" {
-  type = string
-}
-
-variable "base_domain" {
   type = string
 }
 
@@ -53,20 +44,9 @@ variable "re_api_arn" {
   type        = string
 }
 
-variable "waf_secret" {
-  description = "secret the admin sends in the header so the WAF does not rate limit"
-  type        = string
-  sensitive   = true
-}
-
 variable "eks_application_log_group" {
   description = "log group of the k8s cluster applications"
   type        = string
-}
-
-variable "route53_zone_id" {
-  type        = string
-  description = "Used by the scratch environment to reference cdssandbox in staging"
 }
 
 variable "api_lambda_ecr_arn" {
@@ -78,11 +58,6 @@ variable "api_lambda_ecr_repository_url" {
   type        = string
   description = "Docker Repo URL for API Lambda from ECR TF Folder"
 }
-variable "bootstrap" {
-  description = "Boolean value to decide whether or not to build images"
-  type        = bool
-  default     = false
-}
 
 variable "database_read_only_proxy_endpoint" {
   type        = string
@@ -92,12 +67,6 @@ variable "database_read_only_proxy_endpoint" {
 variable "database_read_write_proxy_endpoint" {
   type        = string
   description = "Base read write endpoint for rds proxy"
-}
-
-variable "app_db_user_password" {
-  type        = string
-  sensitive   = true
-  description = "Password for rds cluster"
 }
 
 variable "alb_arn_suffix" {
