467c5e072ef6c473307d7e768fe004b6df55879f
Michael Pond <michael.pond@cds-snc.ca>
2024-09-18 11:12:03 -0400

    fixing vars for ses_receiving emails

--- aws/ses_receiving_emails/variables.tf
+++ aws/ses_receiving_emails/variables.tf
@@ -1,33 +1,3 @@
-variable "billing_tag_value" {
-  type        = string
-  description = "Identifies the billing code."
-}
-
-variable "notify_sending_domain" {
-  type        = string
-  description = "Sending domain for notify."
-}
-
-variable "sqs_region" {
-  type        = string
-  description = "SQS region"
-}
-
-variable "celery_queue_prefix" {
-  type        = string
-  description = "Celery queue prefix"
-}
-
-variable "gc_notify_service_email" {
-  type        = string
-  description = "Service email for GC Notify."
-}
-
-variable "schedule_expression" {
-  type        = string
-  description = "This aws cloudwatch event rule scheule expression that specifies when the scheduler runs."
-}
-
 variable "sns_alert_warning_arn_us_east_1" {
   type = string
 }
