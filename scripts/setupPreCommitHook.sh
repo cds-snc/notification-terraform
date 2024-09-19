f910d104da93dd2a69e31dc76cc3670cd611507a
Ben Larabie <ben.larabie@cds-snc.ca>
2024-09-19 08:30:50 -0400

    Doc

--- scripts/setupPreCommitHook.sh
+++ scripts/setupPreCommitHook.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # This is script is simply a shortcut to the pre-commit hook command that I always forget when I clone this repo.
+# The pre-commit hook in ./hooks will automatically run terraform fmt on all terraform files in the repo.
 git config set core.hooksPath ./hooks
