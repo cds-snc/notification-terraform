2911103178dc58db32b6c5c3a1175a0f9ec18072
Stephen Astels <sastels@gmail.com>
2024-09-23 11:10:25 -0400

    specify pool id when requesting phone number

--- scripts/manage_longcodes/drain_pool.sh
+++ scripts/manage_longcodes/drain_pool.sh
@@ -17,7 +17,7 @@ printf "                         WARNING!!!!\n"
 printf "  This will delete all but one long code from a Pinpoint pool!\n"
 printf "         You do not want to run this or production!\n"
 printf "\n------------------------------------------------------------\n"
-printf "Are you sure you want to continue?"
+printf "Are you sure you want to continue?\n"
 echo -n "If so, type 'drain'> "
 read -r check
 
@@ -26,15 +26,15 @@ if [ "$check" != "drain" ]; then
     exit 1
 fi
 
-if aws pinpoint-sms-voice-v2 describe-pools --pool-ids $1; then    
+if aws pinpoint-sms-voice-v2 describe-pools --pool-ids $1 > /dev/null 2>&1; then    
     numbers=$(aws pinpoint-sms-voice-v2 list-pool-origination-identities --pool-id $1 | jq -r ".OriginationIdentities[].OriginationIdentity")
     read -ra numbersArray <<< $numbers # Split the string into an array
     
     echo "Found ${#numbersArray[@]} numbers in pool $1. Releasing all but one."
     for number in ${numbersArray[@]:1}; do # Skip the first number - have to keep at least one number in the pool
         echo "Releasing $number..."
-        aws pinpoint-sms-voice-v2 disassociate-origination-identity --iso-country-code CA --pool-id $1 --origination-identity $number
-        aws pinpoint-sms-voice-v2 release-phone-number --phone-number-id $number
+        aws pinpoint-sms-voice-v2 disassociate-origination-identity --iso-country-code CA --pool-id $1 --origination-identity $number > /dev/null 2>&1
+        aws pinpoint-sms-voice-v2 release-phone-number --phone-number-id $number > /dev/null 2>&1
     done
 else
     echo "Pool $1 does not exist"
--- scripts/manage_longcodes/request_long_codes.sh
+++ scripts/manage_longcodes/request_long_codes.sh
@@ -19,20 +19,16 @@ if [ -z "$2" ]; then
     echo "Please provide the pool ID to assign the long codes to"
     exit 1
 fi
-if ! aws pinpoint-sms-voice-v2 describe-pools --pool-ids $2; then
-    echo "Pool $2 does not exist"
-    exit 1
-fi
+
 numberOfLongCodes=$1
 poolId=$2
-
 
 printf "\n------------------------------------------------------------\n"
 printf "                        WARNING!!!!\n"
 printf "  This will add new phone numbers to a Pinpoint pool\n"
 printf "    You might not want to run this in production!\n"
 printf "\n------------------------------------------------------------\n"
-printf "Are you sure you want to continue?"
+printf "Are you sure you want to continue?\n"
 echo -n "If so, type 'request'> "
 read -r check
 
@@ -41,24 +37,17 @@ if [ "$check" != "request" ]; then
     exit 1
 fi
 
+# if ! aws pinpoint-sms-voice-v2 describe-pools --pool-ids $poolId > /dev/null 2>&1; then
+#     echo "Pool $poolId does not exist"
+#     exit 1
+# fi
+
 for i in $(seq 1 $numberOfLongCodes); do
     number=$(aws pinpoint-sms-voice-v2 request-phone-number \
+      --pool-id $poolId \
       --iso-country-code CA --message-type TRANSACTIONAL \
       --number-capabilities SMS \
       --number-type LONG_CODE \
       | jq -r ".PhoneNumberId")
-
-    numberStatus="PENDING"
-    while [ "$numberStatus" != "\"ACTIVE\"" ]; do
-        echo "Waiting for number $number to become ACTIVE..."
-        sleep 1
-        numberStatus=$(aws pinpoint-sms-voice-v2 describe-phone-numbers \
-          --phone-number-ids $number \
-          | jq '.PhoneNumbers[0].Status')
-    done
-
-    aws pinpoint-sms-voice-v2 associate-origination-identity \
-      --pool-id $poolId \
-      --origination-identity $number \
-      --iso-country-code CA
+    echo "Requested $number"
 done
