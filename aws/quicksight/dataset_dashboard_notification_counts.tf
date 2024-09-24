a633b1c59d410b876271d49b8a7bb9f605ded9a5
wbanks <william.banks@cds-snc.ca>
2024-09-24 16:01:38 -0400

    Add dashboard notification count dataset
    - The dataset combines the notification, notification_history, and fact_notification_status tables so we can track and identify any discrepancies

---
+++ aws/quicksight/dataset_dashboard_notification_counts.tf
@@ -0,0 +1,127 @@
+# valid column types are [STRING INTEGER DECIMAL DATETIME BIT BOOLEAN JSON]
+
+resource "aws_quicksight_data_set" "dashboard-notification-counts" {
+    data_set_id = "dashboard-notification-counts"
+    name        = "Dashboard notification counts"
+    import_mode = "SPICE"
+
+  physical_table_map {
+    physical_table_map_id = "notification-counts"
+    custom_sql {
+      data_source_arn = aws_quicksight_data_source.rds.arn
+      name            = "dashboard-notification-counts"
+      sql_query       = <<EOF
+        WITH n AS (
+          SELECT
+              service_id,
+              COUNT(id) AS n_count,
+              DATE_PART('day', created_at) AS day
+          FROM notifications
+          WHERE
+              created_at >= '2024-09-01T00:00:00Z'
+              AND created_at <= '2024-09-30T00:00:00Z'
+              AND key_type <> 'test'
+          GROUP BY
+              DATE_PART('day', created_at),
+              service_id
+          ORDER BY day
+        ),
+        nh AS (
+          SELECT
+              service_id,
+              COUNT(id) AS nh_count,
+              DATE_PART('day', created_at) AS day
+          FROM notification_history
+          WHERE
+              created_at >= '2024-09-01T00:00:00Z'
+              AND created_at <= '2024-09-30T00:00:00Z'
+              AND key_type <> 'test'
+          GROUP BY
+              DATE_PART('day', created_at),
+              service_id
+          ORDER BY day, service_id
+        ),
+        ft AS (
+          SELECT
+              service_id,
+              SUM(notification_count) AS ft_count,
+              DATE_PART('day', bst_date) AS day
+          FROM ft_notification_status
+          WHERE
+              bst_date >= '2024-09-01T00:00:00Z'
+              AND bst_date <= '2024-09-30T00:00:00Z'
+              AND key_type <> 'test'
+          GROUP BY
+              DATE_PART('day', bst_date),
+              service_id
+          ORDER BY day
+        )
+        SELECT
+          COALESCE(n.service_id, nh.service_id, ft.service_id) AS service_id,
+          COALESCE(n.day, nh.day, ft.day) AS day,
+          n.n_count,
+          nh.nh_count,
+          ft.ft_count,
+          CASE
+            WHEN nh.nh_count IS NOT NULL AND ft.ft_count IS NOT NULL AND nh.nh_count <> ft.ft_count THEN '❌'
+            ELSE '✅'
+          END AS count_comparison
+        FROM n
+        FULL OUTER JOIN nh
+          ON n.day = nh.day AND n.service_id = nh.service_id
+        FULL OUTER JOIN ft
+          ON COALESCE(n.day, nh.day) = ft.day
+          AND COALESCE(n.service_id, nh.service_id) = ft.service_id
+        ORDER BY day, service_id
+      EOF
+
+      columns {
+        name = "service_id"
+        type = "STRING"
+      }
+      columns {
+        name = "day"
+        type = "INTEGER"
+      }
+      columns {
+        name = "n_count"
+        type = "INTEGER"
+      }
+      columns {
+        name = "nh_count"
+        type = "INTEGER"
+      }
+      columns {
+        name = "ft_count"
+        type = "INTEGER"
+      }
+      columns {
+        name = "count_comparison"
+        type = "INTEGER"
+      }
+    }
+  }
+  permissions {
+    actions   = local.dataset_viewer_permissions
+    principal = aws_quicksight_group.dataset_viewer.arn
+  }
+  permissions {
+    actions   = local.dataset_owner_permissions
+    principal = aws_quicksight_group.dataset_owner.arn
+  }
+}
+
+resource "aws_quicksight_refresh_schedule" "dashboard-notification-counts" {
+  data_set_id = "dashboard-notification-counts"
+  schedule_id = "schedule-dashboard-notification-counts"
+  depends_on  = [aws_quicksight_data_set.dashboard-notification-counts]
+
+  schedule {
+    refresh_type = "FULL_REFRESH"
+
+    schedule_frequency {
+      interval        = "DAILY"
+      time_of_the_day = "01:00"
+    }
+  }
+}
