9094a79c6b6c7a895922d93bdfc6961f090a79f5
Stephen Astels <sastels@gmail.com>
2024-09-25 11:12:43 -0400

    fix typo and tweak error messages

--- scripts/manage_longcodes/request_long_codes.sh
+++ scripts/manage_longcodes/request_long_codes.sh
@@ -8,15 +8,15 @@
 set -e
 
 if [ -z "$1" ]; then
-    echo "Please provide the number of long codes to request"
+    echo "Usage: . request_long_codes.sh numberOfLongCodes poolId"
+    exit 1
+fi
+if [ -z "$2" ]; then
+    echo "Usage: . request_long_codes.sh numberOfLongCodes poolId"
     exit 1
 fi
 if [ $1 -lt 1 ]; then
     echo "Number of long codes must be greater than 0"
-    exit 1
-fi
-if [ -z "$2" ]; then
-    echo "Please provide the pool ID to assign the long codes to"
     exit 1
 fi
 if ! aws pinpoint-sms-voice-v2 describe-pools --pool-ids $2 > /dev/null; then
@@ -46,7 +46,7 @@ for i in $(seq 1 $numberOfLongCodes); do
       --number-capabilities SMS \
       --number-type LONG_CODE \
       | jq -r ".PhoneNumberId")
-    if !aws pinpoint-sms-voice-v2 describe-phone-numbers --phone-number-ids $number > /dev/null; then
+    if ! aws pinpoint-sms-voice-v2 describe-phone-numbers --phone-number-ids $number > /dev/null; then
         echo "Failed to request number $number"
         continue
     else
