c7f56441f1d165fc8097dee9f465b6a0e33c777d
Jimmy Royer <jimleroyer@gmail.com>
2024-10-08 16:07:20 -0400

    Added pinpoint-carrier-dwell-times CWLI query

--- aws/pinpoint_to_sqs_sms_callbacks/cloudwatch_queries.tf
+++ aws/pinpoint_to_sqs_sms_callbacks/cloudwatch_queries.tf
@@ -1,3 +1,21 @@
+resource "aws_cloudwatch_query_definition" "pinpoint-carrier-dwell-times" {
+  count = var.cloudwatch_enabled ? 1 : 0
+  name  = "SMS (Pinpoint) / Carrier dwell times"
+
+  log_group_names = [
+    aws_cloudwatch_log_group.pinpoint_deliveries.name,
+    aws_cloudwatch_log_group.pinpoint_deliveries_failures.name,
+  ]
+
+  query_string = <<QUERY
+stats avg((eventTimestamp - messageRequestTimestamp) / 1000 / 60) as Avg_carrier_time_minutes,
+count(*) as Number by carrierName as Carrier
+| filter isFinal
+| sort by Avg_carrier_time_minutes desc
+| limit 100
+QUERY
+}
+
 resource "aws_cloudwatch_query_definition" "pinpoint-logs" {
   count = var.cloudwatch_enabled ? 1 : 0
   name  = "SMS (Pinpoint) / Logs"
