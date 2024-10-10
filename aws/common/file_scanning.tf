43fd17dbe6f0b8572a1aedb3e461bebd3512273b
Steve Astels <sastels@users.noreply.github.com>
2024-10-10 15:13:40 -0400

    Move out a couple account ids (#1578)

--- aws/common/file_scanning.tf
+++ aws/common/file_scanning.tf
@@ -1,13 +1,9 @@
-locals {
-  scan_files_account = var.env == "production" ? "806545929748" : "127893201980"
-}
-
 module "s3_scan_objects" {
   source = "github.com/cds-snc/terraform-modules//S3_scan_object?ref=v6.1.5"
 
   s3_upload_bucket_name   = "notification-canada-ca-${var.env}-document-download-scan-files"
-  s3_scan_object_role_arn = "arn:aws:iam::${local.scan_files_account}:role/s3-scan-object"
-  scan_files_role_arn     = "arn:aws:iam::${local.scan_files_account}:role/scan-files-api"
+  s3_scan_object_role_arn = "arn:aws:iam::${var.scan_files_account_id}:role/s3-scan-object"
+  scan_files_role_arn     = "arn:aws:iam::${var.scan_files_account_id}:role/scan-files-api"
 
   billing_tag_value = var.billing_tag_value
 }
