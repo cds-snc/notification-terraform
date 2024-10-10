a33a84c2897e5c378fc3b472db2cb171f43a9769
Jimmy Royer <jimleroyer@gmail.com>
2024-10-10 18:37:55 -0400

    Brought forward SNS queries into Pinpoint + fixed queries

--- aws/pinpoint_to_sqs_sms_callbacks/cloudwatch_queries.tf
+++ aws/pinpoint_to_sqs_sms_callbacks/cloudwatch_queries.tf
@@ -1,4 +1,25 @@
-resource "aws_cloudwatch_query_definition" "pinpoint-carrier-dwell-times" {
+resource "aws_cloudwatch_query_definition" "pinpoint-sms-blocked-as-spam" {
+  count = var.cloudwatch_enabled ? 1 : 0
+  name  = "SMS (Pinpoint) / Block as spam"
+
+  log_group_names = [
+    aws_cloudwatch_log_group.pinpoint_deliveries_failures.name
+  ]
+
+  query_string = <<QUERY
+fields @timestamp as Timestamp, carrierName as Carrier, messageStatus as `Provider response`, 
+originationPhoneNumber as `Origination phone number`, destinationPhoneNumber as `Destination phone number`, 
+messageId as `Message ID`, messageStatus as `Message Status`, 
+messageStatusDescription as `Message status description`, @message
+| filter isFinal
+| filter eventType = 'TEXT_SPAM'
+| sort Timestamp desc
+| limit 100
+}
+QUERY
+}
+
+resource "aws_cloudwatch_query_definition" "pinpoint-sms-carrier-dwell-times" {
   count = var.cloudwatch_enabled ? 1 : 0
   name  = "SMS (Pinpoint) / Carrier dwell times"
 
@@ -16,7 +37,7 @@ count(*) as Number by carrierName as Carrier
 QUERY
 }
 
-resource "aws_cloudwatch_query_definition" "pinpoint-failures-by-carrier" {
+resource "aws_cloudwatch_query_definition" "pinpoint-sms-failures-by-carrier" {
   count = var.cloudwatch_enabled ? 1 : 0
   name  = "SMS (Pinpoint) / Failures by carrier"
 
@@ -33,18 +54,123 @@ filter isFinal
 QUERY
 }
 
-resource "aws_cloudwatch_query_definition" "pinpoint-logs" {
+resource "aws_cloudwatch_query_definition" "pintpoint-sms-get-failures" {
+  count = var.cloudwatch_enabled ? 1 : 0
+  name  = "SMS (Pinpoint) / Get failures"
+
+  log_group_names = [
+    aws_cloudwatch_log_group.pinpoint_deliveries_failures.name
+  ]
+
+  query_string = <<QUERY
+fields @timestamp as Timestamp, carrierName as Carrier, messageStatus as `Provider response`, 
+originationPhoneNumber as `Origination phone number`, destinationPhoneNumber as `Destination phone number`, 
+messageId as `Message ID`, messageStatus as `Message Status`, 
+messageStatusDescription as `Message status description`, @message
+| filter isFinal
+| sort @timestamp desc
+| limit 100
+QUERY
+}
+
+resource "aws_cloudwatch_query_definition" "pinpoint-sms-international-sending-status" {
+  count = var.cloudwatch_enabled ? 1 : 0
+  name  = "SMS (Pinpoint) / International sending status"
+
+  log_group_names = [
+    aws_cloudwatch_log_group.pinpoint_deliveries.name,
+    aws_cloudwatch_log_group.pinpoint_deliveries_failures.name
+  ]
+
+  query_string = <<QUERY
+fields @timestamp, @message
+| parse @message /"isoCountryCode":"(?<Country>[^"]+)"/
+| parse @message /"eventType":"(?<Event_Type>[^"]+)"/
+| parse @message /"isFinal":(?<Is_Final>\w+)/
+| filter Is_Final = "true"
+| stats count(*) as Event_Count by Country, Event_Type
+| display Country, Event_Type, Event_Count
+| sort Country asc
+| limit 100
+QUERY
+}
+
+resource "aws_cloudwatch_query_definition" "pinpoint-sms-get-sms-logs-by-dest-phone-number" {
+  count = var.cloudwatch_enabled ? 1 : 0
+  name  = "SMS (Pinpoint) / Get SMS logs by destination phone number"
+
+  log_group_names = [
+    aws_cloudwatch_log_group.pinpoint_deliveries.name,
+    aws_cloudwatch_log_group.pinpoint_deliveries_failures.name
+  ]
+
+  query_string = <<QUERY
+fields @timestamp as Timestamp, carrierName as Carrier, messageStatus as `Provider response`, 
+originationPhoneNumber as `Origination phone number`, destinationPhoneNumber as `Destination phone number`, 
+messageId as `Message ID`, messageStatus as `Message Status`, 
+messageStatusDescription as `Message status description`, @message
+| filter destinationPhoneNumber like '+1416xxxxxxx'
+| sort Timestamp desc
+| limit 100
+QUERY
+}
+
+resource "aws_cloudwatch_query_definition" "pinpoint-sms-get-sms-logs-by-orig-phone-number" {
+  count = var.cloudwatch_enabled ? 1 : 0
+  name  = "SMS (Pinpoint) / Get SMS logs by origination phone number"
+
+  log_group_names = [
+    aws_cloudwatch_log_group.pinpoint_deliveries.name,
+    aws_cloudwatch_log_group.pinpoint_deliveries_failures.name
+  ]
+
+  query_string = <<QUERY
+fields @timestamp as Timestamp, carrierName as Carrier, messageStatus as `Provider response`, 
+originationPhoneNumber as `Origination phone number`, destinationPhoneNumber as `Destination phone number`, 
+messageId as `Message ID`, messageStatus as `Message Status`, 
+messageStatusDescription as `Message status description`, @message
+| filter originationPhoneNumber like '237762'
+| sort Timestamp desc
+| limit 100
+QUERY
+}
+
+resource "aws_cloudwatch_query_definition" "pinpoint-sms-logs" {
   count = var.cloudwatch_enabled ? 1 : 0
   name  = "SMS (Pinpoint) / Logs"
 
   log_group_names = [
-    aws_cloudwatch_log_group.pinpoint_deliveries.name
+    aws_cloudwatch_log_group.pinpoint_deliveries.name,
+    aws_cloudwatch_log_group.pinpoint_deliveries_failures.name
   ]
 
   query_string = <<QUERY
-fields @timestamp, destinationPhoneNumber, messageStatus, messageStatusDescription, @logStream
+fields @timestamp as Timestamp, carrierName as Carrier, messageStatus as `Provider response`, 
+originationPhoneNumber as `Origination phone number`, destinationPhoneNumber as `Destination phone number`, 
+messageId as `Message ID`, messageStatus as `Message Status`, 
+messageStatusDescription as `Message status description`, @message
 | filter isFinal
 | sort @timestamp desc
 | limit 100
+QUERY
+}
+
+resource "aws_cloudwatch_query_definition" "pinpoint-sms-success-vs-unreachable" {
+  count = var.cloudwatch_enabled ? 1 : 0
+  name  = "SMS (Pinpoint) / Success vs Unreachable"
+
+  log_group_names = [
+    aws_cloudwatch_log_group.pinpoint_deliveries.name,
+    aws_cloudwatch_log_group.pinpoint_deliveries_failures.name
+  ]
+
+  query_string = <<QUERY
+fields @timestamp as Timestamp, carrierName as Carrier, messageStatus as `Provider response`, 
+originationPhoneNumber as `Origination phone number`, destinationPhoneNumber as `Destination phone number`, 
+messageId as `Message ID`, messageStatus as `Message Status`, 
+messageStatusDescription as `Message status description`, @message
+| parse messageStatusDescription "Phone is currently unreachable/*" as @unavailable
+| parse messageStatusDescription "Message has been * by phone" as @available
+| stats count(@unavailable) as Unavailable, count(@available) as Available, count(*) as Total by bin(1h)
 QUERY
 }
