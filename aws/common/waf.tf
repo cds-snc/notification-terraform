33fdac26539f80de9ce64bf27105cf00667528a2
Stephen Astels <sastels@gmail.com>
2024-10-15 14:12:46 -0400

    Revert "make waf regexes more readable"
    This reverts commit cc1768676efcf2272a18ee29b48bc50da3c3505a.

--- aws/common/waf.tf
+++ aws/common/waf.tf
@@ -80,17 +80,7 @@ resource "aws_wafv2_regex_pattern_set" "re_admin" {
 
   # GCA routes
   regular_expression {
-    regex_string = <<EOF
-/.*-contact-information|
-/.*-a-jour-les-coordonnees|
-/delivery-and-failure|
-/livraison-.*-et-echec|
-/system-status|
-/etat-du-systeme|
-/comprendre-.*-livraison|
-/sending-custom-content|
-/utiliser-.*-de-calcul
-EOF
+    regex_string = "/.*-contact-information|/.*-a-jour-les-coordonnees|/delivery-and-failure|/livraison-.*-et-echec|/system-status|/etat-du-systeme|/comprendre-.*-livraison|/sending-custom-content|/utiliser-.*-de-calcul"
   }
 
   tags = {
@@ -108,19 +98,7 @@ resource "aws_wafv2_regex_pattern_set" "re_admin2" {
   # https://docs.aws.amazon.com/waf/latest/developerguide/waf-regex-pattern-set-managing.html
 
   regular_expression {
-    regex_string = <<EOF
-/notifications.*|
-/organisation.*|
-/organisations.*|
-/platform-stats.*|
-/provider-details.*|
-/service.*|
-/static.*|
-/user.*|
-/v2.*|
-/cache-clear|
-/support/find-ids
-EOF
+    regex_string = "/sitemap|/plandesite|/agree-terms|/getting-started|/decouvrir-notification-gc|/template-category.*|/template-categories.*|/mettre-en-forme-les-courriels|/find-ids"
   }
 
   tags = {
