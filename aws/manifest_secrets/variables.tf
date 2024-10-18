42903f7d23a36edc49833e09ce4d6d9fe9d9e487
Michael Pond <michael.pond@cds-snc.ca>
2024-10-18 15:23:48 -0400

    removing another bad var

--- aws/manifest_secrets/variables.tf
+++ aws/manifest_secrets/variables.tf
@@ -6,10 +6,6 @@ variable "database_read_only_proxy_endpoint" {
   type = string
 }
 
-variable "postgres_rds_instance_id" {
-  type = string
-}
-
 variable "postgres_cluster_endpoint" {
   type = string
 }
