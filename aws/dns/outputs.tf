c22160a65d49d91aaddee3603c7ad93528a9e8ab
Ben Larabie <ben.larabie@cds-snc.ca>
2024-10-22 11:21:22 -0400

    formatting

--- aws/dns/outputs.tf
+++ aws/dns/outputs.tf
@@ -1,8 +1,3 @@
-output "ses_verification" {
-  description = "Verification TXT record for SES"
-  value       = aws_ses_domain_identity.notification-canada-ca.verification_token
-}
-
 output "dkim_verification_token" {
   description = "DKIM CNAME record for SES"
   value       = aws_ses_domain_dkim.notification-canada-ca.dkim_tokens
