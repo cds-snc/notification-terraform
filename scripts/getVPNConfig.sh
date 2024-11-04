78ddfb117be072ef5bc2b7139ca2896d02031efb
Ben Larabie <ben.larabie@cds-snc.ca>
2024-11-04 11:49:21 -0500

    Comments

--- scripts/getVPNConfig.sh
+++ scripts/getVPNConfig.sh
@@ -22,22 +22,26 @@ while getopts 'c:u:' opt; do
 done
 shift "$(($OPTIND -1))"
 
-echo $FILE_NAME
+echo "$FILE_NAME"
 #Find the logged in user
 LOGGED_USER=$(stat -f %Su /dev/console)
+# Get full path
 export FULL_PATH="/Users/$LOGGED_USER/.config/AWSVPNClient/OpenVpnConfigs/$FILE_NAME"
 
 echo "Getting SAML VPN Config for AWS Profile: $AWS_PROFILE"
 VPN_ID=$(aws ec2 describe-client-vpn-endpoints --query 'ClientVpnEndpoints[? Description == `private-subnets`].ClientVpnEndpointId' --output text)
 
+# Fetch the OVPN Config
 aws ec2 export-client-vpn-client-configuration --client-vpn-endpoint-id $VPN_ID --output text | sed '/auth-federate/d' > /Users/$LOGGED_USER/.config/AWSVPNClient/OpenVpnConfigs/"$FILE_NAME"
 
+# Backup the ConnectionProfiles file 
 cp /Users/$LOGGED_USER/.config/AWSVPNClient/ConnectionProfiles /Users/$LOGGED_USER/.config/AWSVPNClient/ConnectionProfiles.bak
 
 if [[ "$CREATE" == "true" ]]; then
   echo "Creating new VPN Config"
   mkdir -p /Users/$LOGGED_USER/.config/AWSVPNClient/OpenVpnConfigs
 
+  # Inject a new profile into the ConnectionProfiles file
   jq -c '.ConnectionProfiles[.ConnectionProfiles | length] |= . + {
         "ProfileName": env.FILE_NAME, 
         "OvpnConfigFilePath": "/Users/env.LOGGED_USER/.config/AWSVPNClient/OpenVpnConfigs/env.FILE_NAME",
@@ -47,10 +51,10 @@ if [[ "$CREATE" == "true" ]]; then
         "FederatedAuthType": 1
   }' /Users/$LOGGED_USER/.config/AWSVPNClient/ConnectionProfiles > /Users/$LOGGED_USER/.config/AWSVPNClient/ConnectionProfiles.tmp   
   
-
-
+  # Update the OvpnConfigFilePath with the full path. Had to do this in two steps.
   jq -c '( .ConnectionProfiles[] | select(.ProfileName == env.FILE_NAME) | .OvpnConfigFilePath ) |= env.FULL_PATH' /Users/$LOGGED_USER/.config/AWSVPNClient/ConnectionProfiles.tmp  > /Users/$LOGGED_USER/.config/AWSVPNClient/ConnectionProfiles.tmp1 
 
+  # Clean up
   rm /Users/$LOGGED_USER/.config/AWSVPNClient/ConnectionProfiles.tmp
   mv /Users/$LOGGED_USER/.config/AWSVPNClient/ConnectionProfiles.tmp1 /Users/$LOGGED_USER/.config/AWSVPNClient/ConnectionProfiles
 
@@ -58,7 +62,7 @@ fi
 
 if [[ "$UPDATE" == "true" ]]; then
   echo "Updating VPN Config"
-
+  # Update the VPN Id for the profile
   jq -c '( .ConnectionProfiles[] | select(.ProfileName == env.FILE_NAME) | .CvpnEndpointId ) |= env.VPN_ID' /Users/$LOGGED_USER/.config/AWSVPNClient/ConnectionProfiles > /Users/$LOGGED_USER/.config/AWSVPNClient/ConnectionProfiles.tmp   
   mv /Users/$LOGGED_USER/.config/AWSVPNClient/ConnectionProfiles.tmp /Users/$LOGGED_USER/.config/AWSVPNClient/ConnectionProfiles
 fi
