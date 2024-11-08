6a5d333d72f5e524b8fc47b63db26797bd8e7053
Pat Heard <patrick.heard@cds-snc.ca>
2024-11-08 13:34:37 -0500

    chore: update to latest `simple_static_website` module (#1651)
    Update to the latest major version of the module which requires us to
    explicitly set a variable controlling the creation of a hosted zone.

--- aws/system_status_static_site/main.tf
+++ aws/system_status_static_site/main.tf
@@ -1,9 +1,10 @@
 module "system_status_static_site" {
-  source = "github.com/cds-snc/terraform-modules//simple_static_website?ref=v9.6.4"
+  source = "github.com/cds-snc/terraform-modules//simple_static_website?ref=v10.0.0"
 
   domain_name_source                 = var.env == "production" ? "status.notification.canada.ca" : "status.${var.env}.notification.cdssandbox.xyz"
   billing_tag_value                  = var.billing_tag_value
   hosted_zone_id                     = var.route53_zone_id
+  is_create_hosted_zone              = false
   s3_bucket_name                     = "notification-canada-ca-${var.env}-system-status"
   force_destroy_s3_bucket            = true
   cloudfront_query_string_forwarding = true
