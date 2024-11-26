0659cd9c56732585fe8ae2514ffeb7d9efd6b6f7
Ben Larabie <ben.larabie@cds-snc.ca>
2024-11-26 15:28:54 -0500

    fixes [review]

--- aws/github/admin-secrets.tf
+++ aws/github/admin-secrets.tf
@@ -15,7 +15,7 @@ resource "github_actions_secret" "admin_aws_access_key" {
 resource "github_actions_secret" "admin_aws_secret_access_key" {
   count           = var.env == "production" || var.env == "staging" ? 1 : 0
   repository      = data.github_repository.notification_admin.name
-  secret_name     = "${upper(var.env)}_AWS_SECRET_ACCESS_KEY_ID"
+  secret_name     = "${upper(var.env)}_AWS_SECRET_ACCESS_KEY"
   plaintext_value = var.aws_secret_access_key
 }
 
