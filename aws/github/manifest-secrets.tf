35570b0cd10507dae80fd9563a65655d99375e16
Ben Larabie <ben.larabie@cds-snc.ca>
2024-11-26 08:59:36 -0500

    manifests smoke secrets [review]

--- aws/github/manifest-secrets.tf
+++ aws/github/manifest-secrets.tf
@@ -52,3 +52,17 @@ resource "github_actions_secret" "manifests_new_relic_api_key" {
   secret_name     = "${upper(var.env)}_NEW_RELIC_API_KEY"
   plaintext_value = var.new_relic_api_key
 }
+
+resource "github_actions_secret" "smoke_admin_client_secret" {
+  count           = var.env == "staging" ? 1 : 0
+  repository      = data.github_repository.notification_manifests.name
+  secret_name     = "SMOKE_ADMIN_CLIENT_SECRET"
+  plaintext_value = var.manifest_smoke_admin_client_secret
+}
+
+resource "github_actions_secret" "smoke_admin_api_key" {
+  count           = var.env == "staging" ? 1 : 0
+  repository      = data.github_repository.notification_manifests.name
+  secret_name     = "SMOKE_API_KEY"
+  plaintext_value = var.manifest_smoke_admin_api_key
+}
