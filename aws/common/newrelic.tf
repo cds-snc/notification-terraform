9bcef4a5d3edcf66645aa27f1dec91fb463c1a0c
Ben Larabie <ben.larabie@cds-snc.ca>
2024-12-16 09:41:11 -0500

    ingest type

--- aws/common/newrelic.tf
+++ aws/common/newrelic.tf
@@ -88,11 +88,12 @@ resource "newrelic_cloud_aws_link_account" "newrelic_cloud_integration_push" {
 }
 
 resource "newrelic_api_access_key" "newrelic_aws_access_key" {
-  count      = var.enable_new_relic && var.env != "production" ? 1 : 0
-  account_id = var.new_relic_account_id
-  key_type   = var.env == "staging" ? "USER" : "INGEST"
-  name       = var.env == "staging" ? "notify_tf_provider" : "notify_tf_provider_${var.env}"
-  notes      = "Used by Notify Terraform Code to create New Relic Resources"
+  count       = var.enable_new_relic && var.env != "production" ? 1 : 0
+  account_id  = var.new_relic_account_id
+  key_type    = var.env == "staging" ? "USER" : "INGEST"
+  ingest_type = var.env == "staging" ? null : "LICENSE"
+  name        = var.env == "staging" ? "notify_tf_provider" : "notify_tf_provider_${var.env}"
+  notes       = "Used by Notify Terraform Code to create New Relic Resources"
 }
 
 resource "aws_iam_role" "firehose_newrelic_role" {
