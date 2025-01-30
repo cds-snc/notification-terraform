75a3d315978b48c522cc1fc8aad4b30ce6d782dc
wbanks <william.banks@cds-snc.ca>
2025-01-30 17:17:21 -0500

    Update 504 query to group results by hour

--- aws/lambda-api/cloudwatch_queries.tf
+++ aws/lambda-api/cloudwatch_queries.tf
@@ -30,15 +30,17 @@ QUERY
 }
 
 resource "aws_cloudwatch_query_definition" "api-gateway-504-timeouts" {
-  name = "API Gateway 504 timeouts"
+  name = "API Gateway 504 timeouts grouped by hour"
 
   log_group_names = [
     local.api_gateway_log_group
   ]
 
   query_string = <<QUERY
-fields @timestamp, @message, @logStream
-| filter @message like /504/
+fields @timestamp, @message, @logStream, status
+| filter status = "504"
+| stats count() as error_count by bin(@timestamp, 1h)
 | sort @timestamp desc
+| limit 1000
 QUERY
 }
