5aa64720791704161e5b119ab7de89576e1225dd
wbanks <william.banks@cds-snc.ca>
2025-02-03 12:05:14 -0500

    Formatting [review]

--- aws/lambda-api/variables.tf
+++ aws/lambda-api/variables.tf
@@ -35,7 +35,7 @@ variable "sns_alert_critical_arn" {
 }
 
 locals {
-  api_lambda_log_group = "/aws/lambda/api-lambda"
+  api_lambda_log_group  = "/aws/lambda/api-lambda"
   api_gateway_log_group = "api_gateway_log_group"
 }
 
