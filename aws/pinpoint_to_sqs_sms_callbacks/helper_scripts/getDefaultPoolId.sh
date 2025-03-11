6602041f87ab37800998a85cab5cdb6eda8e1a38
Ben Larabie <ben.larabie@blarabie-jtfk.home>
2025-03-11 10:50:43 -0400

    comment [review]

--- aws/pinpoint_to_sqs_sms_callbacks/helper_scripts/getDefaultPoolId.sh
+++ aws/pinpoint_to_sqs_sms_callbacks/helper_scripts/getDefaultPoolId.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# This is a dirty hack to get the ARN of the PCA so that we can import it when creating a new environment.
+# This is a dirty hack to get the pool id so that we can import it when creating a new environment.
 # Terraform external datasources require a very basic JSON output, so we're using jq to format the output.
 poolArns=$(aws pinpoint-sms-voice-v2 describe-pools --output json | jq -r '.Pools[].PoolArn')
 defaultPoolExists=false
