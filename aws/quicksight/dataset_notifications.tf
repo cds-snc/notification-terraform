e5a79d9ed3248dd9bf6da2596f008122a241b853
Ben Larabie <ben.larabie@cds-snc.ca>
2025-03-31 13:53:40 -0400

    Adding ignore lifecycle changes to refresh_properties (#1838)
    * Adding ignore lifecycle changes to refresh_properties
    
    * removing blank lines
    
    * removing whitespace
    
    ---------
    
    Co-authored-by: Ben Larabie <ben.larabie@blarabie-jtfk.home>

--- aws/quicksight/dataset_notifications.tf
+++ aws/quicksight/dataset_notifications.tf
@@ -5,6 +5,12 @@ resource "aws_quicksight_data_set" "notifications" {
   name        = "Notifications"
   import_mode = "SPICE"
 
+  lifecycle {
+    ignore_changes = [
+      refresh_properties,
+    ]
+  }
+
   physical_table_map {
     physical_table_map_id = "notifications"
     custom_sql {
--- aws/quicksight/dataset_users.tf
+++ aws/quicksight/dataset_users.tf
@@ -5,6 +5,12 @@ resource "aws_quicksight_data_set" "users" {
   name        = "Users"
   import_mode = "SPICE"
 
+  lifecycle {
+    ignore_changes = [
+      refresh_properties,
+    ]
+  }
+
   physical_table_map {
     physical_table_map_id = "users"
     relational_table {
