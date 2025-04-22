eb4193e5e16237277d4de9adcf71f2a7e9c29d3f
wbanks <william.banks@cds-snc.ca>
2025-04-22 15:34:47 -0400

    Tweak sns-sms-success-rate-canadian-numbers-us-west-2-warning
    - Moved the successRate into its own metric query
    - Added a new metric query to measure total canadian SMS published
    - Added a guard condition that prevents the alarm from sounding if the messagesPublished metric is below a threshold of 75. This should prevent the alarm from sounding during periods of low sending rates. E.g holiday long weekends
    [review]

--- aws/common/cloudwatch_alarms_sms.tf
+++ aws/common/cloudwatch_alarms_sms.tf
@@ -93,23 +93,54 @@ resource "aws_cloudwatch_metric_alarm" "sns-sms-success-rate-canadian-numbers-wa
 
 resource "aws_cloudwatch_metric_alarm" "sns-sms-success-rate-canadian-numbers-us-west-2-warning" {
   provider = aws.us-west-2
+  count    = var.cloudwatch_enabled ? 1 : 0
 
-  count               = var.cloudwatch_enabled ? 1 : 0
   alarm_name          = "sns-sms-success-rate-canadian-numbers-us-west-2-warning"
   alarm_description   = "SMS success rate to Canadian numbers is below 85% over 2 consecutive periods of 12 hours"
   comparison_operator = "LessThanThreshold"
   evaluation_periods  = "2"
   datapoints_to_alarm = "2"
-  metric_name         = "SMSSuccessRate"
-  namespace           = "AWS/SNS"
-  period              = 60 * 60 * 12
-  statistic           = "Average"
-  threshold           = 85 / 100
+  threshold           = "1"
   alarm_actions       = [aws_sns_topic.notification-canada-ca-alert-warning-us-west-2.arn]
   treat_missing_data  = "notBreaching"
-  dimensions = {
-    SMSType = "Transactional"
-    Country = "CA"
+
+  metric_query {
+    id          = "successRate"
+    label       = "Success Rate"
+    return_data = false
+    metric {
+      namespace   = "AWS/SNS"
+      metric_name = "SMSSuccessRate"
+      period      = 60 * 60 * 12
+      stat        = "Average"
+      dimensions = {
+        SMSType = "Transactional"
+        Country = "CA"
+      }
+    }
+  }
+
+  metric_query {
+    id          = "messagesPublished"
+    label       = "Messages Published"
+    return_data = false
+    metric {
+      namespace   = "AWS/SNS"
+      metric_name = "NumberOfMessagesPublished"
+      period      = 60 * 60 * 12
+      stat        = "Sum"
+      dimensions = {
+        SMSType = "Transactional"
+        Country = "CA"
+      }
+    }
+  }
+
+  metric_query {
+    id          = "alarmCondition"
+    label       = "Guarded Alarm Condition"
+    return_data = true
+    expression  = "IF(successRate < 0.85 AND messagesPublished > 75, 0, 1)"
   }
 }
 
