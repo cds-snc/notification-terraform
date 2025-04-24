616d4477064f9530690b21f5c62215c409da18d6
Pat Heard <patrick.heard@cds-snc.ca>
2025-04-24 13:25:58 -0400

    fix: remove quotes

--- aws/rds/iam.tf
+++ aws/rds/iam.tf
@@ -98,26 +98,26 @@ data "aws_iam_policy_document" "platform_data_lake_export" {
 }
 
 moved {
-  from = "aws_iam_role.platform_data_lake_export[0]"
-  to   = "aws_iam_role.platform_data_lake_export"
+  from = aws_iam_role.platform_data_lake_export[0]
+  to   = aws_iam_role.platform_data_lake_export
 }
 
 moved {
-  from = "aws_iam_policy.platform_data_lake_export[0]"
-  to   = "aws_iam_policy.platform_data_lake_export"
+  from = aws_iam_policy.platform_data_lake_export[0]
+  to   = aws_iam_policy.platform_data_lake_export
 }
 
 moved {
-  from = "aws_iam_role_policy_attachment.platform_data_lake_export[0]"
-  to   = "aws_iam_role_policy_attachment.platform_data_lake_export"
+  from = aws_iam_role_policy_attachment.platform_data_lake_export[0]
+  to   = aws_iam_role_policy_attachment.platform_data_lake_export
 }
 
 moved {
-  from = "data.aws_iam_policy_document.platform_data_lake_export_assume[0]"
-  to   = "data.aws_iam_policy_document.platform_data_lake_export_assume"
+  from = data.aws_iam_policy_document.platform_data_lake_export_assume[0]
+  to   = data.aws_iam_policy_document.platform_data_lake_export_assume
 }
 
 moved {
-  from = "data.aws_iam_policy_document.platform_data_lake_export[0]"
-  to   = "data.aws_iam_policy_document.platform_data_lake_export"
+  from = data.aws_iam_policy_document.platform_data_lake_export[0]
+  to   = data.aws_iam_policy_document.platform_data_lake_export
 }
