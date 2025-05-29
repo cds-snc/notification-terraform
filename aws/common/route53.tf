f6827e80aeb1ff78c019041fe53e0183df80bca9
Mike Pond <32133001+P0NDER0SA@users.noreply.github.com>
2025-05-29 16:07:10 -0400

    DNS Failure Alarms (#1926)
    * trying out something to see if it makes sense for dns failures alerting
    
    * formatting
    
    * adjusting to make it for Route53
    
    * adding topic back
    
    * formatting
    
    * attempting to filter on our notification sandbox domain
    
    * removing the redundant environment from the name
    
    * formatting
    
    * Moving a few things around and adjusting the topics to use the existing proper ones
    
    * missing arns
    
    * adding discrete alarms for warnring and critical.  commenting out the critical for now
    
    * turning off the critical one for now
    
    * just setting them all to warnings until we're ok with it

--- aws/common/route53.tf
+++ aws/common/route53.tf
@@ -17,3 +17,55 @@ resource "aws_route53_resolver_query_log_config_association" "dns_query_log_conf
   resolver_query_log_config_id = aws_route53_resolver_query_log_config.dns_query_log_config[0].id
   resource_id                  = aws_vpc.notification-canada-ca.id
 }
+
+# Route53 Resolver Query Logging Configuration
+resource "aws_route53_resolver_query_log_config" "main" {
+  count = var.cloudwatch_enabled ? 1 : 0
+  name  = "route53-query-logging"
+
+  destination_arn = aws_cloudwatch_log_group.route53_resolver_query_log[0].arn
+
+  tags = {
+    Environment = var.env
+    CostCenter  = "notification-canada-ca-${var.env}"
+    Service     = "dns-monitoring"
+  }
+}
+
+# Associate query logging with VPCs
+resource "aws_route53_resolver_query_log_config_association" "main" {
+  count = var.cloudwatch_enabled ? length(var.vpc_ids) : 0
+
+  resolver_query_log_config_id = aws_route53_resolver_query_log_config.main[0].id
+  resource_id                  = var.vpc_ids[count.index]
+}
+
+# Metric Filter for NXDOMAIN errors on notification.cdssandbox.ca domain
+resource "aws_cloudwatch_log_metric_filter" "route53_nxdomain_notification" {
+  count = var.cloudwatch_enabled ? 1 : 0
+  name  = "Route53NXDOMAINNotificationDomain"
+  # Simplified pattern that should be compatible with CloudWatch
+  pattern        = "{ $.rcode = \"NXDOMAIN\" && $.query_name = \"*.notification.cdssandbox.ca\" }"
+  log_group_name = aws_cloudwatch_log_group.route53_resolver_query_log[0].name
+
+  metric_transformation {
+    name      = "Route53DNSResolutionFailureCount"
+    namespace = "Route53/Resolver"
+    value     = "1"
+  }
+}
+
+# Metric Filter for SERVFAIL errors on notification.cdssandbox.ca domain
+resource "aws_cloudwatch_log_metric_filter" "route53_servfail_notification" {
+  count = var.cloudwatch_enabled ? 1 : 0
+  name  = "Route53SERVFAILNotificationDomain"
+  # Simplified pattern that should be compatible with CloudWatch
+  pattern        = "{ $.rcode = \"SERVFAIL\" && $.query_name = \"*.notification.cdssandbox.ca\" }"
+  log_group_name = aws_cloudwatch_log_group.route53_resolver_query_log[0].name
+
+  metric_transformation {
+    name      = "Route53DNSResolutionFailureCount"
+    namespace = "Route53/Resolver"
+    value     = "1"
+  }
+}
