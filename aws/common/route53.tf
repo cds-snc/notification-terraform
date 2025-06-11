ec32ea61e7fc12143e7a16b16e5cd3d02fb3a397
Michael Pond <mikepond11@gmail.com>
2025-06-11 15:51:38 -0400

    cleaning out a couple of unneeded code snippets and fixing the references so the alarms actually read the data from the metrics

--- aws/common/route53.tf
+++ aws/common/route53.tf
@@ -2,21 +2,6 @@
 # AWS Route 53 for Notification application
 ###
 
-resource "aws_route53_resolver_query_log_config" "dns_query_log_config" {
-  provider        = aws.us-east-1
-  count           = var.cloudwatch_enabled ? 1 : 0
-  name            = "${var.region}_${var.account_id}_dns_query_log_config"
-  destination_arn = aws_cloudwatch_log_group.route53_resolver_query_log[0].arn
-
-  tags = {
-    CostCenter = "notification-canada-ca-${var.env}"
-  }
-
-  depends_on = [
-    aws_cloudwatch_log_resource_policy.route53_resolver_query_logging_policy
-  ]
-}
-
 # Route53 Resolver Query Logging Configuration
 resource "aws_route53_resolver_query_log_config" "main" {
   provider = aws.us-east-1
@@ -34,14 +19,6 @@ resource "aws_route53_resolver_query_log_config" "main" {
   depends_on = [
     aws_cloudwatch_log_resource_policy.route53_resolver_query_logging_policy
   ]
-}
-
-# Associate query logging with VPCs
-resource "aws_route53_resolver_query_log_config_association" "main" {
-  count = var.cloudwatch_enabled ? length(var.vpc_ids) : 0
-
-  resolver_query_log_config_id = aws_route53_resolver_query_log_config.main[0].id
-  resource_id                  = var.vpc_ids[count.index]
 }
 
 # Metric Filter for NXDOMAIN errors on notification.cdssandbox.ca domain - public DNS only
