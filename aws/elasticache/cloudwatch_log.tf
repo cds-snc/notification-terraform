5f101e179b2b4d28c41d19b7edb052d90e8d7f2d
Ben Larabie <ben.larabie@cds-snc.ca>
2025-06-25 11:27:41 -0400

    missed adding these (#2008)
    Co-authored-by: Ben Larabie <ben.larabie@blarabie-jtfk.home>

--- aws/elasticache/cloudwatch_log.tf
+++ aws/elasticache/cloudwatch_log.tf
@@ -15,13 +15,13 @@ resource "aws_cloudwatch_log_group" "redis-batch-saving" {
 }
 
 resource "aws_cloudwatch_log_group" "elasticache_queue_cache_slow_logs" {
-  count             = var.env == "dev" ? 1 : 0
+  count             = var.env != "production" ? 1 : 0
   name              = "/aws/elasticache/notify-${var.env}-queue-cache/slow-logs"
   retention_in_days = var.log_retention_period_days
 }
 
 resource "aws_cloudwatch_log_group" "elasticache_queue_cache_engine_logs" {
-  count             = var.env == "dev" ? 1 : 0
+  count             = var.env != "production" ? 1 : 0
   name              = "/aws/elasticache/notify-${var.env}-queue-cache/engine-logs"
   retention_in_days = var.log_retention_period_days
 }
