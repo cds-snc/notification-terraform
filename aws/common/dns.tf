3f3c52e113ca2873306aa1c9a172e87c41fe7bc5
Ben Larabie <ben.larabie@cds-snc.ca>
2025-07-07 11:21:08 -0400

    Coredns log fix (#2026)
    * Fix the location of the coredns log group
    
    * Get the log group name from the resource
    
    ---------
    
    Co-authored-by: Ben Larabie <ben.larabie@blarabie-jtfk.home>

--- aws/common/dns.tf
+++ aws/common/dns.tf
@@ -1,21 +1,4 @@
 ###
-# CORE DNS resources for Notification application
-###
-
-resource "aws_cloudwatch_log_metric_filter" "coredns-nxdomain-notification-filter" {
-  count          = var.cloudwatch_enabled ? 1 : 0
-  name           = "CoreDNSNXDOMAINNotificationFilter"
-  log_group_name = "/aws/containerinsights/${var.eks_cluster_name}/application"
-  pattern        = "{ $.log = \"*notification*\" && $.log = \"*NXDOMAIN*\" }"
-
-  metric_transformation {
-    name      = "CoreDNSNXDOMAINNotificationCount"
-    namespace = "NotificationCanadaCa/DNS"
-    value     = "1"
-  }
-}
-
-###
 # AWS Route 53 for Notification application
 ###
 
@@ -38,13 +21,13 @@ resource "aws_route53_resolver_query_log_config" "main" {
   ]
 }
 
-# Metric Filter for NXDOMAIN errors on notification.cdssandbox.ca domain - public DNS only
-resource "aws_cloudwatch_log_metric_filter" "route53_nxdomain_notification" {
+# Metric Filter for SERVFAIL errors on notification.cdssandbox.ca domain - public DNS only
+resource "aws_cloudwatch_log_metric_filter" "route53_servfail_notification" {
   provider = aws.us-east-1
   count    = var.cloudwatch_enabled ? 1 : 0
-  name     = "Route53NXDOMAINNotificationDomain"
+  name     = "Route53SERVFAILNotificationDomain"
   # Pattern simplified to only exclude internal queries
-  pattern        = "?${var.base_domain} NXDOMAIN"
+  pattern        = "?${var.base_domain} SERVFAIL"
   log_group_name = aws_cloudwatch_log_group.route53_resolver_query_log[0].name
 
   metric_transformation {
@@ -54,13 +37,18 @@ resource "aws_cloudwatch_log_metric_filter" "route53_nxdomain_notification" {
   }
 }
 
-# Metric Filter for SERVFAIL errors on notification.cdssandbox.ca domain - public DNS only
-resource "aws_cloudwatch_log_metric_filter" "route53_servfail_notification" {
+###
+# AWS Route 53 for Notification application
+###
+
+
+# Metric Filter for NXDOMAIN errors on notification.cdssandbox.ca domain - public DNS only
+resource "aws_cloudwatch_log_metric_filter" "route53_nxdomain_notification" {
   provider = aws.us-east-1
   count    = var.cloudwatch_enabled ? 1 : 0
-  name     = "Route53SERVFAILNotificationDomain"
+  name     = "Route53NXDOMAINNotificationDomain"
   # Pattern simplified to only exclude internal queries
-  pattern        = "?${var.base_domain} SERVFAIL"
+  pattern        = "?${var.base_domain} NXDOMAIN"
   log_group_name = aws_cloudwatch_log_group.route53_resolver_query_log[0].name
 
   metric_transformation {
