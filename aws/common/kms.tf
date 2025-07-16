f82da0b3979c94922a9727f29595261848c20cdd
Ben Larabie <ben.larabie@blarabie-jtfk.home>
2025-07-16 10:40:56 -0400

    formatting

--- aws/common/kms.tf
+++ aws/common/kms.tf
@@ -5,83 +5,147 @@ resource "aws_kms_key" "notification-canada-ca" {
   enable_key_rotation = true
 
   # This policy allows encryption/decryption in Cloudwatch
-  policy = <<EOF
-{
-  "Version" : "2012-10-17",
-  "Id" : "key-default-1",
-  "Statement" : [ {
-      "Sid" : "Enable IAM User Permissions",
-      "Effect" : "Allow",
-      "Principal" : {
-        "AWS" : "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
-      },
-      "Action" : "kms:*",
-      "Resource" : "*"
-    },
-    {
-      "Effect": "Allow",
-      "Principal": { "Service": "logs.${var.region}.amazonaws.com" },
-      "Action": [
-        "kms:Encrypt*",
-        "kms:Decrypt*",
-        "kms:ReEncrypt*",
-        "kms:GenerateDataKey*",
-        "kms:Describe*"
-      ],
-      "Resource": "*"
-    },
-    {
-      "Sid": "Allow_CloudWatch_for_CMK",
-      "Effect": "Allow",
-      "Principal": {
-          "Service":[
-              "cloudwatch.amazonaws.com"
-          ]
-      },
-      "Action": [
-          "kms:Decrypt","kms:GenerateDataKey"
-      ],
-      "Resource": "*"
-    },
-    {
-    "Effect": "Allow",
-    "Principal": {
-      "Service": "ses.amazonaws.com"
-    },
-    "Action": [
-      "kms:GenerateDataKey*",
-      "kms:Decrypt"
-    ],
-    "Resource": "*"
-  },
-  {
-    "Sid": "Allow_Budgets_for_CMK",
-    "Effect": "Allow",
-    "Principal": {
-      "Service": "budgets.amazonaws.com"
-    },
-    "Action": [
-      "kms:GenerateDataKey*",
-      "kms:Decrypt"
-    ],
-    "Resource": "*",
-    "Condition": {
-      "StringEquals": {
-        "aws:SourceAccount": "${data.aws_caller_identity.current.account_id}"
-      },
-      "ArnLike": {
-        "aws:SourceArn": "arn:aws:budgets::${data.aws_caller_identity.current.account_id}:*"
-      }
-    }
-  }
-
-  ]
-}
-EOF
+  policy = var.env == "production" ? data.aws_iam_policy_document.standard_kms_policy.json : data.aws_iam_policy_document.encrypted_kms_policy.json
 
   tags = {
     Name       = "notification-canada-ca"
     CostCenter = "notification-canada-ca-${var.env}"
+  }
+}
+
+data "aws_iam_policy_document" "standard_kms_policy" {
+  statement {
+    sid    = "Enable IAM User Permissions"
+    effect = "Allow"
+    principals {
+      type        = "AWS"
+      identifiers = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"]
+    }
+    actions   = ["kms:*"]
+    resources = ["*"]
+  }
+
+  statement {
+    effect = "Allow"
+    principals {
+      type        = "Service"
+      identifiers = ["logs.${var.region}.amazonaws.com"]
+    }
+    actions = [
+      "kms:Encrypt*",
+      "kms:Decrypt*",
+      "kms:ReEncrypt*",
+      "kms:GenerateDataKey*",
+      "kms:Describe*"
+    ]
+    resources = ["*"]
+  }
+
+  statement {
+    sid    = "Allow_CloudWatch_for_CMK"
+    effect = "Allow"
+    principals {
+      type        = "Service"
+      identifiers = ["cloudwatch.amazonaws.com"]
+    }
+    actions = [
+      "kms:Decrypt",
+      "kms:GenerateDataKey"
+    ]
+    resources = ["*"]
+  }
+
+  statement {
+    effect = "Allow"
+    principals {
+      type        = "Service"
+      identifiers = ["ses.amazonaws.com"]
+    }
+    actions = [
+      "kms:GenerateDataKey*",
+      "kms:Decrypt"
+    ]
+    resources = ["*"]
+  }
+}
+
+data "aws_iam_policy_document" "encrypted_kms_policy" {
+  statement {
+    sid    = "Enable IAM User Permissions"
+    effect = "Allow"
+    principals {
+      type        = "AWS"
+      identifiers = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"]
+    }
+    actions   = ["kms:*"]
+    resources = ["*"]
+  }
+
+  statement {
+    effect = "Allow"
+    principals {
+      type        = "Service"
+      identifiers = ["logs.${var.region}.amazonaws.com"]
+    }
+    actions = [
+      "kms:Encrypt*",
+      "kms:Decrypt*",
+      "kms:ReEncrypt*",
+      "kms:GenerateDataKey*",
+      "kms:Describe*"
+    ]
+    resources = ["*"]
+  }
+
+  statement {
+    sid    = "Allow_CloudWatch_for_CMK"
+    effect = "Allow"
+    principals {
+      type        = "Service"
+      identifiers = ["cloudwatch.amazonaws.com"]
+    }
+    actions = [
+      "kms:Decrypt",
+      "kms:GenerateDataKey"
+    ]
+    resources = ["*"]
+  }
+
+  statement {
+    effect = "Allow"
+    principals {
+      type        = "Service"
+      identifiers = ["ses.amazonaws.com"]
+    }
+    actions = [
+      "kms:GenerateDataKey*",
+      "kms:Decrypt"
+    ]
+    resources = ["*"]
+  }
+
+  statement {
+    sid    = "Allow_Budgets_for_CMK"
+    effect = "Allow"
+    principals {
+      type        = "Service"
+      identifiers = ["budgets.amazonaws.com"]
+    }
+    actions = [
+      "kms:GenerateDataKey*",
+      "kms:Decrypt"
+    ]
+    resources = ["*"]
+    condition {
+      test     = "StringEquals"
+      variable = "aws:SourceAccount"
+      values   = ["${data.aws_caller_identity.current.account_id}"]
+    }
+    condition {
+      test     = "ArnLike"
+      variable = "aws:SourceArn"
+      values   = ["arn:aws:budgets::${data.aws_caller_identity.current.account_id}:*"]
+    }
   }
 }
 
