4a1f8ba7e8757364d3d808193c8b3a33211a65b7
Michael Pond <mikepond11@gmail.com>
2025-07-17 09:58:18 -0400

    reducing redundancy by creating locals

--- aws/common/kms.tf
+++ aws/common/kms.tf
@@ -1,5 +1,45 @@
 data "aws_caller_identity" "current" {}
 
+locals {
+  # Common KMS configuration for SQS queues
+  sqs_kms_config = {
+    kms_master_key_id                 = aws_kms_key.notification-canada-ca.arn
+    kms_data_key_reuse_period_seconds = 300
+  }
+
+  # Common KMS configuration for SNS topics
+  sns_kms_config = {
+    kms_master_key_id = aws_kms_key.notification-canada-ca.arn
+  }
+
+  # Common KMS configuration for SNS topics in us-west-2
+  sns_kms_config_us_west_2 = {
+    kms_master_key_id = aws_kms_key.notification-canada-ca-us-west-2.arn
+  }
+
+  # Common tags for SQS queues
+  sqs_common_tags = {
+    CostCenter = "notification-canada-ca-${var.env}"
+  }
+
+  # Common tags for SQS queues with Environment tag
+  sqs_common_tags_with_env = {
+    Environment = var.env
+    CostCenter  = "notification-canada-ca-${var.env}"
+  }
+
+  # Common tags for SNS topics
+  sns_common_tags = {
+    CostCenter = "notification-canada-ca-${var.env}"
+  }
+
+  # Common tags for all resources
+  common_tags = {
+    Name       = "notification-canada-ca"
+    CostCenter = "notification-canada-ca-${var.env}"
+  }
+}
+
 resource "aws_kms_key" "notification-canada-ca" {
   description         = "notification-canada-ca ${var.env} encryption key"
   enable_key_rotation = true
--- aws/common/sqs.tf
+++ aws/common/sqs.tf
@@ -3,71 +3,59 @@ resource "aws_sqs_queue" "priority_db_tasks_queue" {
   fifo_queue                        = true
   deduplication_scope               = "messageGroup"
   fifo_throughput_limit             = "perMessageGroupId"
-  kms_master_key_id                 = aws_kms_key.notification-canada-ca.arn
-  kms_data_key_reuse_period_seconds = 300
+  kms_master_key_id                 = local.sqs_kms_config.kms_master_key_id
+  kms_data_key_reuse_period_seconds = local.sqs_kms_config.kms_data_key_reuse_period_seconds
 
-  tags = {
-    CostCenter = "notification-canada-ca-${var.env}"
-  }
+  tags = local.sqs_common_tags
 }
 
 resource "aws_sqs_queue" "normal_db_tasks_queue" {
   name                              = "${var.celery_queue_prefix}-${var.sqs_normal_db_tasks_queue_name}"
-  kms_master_key_id                 = aws_kms_key.notification-canada-ca.arn
-  kms_data_key_reuse_period_seconds = 300
+  kms_master_key_id                 = local.sqs_kms_config.kms_master_key_id
+  kms_data_key_reuse_period_seconds = local.sqs_kms_config.kms_data_key_reuse_period_seconds
 
-  tags = {
-    CostCenter = "notification-canada-ca-${var.env}"
-  }
+  tags = local.sqs_common_tags
 }
 
 resource "aws_sqs_queue" "bulk_db_tasks_queue" {
   name                              = "${var.celery_queue_prefix}-${var.sqs_bulk_db_tasks_queue_name}"
-  kms_master_key_id                 = aws_kms_key.notification-canada-ca.arn
-  kms_data_key_reuse_period_seconds = 300
+  kms_master_key_id                 = local.sqs_kms_config.kms_master_key_id
+  kms_data_key_reuse_period_seconds = local.sqs_kms_config.kms_data_key_reuse_period_seconds
 
-  tags = {
-    CostCenter = "notification-canada-ca-${var.env}"
-  }
+  tags = local.sqs_common_tags
 }
 
 resource "aws_sqs_queue" "notify_internal_tasks_queue" {
   name = "${var.celery_queue_prefix}notify-internal-tasks"
   # Updated to use customer-managed KMS key for better security compliance
-  kms_master_key_id                 = aws_kms_key.notification-canada-ca.arn
-  kms_data_key_reuse_period_seconds = 300
+  kms_master_key_id                 = local.sqs_kms_config.kms_master_key_id
+  kms_data_key_reuse_period_seconds = local.sqs_kms_config.kms_data_key_reuse_period_seconds
   # This queue was created outside of terraform and has this value set to default in staging and production.
   visibility_timeout_seconds = var.sqs_visibility_timeout_default
 
-  tags = {
-    CostCenter = "notification-canada-ca-${var.env}"
-  }
+  tags = local.sqs_common_tags
 }
 
 resource "aws_sqs_queue" "eks_notification_canada_ca_sms_high_queue" {
   name                              = "${var.celery_queue_prefix}send-sms-high"
-  kms_master_key_id                 = aws_kms_key.notification-canada-ca.arn
-  kms_data_key_reuse_period_seconds = 300
+  kms_master_key_id                 = local.sqs_kms_config.kms_master_key_id
+  kms_data_key_reuse_period_seconds = local.sqs_kms_config.kms_data_key_reuse_period_seconds
 
   # Visibility timeout is set to 26 seconds for an application side retry set to 25 seconds.
   visibility_timeout_seconds = var.sqs_visibility_timeout_priority_high
 
-  tags = {
-    CostCenter = "notification-canada-ca-${var.env}"
-  }
+  tags = local.sqs_common_tags
 }
 
 resource "aws_sqs_queue" "eks_notification_canada_ca_email_high_queue" {
   name                              = "${var.celery_queue_prefix}send-email-high"
-  kms_master_key_id                 = aws_kms_key.notification-canada-ca.arn
-  kms_data_key_reuse_period_seconds = 300
+  kms_master_key_id                 = local.sqs_kms_config.kms_master_key_id
+  kms_data_key_reuse_period_seconds = local.sqs_kms_config.kms_data_key_reuse_period_seconds
 
   # Visibility timeout is set to 26 seconds for an application side retry set to 25 seconds.
   visibility_timeout_seconds = var.sqs_visibility_timeout_priority_high
 
-  tags = {
-    CostCenter = "notification-canada-ca-${var.env}"
-  }
+  tags = local.sqs_common_tags
 }
 
 resource "aws_sqs_queue" "eks_notification_canada_cadelivery_receipts" {
@@ -78,13 +66,10 @@ resource "aws_sqs_queue" "eks_notification_canada_cadelivery_receipts" {
   message_retention_seconds         = 345600
   receive_wait_time_seconds         = 0
   visibility_timeout_seconds        = var.sqs_visibility_timeout_default
-  kms_master_key_id                 = aws_kms_key.notification-canada-ca.arn
-  kms_data_key_reuse_period_seconds = 300
+  kms_master_key_id                 = local.sqs_kms_config.kms_master_key_id
+  kms_data_key_reuse_period_seconds = local.sqs_kms_config.kms_data_key_reuse_period_seconds
 
-  tags = {
-    Environment = var.env
-    CostCenter  = "notification-canada-ca-${var.env}"
-  }
+  tags = local.sqs_common_tags_with_env
 }
 
 resource "aws_sqs_queue" "ses_receipt_callback_buffer" {
@@ -94,11 +79,8 @@ resource "aws_sqs_queue" "ses_receipt_callback_buffer" {
   message_retention_seconds         = 345600
   receive_wait_time_seconds         = 0
   visibility_timeout_seconds        = var.sqs_visibility_timeout_default
-  kms_master_key_id                 = aws_kms_key.notification-canada-ca.arn
-  kms_data_key_reuse_period_seconds = 300
+  kms_master_key_id                 = local.sqs_kms_config.kms_master_key_id
+  kms_data_key_reuse_period_seconds = local.sqs_kms_config.kms_data_key_reuse_period_seconds
 
-  tags = {
-    Environment = var.env
-    CostCenter  = "notification-canada-ca-${var.env}"
-  }
+  tags = local.sqs_common_tags_with_env
 }
