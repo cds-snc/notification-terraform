50995b01863b36cc8b1321928210e1e4aa0db7ec
Ben Larabie <ben.larabie@blarabie-jtfk.home>
2025-09-29 14:24:20 -0400

    doc download

--- aws/ecr/ecr.tf
+++ aws/ecr/ecr.tf
@@ -59,6 +59,20 @@ resource "aws_ecr_repository" "api-lambda" {
   }
 }
 
+resource "aws_ecr_repository" "document-download" {
+  # The :latest tag is used in Staging
+  #tfsec:ignore:AWS078
+
+  name                 = "notify/document-download"
+  image_tag_mutability = "IMMUTABLE"
+  force_delete         = var.force_delete_ecr
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
+
 resource "aws_ecr_repository" "google-cidr" {
   # The :latest tag is used in Staging
 
