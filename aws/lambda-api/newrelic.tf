35d59254b377957a7bcdd7fbc9e730231ef41d3b
Michael Pond <mikepond11@gmail.com>
2025-10-01 11:38:27 -0400

    removing documentation

--- aws/lambda-api/newrelic.tf
+++ aws/lambda-api/newrelic.tf
@@ -1,38 +1,3 @@
-# New Relic Lambda instrumentation configuration
-# This configures the New Relic Lambda extension for monitoring the API Lambda function
-# 
-# For containerized Lambdas, the extension and agent must be included in the Docker image.
-# See: https://docs.newrelic.com/docs/serverless-function-monitoring/aws-lambda-monitoring/instrument-lambda-function/containerized-images/
-#
-# IMPORTANT: This Terraform configuration only manages environment variables and IAM permissions.
-# You must also update your Lambda's Dockerfile to include the New Relic agent and extension.
-#
-# Example Dockerfile changes needed:
-# 
-#   # Add New Relic layer as a build stage
-#   FROM public.ecr.aws/newrelic-lambda-layers-for-docker/newrelic-lambda-layers-python:312 AS layer
-#   
-#   FROM public.ecr.aws/lambda/python:3.12
-#   
-#   # Copy New Relic extension and agent from layer
-#   COPY --from=layer /opt/ /opt/
-#   
-#   # Your existing application code
-#   COPY app.py requirements.txt ./
-#   RUN python3.12 -m pip install -r requirements.txt -t .
-#   
-#   # Update CMD to use New Relic wrapper
-#   CMD [ "newrelic_lambda_wrapper.handler" ]
-#
-# Available New Relic container layer tags:
-#   - newrelic-lambda-layers-python:38 (Python 3.8)
-#   - newrelic-lambda-layers-python:39 (Python 3.9)
-#   - newrelic-lambda-layers-python:310 (Python 3.10)
-#   - newrelic-lambda-layers-python:311 (Python 3.11)
-#   - newrelic-lambda-layers-python:312 (Python 3.12)
-#
-# More info: https://github.com/newrelic/newrelic-lambda-extension/tree/main/examples/sam/containerized-lambda
-
 locals {
   # Conditional New Relic environment variables
   # Only include these if api_enable_new_relic is true
