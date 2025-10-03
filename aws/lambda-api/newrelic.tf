0d0672a8dd95f7ffcbdf60e057637c84dbff0b8e
Michael Pond <mikepond11@gmail.com>
2025-10-02 22:35:05 -0400

    cleaning out secrets and organizing a few things for lambda api

--- aws/lambda-api/newrelic.tf
+++ aws/lambda-api/newrelic.tf
@@ -1,3 +1,14 @@
+resource "aws_secretsmanager_secret" "new-relic-license-key" {
+  name                    = "NEW_RELIC_LICENSE_KEY"
+  description             = "The New Relic license key, for sending telemetry"
+  recovery_window_in_days = 0
+}
+
+resource "aws_secretsmanager_secret_version" "new-relic-license-key" {
+  secret_id     = aws_secretsmanager_secret.new-relic-license-key.id
+  secret_string = var.new_relic_license_key
+}
+
 locals {
   # New Relic environment variables
   newrelic_env_vars = {
