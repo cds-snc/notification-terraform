c0dddb578e3d3dac2ec6ef36245c4c0dc06935b2
Ben Larabie <ben.larabie@blarabie-jtfk.home>
2025-10-07 09:53:10 -0400

    update

--- aws/github/notification-system-status-frontend-secrets.tf
+++ aws/github/notification-system-status-frontend-secrets.tf
@@ -16,7 +16,7 @@ resource "github_actions_secret" "system_status_bucket" {
   count           = var.env == "production" || var.env == "staging" ? 1 : 0
   repository      = data.github_repository.notification_system_status_frontend.name
   secret_name     = "${upper(var.env)}_SYSTEM_STATUS_BUCKET"
-  plaintext_value = var.system_status_bucket
+  plaintext_value = "notification-canada-ca-${var.env}-system-status"
 }
 
 resource "github_actions_secret" "system_status_cloudfront_distribution" {
@@ -30,5 +30,5 @@ resource "github_actions_secret" "slack_notify_dev_webhook" {
   count           = var.env == "production" ? 1 : 0
   repository      = data.github_repository.notification_system_status_frontend.name
   secret_name     = "SLACK_NOTIFY_DEV_WEBHOOK"
-  plaintext_value = var.slack_notify_dev_webhook
+  plaintext_value = var.notify_dev_slack_webhook
 }
