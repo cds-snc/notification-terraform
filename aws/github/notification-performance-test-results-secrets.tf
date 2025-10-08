304027a22592872370cdb8ce6146e8beb15b9a15
Ben Larabie <ben.larabie@blarabie-jtfk.home>
2025-10-08 14:35:15 -0400

    staging not prod

--- aws/github/notification-performance-test-results-secrets.tf
+++ aws/github/notification-performance-test-results-secrets.tf
@@ -1,19 +1,19 @@
 resource "github_actions_secret" "perf_test_aws_access_key_id" {
-  count           = var.env == "production" ? 1 : 0
+  count           = var.env == "staging" ? 1 : 0
   repository      = data.github_repository.notification_performance_test_results.name
   secret_name     = "${upper(var.env)}_AWS_ACCESS_KEY_ID"
   plaintext_value = var.aws_access_key_id
 }
 
 resource "github_actions_secret" "perf_test_aws_secret_access_key" {
-  count           = var.env == "production" ? 1 : 0
+  count           = var.env == "staging" ? 1 : 0
   repository      = data.github_repository.notification_performance_test_results.name
   secret_name     = "${upper(var.env)}_AWS_SECRET_ACCESS_KEY"
   plaintext_value = var.aws_secret_access_key
 }
 
 resource "github_actions_secret" "perf_test_slack_webhook" {
-  count           = var.env == "production" ? 1 : 0
+  count           = var.env == "staging" ? 1 : 0
   repository      = data.github_repository.notification_performance_test_results.name
   secret_name     = "SLACK_WEBHOOK"
   plaintext_value = var.notify_dev_slack_webhook
