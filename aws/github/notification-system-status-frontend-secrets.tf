d44cd5a6e76e67b09a28e0592b04e0e6daeddba0
Ben Larabie <ben.larabie@blarabie-jtfk.home>
2025-10-08 15:32:05 -0400

    do the right one dingus

--- aws/github/notification-system-status-frontend-secrets.tf
+++ aws/github/notification-system-status-frontend-secrets.tf
@@ -1,34 +1,34 @@
 resource "github_actions_secret" "system_status_static_aws_access_key_id" {
   count           = var.env == "production" || var.env == "staging" ? 1 : 0
-  repository      = "notification-performance-test-results"
+  repository      = data.github_repository.notification_system_status_frontend.name
   secret_name     = "${upper(var.env)}_AWS_ACCESS_KEY_ID"
   plaintext_value = var.aws_access_key_id
 }
 
 resource "github_actions_secret" "system_status_static_aws_secret_access_key" {
   count           = var.env == "production" || var.env == "staging" ? 1 : 0
-  repository      = "notification-performance-test-results"
+  repository      = data.github_repository.notification_system_status_frontend.name
   secret_name     = "${upper(var.env)}_AWS_SECRET_ACCESS_KEY"
   plaintext_value = var.aws_secret_access_key
 }
 
 resource "github_actions_secret" "system_status_bucket" {
   count           = var.env == "production" || var.env == "staging" ? 1 : 0
-  repository      = "notification-performance-test-results"
+  repository      = data.github_repository.notification_system_status_frontend.name
   secret_name     = "${upper(var.env)}_SYSTEM_STATUS_BUCKET"
   plaintext_value = "notification-canada-ca-${var.env}-system-status"
 }
 
 resource "github_actions_secret" "system_status_cloudfront_distribution" {
   count           = var.env == "production" || var.env == "staging" ? 1 : 0
-  repository      = "notification-performance-test-results"
+  repository      = data.github_repository.notification_system_status_frontend.name
   secret_name     = "${upper(var.env)}_SYSTEM_STATUS_CLOUDFRONT_DISTRIBUTION"
   plaintext_value = var.system_status_static_site_cloudfront_distribution
 }
 
 resource "github_actions_secret" "slack_notify_dev_webhook" {
   count           = var.env == "production" ? 1 : 0
-  repository      = "notification-performance-test-results"
+  repository      = data.github_repository.notification_system_status_frontend.name
   secret_name     = "SLACK_NOTIFY_DEV_WEBHOOK"
   plaintext_value = var.notify_dev_slack_webhook
 }
