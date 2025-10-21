e5c17b2795a4159213c2b83cc3339a3212657709
Michael Pond <mikepond11@gmail.com>
2025-10-21 09:25:12 -0400

    adding app name back

--- aws/lambda-api/newrelic.tf
+++ aws/lambda-api/newrelic.tf
@@ -3,6 +3,7 @@ locals {
   newrelic_env_vars = {
     # New Relic account configuration
     NEW_RELIC_ACCOUNT_ID         = var.new_relic_account_id
+    NEW_RELIC_APP_NAME           = var.lambda_new_relic_app_name
     NEW_RELIC_LICENSE_KEY_SECRET = aws_secretsmanager_secret.new-relic-license-key.name
     ENABLE_NEW_RELIC             = var.enable_new_relic
   }
