1a8e91b55e9d574dfb4b7a2fe43ace090a19bb64
Pat Heard <patrick.heard@cds-snc.ca>
2025-10-24 08:26:57 -0400

    feat: setup GuardDuty Malware S3 scanning
    Enable GuardDuty Malware scanning for S3 in preperation for
    switching away from Scan File.  This does not enable object
    tagging yet so there will be no change in how attachments
    are scanned when this is merged.

--- aws/common/file_scanning.tf
+++ aws/common/file_scanning.tf
@@ -7,3 +7,18 @@ module "s3_scan_objects" {
 
   billing_tag_value = var.billing_tag_value
 }
+
+module "guardduty_malware_s3" {
+  source = "github.com/cds-snc/terraform-modules//guardduty_malware_s3?ref=v10.8.2"
+
+  s3_bucket_name = "notification-canada-ca-${var.env}-document-download-scan-files"
+  tagging_status = "DISABLED" # Set to `ENABLED` when ready to switch over from Scan Files
+
+  alarms_enabled                           = true
+  alarm_completed_scan_count_threshold     = 20000
+  alarm_completed_scan_gigabytes_threshold = 2
+  alarm_sns_topic_alarm_arn                = aws_sns_topic.notification-canada-ca-alert-warning.arn
+  alarm_sns_topic_ok_arn                   = aws_sns_topic.notification-canada-ca-alert-ok.arn
+
+  billing_tag_value = var.billing_tag_value
+}
