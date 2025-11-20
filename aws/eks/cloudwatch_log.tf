122b58014e2b6b4f09ea0d06eb209a3a6c7b7828
Ben Larabie <ben.larabie@blarabie-jtfk.home>
2025-11-20 11:56:41 -0500

    missed a name

--- aws/eks/cloudwatch_log.tf
+++ aws/eks/cloudwatch_log.tf
@@ -65,7 +65,7 @@ resource "aws_cloudwatch_log_metric_filter" "celery-error" {
 # We are adding this alarm in to staging since we are seeing some issues with cypress tests causing not found errors in staging
 resource "aws_cloudwatch_log_metric_filter" "celery-not-found-error" {
   count          = var.cloudwatch_enabled && var.env == "staging" ? 1 : 0
-  name           = "celery-error"
+  name           = "celery-not-found-error"
   pattern        = "%notifications not found for SES references%"
   log_group_name = aws_cloudwatch_log_group.notification-canada-ca-eks-application-logs[0].name
 
