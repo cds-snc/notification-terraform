99757e43307553c89a9821ed003d495334bced3f
Ben Larabie <ben.larabie@blarabie-jtfk.home>
2025-11-20 11:52:45 -0500

    fix

--- aws/eks/cloudwatch_log.tf
+++ aws/eks/cloudwatch_log.tf
@@ -66,7 +66,7 @@ resource "aws_cloudwatch_log_metric_filter" "celery-error" {
 resource "aws_cloudwatch_log_metric_filter" "celery-not-found-error" {
   count          = var.cloudwatch_enabled && var.env == "staging" ? 1 : 0
   name           = "celery-error"
-  pattern        = var.env == "staging" ? "%notifications not found for SES references%"
+  pattern        = "%notifications not found for SES references%"
   log_group_name = aws_cloudwatch_log_group.notification-canada-ca-eks-application-logs[0].name
 
   metric_transformation {
