56c911e94d15868d79e403323b0f1770d5740fcc
Mike Pond <32133001+P0NDER0SA@users.noreply.github.com>
2025-12-09 09:34:44 -0500

    Roles, Rights and ECR creations for Dev VPN, K8s and ECR (#2245)
    * adding vpn rights to role to manifests
    
    * needed to add rights for k8s as well
    
    * adding documentation to the private ecrs!
    
    * fixingt doc download repo name
    
    * removing old rights

--- aws/aws-auth/aws-auth.tf
+++ aws/aws-auth/aws-auth.tf
@@ -72,6 +72,11 @@ module "eks" {
       username = "notification-manifests-helmfile-diff"
       groups   = ["system:masters"]
     },
+    {
+      rolearn  = "arn:aws:iam::${var.account_id}:role/notification-manifests-k8s-lambda-apply-main-branch"
+      username = "notification-manifests-k8s-lambda-apply-main-branch"
+      groups   = ["system:masters"]
+    },
   ]
 
   aws_auth_accounts = [
