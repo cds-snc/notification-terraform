cc8fda12ed7ec7b884ba7dd1be850748c503dcd7
Pat Heard <patrick.heard@cds-snc.ca>
2026-01-13 11:46:50 -0500

    chore: disable Scan Files
    Remove Scan Files now that GuardDuty Malware Protection for S3 has
    been enabled and tested.

--- aws/common/file_scanning.tf
+++ aws/common/file_scanning.tf
@@ -1,15 +1,3 @@
-module "s3_scan_objects" {
-  count = var.env == "staging" ? 0 : 1
-
-  source = "github.com/cds-snc/terraform-modules//S3_scan_object?ref=v10.9.1"
-
-  s3_upload_bucket_names  = ["notification-canada-ca-${var.env}-document-download-scan-files"]
-  s3_scan_object_role_arn = "arn:aws:iam::${var.scan_files_account_id}:role/s3-scan-object"
-  scan_files_role_arn     = "arn:aws:iam::${var.scan_files_account_id}:role/scan-files-api"
-
-  billing_tag_value = var.billing_tag_value
-}
-
 module "guardduty_malware_s3" {
   count = var.enable_guardduty_malware_s3 ? 1 : 0
 
@@ -27,47 +15,14 @@ module "guardduty_malware_s3" {
   billing_tag_value = var.billing_tag_value
 }
 
-moved {
-  from = module.s3_scan_objects.aws_iam_policy.scan_files[0]
-  to   = module.s3_scan_objects[0].aws_iam_policy.scan_files[0]
-}
-
-moved {
-  from = module.s3_scan_objects.aws_iam_role.scan_files[0]
-  to   = module.s3_scan_objects[0].aws_iam_role.scan_files[0]
-}
-
-moved {
-  from = module.s3_scan_objects.aws_iam_role_policy_attachment.scan_files[0]
-  to   = module.s3_scan_objects[0].aws_iam_role_policy_attachment.scan_files[0]
-}
-
-moved {
-  from = module.s3_scan_objects.aws_kms_alias.s3_scan_object_queue
-  to   = module.s3_scan_objects[0].aws_kms_alias.s3_scan_object_queue
-}
-
-moved {
-  from = module.s3_scan_objects.aws_kms_key.s3_scan_object_queue
-  to   = module.s3_scan_objects[0].aws_kms_key.s3_scan_object_queue
-}
-
-moved {
-  from = module.s3_scan_objects.aws_s3_bucket_notification.s3_scan_object[0]
-  to   = module.s3_scan_objects[0].aws_s3_bucket_notification.s3_scan_object[0]
-}
-
-moved {
-  from = module.s3_scan_objects.aws_s3_bucket_policy.upload_bucket[0]
-  to   = module.s3_scan_objects[0].aws_s3_bucket_policy.upload_bucket[0]
-}
-
-moved {
-  from = module.s3_scan_objects.aws_sqs_queue.s3_scan_object
-  to   = module.s3_scan_objects[0].aws_sqs_queue.s3_scan_object
-}
-
-moved {
-  from = module.s3_scan_objects.aws_sqs_queue_policy.s3_scan_object
-  to   = module.s3_scan_objects[0].aws_sqs_queue_policy.s3_scan_object
+/*
+ * Prevent the destruction of S3 bucket notifications when the `S3_scan_object` module
+ * is removed.  This ensures that the GuardDuty Malware S3 EventBridge notifications
+ * continue to be sent.
+ */
+removed {
+  from = module.s3_scan_objects[0].aws_s3_bucket_notification.s3_scan_object[0]
+  lifecycle {
+    destroy = false
+  }
 }
