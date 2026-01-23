f91cb05f79c5b0a0ab722689aa3deb64bbd0dcd9
Ben Larabie <ben.larabie@blarabie-jtfk.home>
2026-01-23 13:17:31 -0500

    formatting

--- aws/eks/eks_auto.tf
+++ aws/eks/eks_auto.tf
@@ -1,6 +1,7 @@
 resource "aws_eks_cluster" "eks_auto" {
+  count    = var.env == "dev" ? 1 : 0
   name     = "notify-${var.env}-eks"
-  role_arn = aws_iam_role.cluster.arn
+  role_arn = aws_iam_role.cluster[0].arn
   version  = var.eks_cluster_version
 
   vpc_config {
@@ -26,7 +27,7 @@ resource "aws_eks_cluster" "eks_auto" {
   compute_config {
     enabled       = true
     node_pools    = ["general-purpose", "system"]
-    node_role_arn = aws_iam_role.node.arn
+    node_role_arn = aws_iam_role.node[0].arn
   }
 
   kubernetes_network_config {
@@ -43,13 +44,15 @@ resource "aws_eks_cluster" "eks_auto" {
 }
 
 resource "aws_iam_role" "cluster" {
-  name = "eks-test-cluster-role"
+  count = var.env == "dev" ? 1 : 0
+  name  = "eks-test-cluster-role"
 
   assume_role_policy = data.aws_iam_policy_document.cluster_role_assume_role_policy.json
 }
 
 resource "aws_iam_role_policy_attachments_exclusive" "cluster" {
-  role_name = aws_iam_role.cluster.name
+  count     = var.env == "dev" ? 1 : 0
+  role_name = aws_iam_role.cluster[0].name
   policy_arns = [
     "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy",
     "arn:aws:iam::aws:policy/AmazonEKSComputePolicy",
@@ -73,7 +76,8 @@ data "aws_iam_policy_document" "cluster_role_assume_role_policy" {
 }
 
 resource "aws_iam_role" "node" {
-  name = "eks-auto-node"
+  count = var.env == "dev" ? 1 : 0
+  name  = "eks-auto-node"
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
@@ -89,24 +93,28 @@ resource "aws_iam_role" "node" {
 }
 
 resource "aws_iam_role_policy_attachment" "node_AmazonEKSWorkerNodeMinimalPolicy" {
+  count      = var.env == "dev" ? 1 : 0
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodeMinimalPolicy"
-  role       = aws_iam_role.node.name
+  role       = aws_iam_role.node[0].name
 }
 
 resource "aws_iam_role_policy_attachment" "node_AmazonEC2ContainerRegistryPullOnly" {
+  count      = var.env == "dev" ? 1 : 0
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryPullOnly"
-  role       = aws_iam_role.node.name
+  role       = aws_iam_role.node[0].name
 }
 
 # Grant cluster admin access to the SSO admin role
 resource "aws_eks_access_entry" "admin_sso" {
-  cluster_name  = aws_eks_cluster.eks_auto.name
+  count         = var.env == "dev" ? 1 : 0
+  cluster_name  = aws_eks_cluster.eks_auto[0].name
   principal_arn = "arn:aws:iam::${var.account_id}:role/aws-reserved/sso.amazonaws.com/ca-central-1/AWSReservedSSO_AWSAdministratorAccess_e6e62a284c3c35fc"
   type          = "STANDARD"
 }
 
 resource "aws_eks_access_policy_association" "admin_sso_policy" {
-  cluster_name  = aws_eks_cluster.eks_auto.name
+  count         = var.env == "dev" ? 1 : 0
+  cluster_name  = aws_eks_cluster.eks_auto[0].name
   principal_arn = "arn:aws:iam::${var.account_id}:role/aws-reserved/sso.amazonaws.com/ca-central-1/AWSReservedSSO_AWSAdministratorAccess_e6e62a284c3c35fc"
   policy_arn    = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
 
@@ -119,38 +127,42 @@ resource "aws_eks_access_policy_association" "admin_sso_policy" {
 
 
 data "aws_security_group" "eks_auto" {
-  id = aws_eks_cluster.eks_auto.vpc_config[0].cluster_security_group_id
+  count = var.env == "dev" ? 1 : 0
+  id    = aws_eks_cluster.eks_auto[0].vpc_config[0].cluster_security_group_id
 }
 
 # eks-securitygroup
 
 resource "aws_security_group_rule" "eks_auto_private_endpoint_egress" {
+  count                    = var.env == "dev" ? 1 : 0
   description              = "Internal egress to VPC PrivateLink endpoints from eks securitygroup"
   type                     = "egress"
   from_port                = 443
   to_port                  = 443
   protocol                 = "tcp"
   source_security_group_id = var.private-links-vpc-endpoints-securitygroup
-  security_group_id        = data.aws_security_group.eks_auto.id
+  security_group_id        = data.aws_security_group.eks_auto[0].id
 }
 
 resource "aws_security_group_rule" "private_endpoints_ingress_eks_auto" {
+  count                    = var.env == "dev" ? 1 : 0
   description              = "VPC PrivateLink endpoints ingress from eks securitygroup"
   type                     = "ingress"
   from_port                = 443
   to_port                  = 443
   protocol                 = "tcp"
-  source_security_group_id = data.aws_security_group.eks_auto.id
+  source_security_group_id = data.aws_security_group.eks_auto[0].id
   security_group_id        = var.private-links-vpc-endpoints-securitygroup
 }
 
 resource "aws_security_group_rule" "eks_auto_egress_endpoints_gateway" {
+  count             = var.env == "dev" ? 1 : 0
   description       = "Security group rule for eks securitygroup to S3 gateway"
   type              = "egress"
   from_port         = 443
   to_port           = 443
   protocol          = "tcp"
-  security_group_id = data.aws_security_group.eks_auto.id
+  security_group_id = data.aws_security_group.eks_auto[0].id
   prefix_list_ids   = var.private-links-gateway-prefix-list-ids
 
 }
