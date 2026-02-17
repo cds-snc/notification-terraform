6b0a49c910949dddf55eda0f19bd91547edc9a83
Pat Heard <patrick.heard@cds-snc.ca>
2026-02-17 21:03:17 +0000

    fix: indent for easier maintenance

--- env/variables.tf
+++ env/variables.tf
@@ -1152,11 +1152,11 @@ variable "security_txt_content" {
   type        = string
   description = "The content of the security.txt file"
   default     = <<-EOT
-  Contact: mailto:ZZTBSCYBERS@tbs-sct.gc.ca
-  Contact: https://hackerone.com/tbs-sct/
-  Policy: https://hackerone.com/tbs-sct/policy
-  Canonical: https://cdssandbox.xyz/.well-known/security.txt
-  Preferred-Languages: en, fr
-  Expires: 2026-03-02T12:00:00.000Z
+    Contact: mailto:ZZTBSCYBERS@tbs-sct.gc.ca
+    Contact: https://hackerone.com/tbs-sct/
+    Policy: https://hackerone.com/tbs-sct/policy
+    Canonical: https://cdssandbox.xyz/.well-known/security.txt
+    Preferred-Languages: en, fr
+    Expires: 2026-03-02T12:00:00.000Z
   EOT
 }
