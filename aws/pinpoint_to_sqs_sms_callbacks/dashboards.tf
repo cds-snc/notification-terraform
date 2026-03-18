f5146f4fc96a2f98c5825bc2f8803e4da418ddf8
cds-stephenchow <stephen.chow@cds-snc.ca>
2026-03-18 10:00:33 -0600

    update pinpoint dashboard to include us-west-2 metrics (#2426)
    * update pinpoint dashboard to include us-west-2 metrics

--- aws/pinpoint_to_sqs_sms_callbacks/dashboards.tf
+++ aws/pinpoint_to_sqs_sms_callbacks/dashboards.tf
@@ -214,7 +214,7 @@ resource "aws_cloudwatch_dashboard" "pinpoint" {
             "x": 18,
             "type": "text",
             "properties": {
-                "markdown": "\n## Limits\n- [Spending limit](https://${var.region}.console.aws.amazon.com/sns/v3/home?region=${var.region}#/mobile/text-messaging) of 30,000 USD/month\n\n## Message flow\nAfter a notification has been created in the database, Celery sends the SMS to the provider using the `deliver_sms` Celery task. This Celery task is assigned to the SQS queue [${var.celery_queue_prefix}send-sms-low](#/queues/https%3A%2F%2Fsqs.${var.region}.amazonaws.com%2F${var.account_id}%2F${var.celery_queue_prefix}send-sms-low), [${var.celery_queue_prefix}send-sms-medium](#/queues/https%3A%2F%2Fsqs.${var.region}.amazonaws.com%2F${var.account_id}%2F${var.celery_queue_prefix}send-sms-medium), or [${var.celery_queue_prefix}send-sms-high](#/queues/https%3A%2F%2Fsqs.${var.region}.amazonaws.com%2F${var.account_id}%2F${var.celery_queue_prefix}send-sms-high) depending on the SMS priority. This task calls the SNS pr Pinpoint API to send a text message.\n\n## SMS IDs\nAWS keeps track of SMS with a `messageId`, the value of SNS' `messageId` is stored in the `Notification` object in the `reference` column.\n\n## Logging\nCelery tasks output multiple messages when processing tasks/calling the SNS/Pinpoint API, take a look at the relevant Celery code to know more.\n\nAfter an SMS has been sent by SNS, the delivery details are stored in CloudWatch Log groups:\n\n- [sns/${var.region}/${var.account_id}/DirectPublishToPhoneNumber](#logsV2:log-groups/log-group/sns$252F${var.region}$252F${var.account_id}$252FDirectPublishToPhoneNumber) for successful deliveries\n- [sns/${var.region}/${var.account_id}/DirectPublishToPhoneNumber/Failure](#logsV2:log-groups/log-group/sns$252F${var.region}$252F${var.account_id}$252FDirectPublishToPhoneNumber$252FFailure) for failures\n\n## Phone numbers\n\nSMS sent in `${var.region}` use phone numbers reserved for Notify's use\n\n### ⚠️  SNS in `us-west-2`\nIf a Notify service has an inbound number attached, SMS will be sent with SNS using a long code phone number ordered on Pinpoint in the `us-west-2` region. Statistics for this region and alarms are **not visible on this dashboard**.\n"
+                "markdown": "\n## Limits\n- [Spending limit](https://${var.region}.console.aws.amazon.com/sns/v3/home?region=${var.region}#/mobile/text-messaging) of 30,000 USD/month\n\n## Message flow\nAfter a notification has been created in the database, Celery sends the SMS to the provider using the `deliver_sms` Celery task. This Celery task is assigned to the SQS queue [${var.celery_queue_prefix}send-sms-low](#/queues/https%3A%2F%2Fsqs.${var.region}.amazonaws.com%2F${var.account_id}%2F${var.celery_queue_prefix}send-sms-low), [${var.celery_queue_prefix}send-sms-medium](#/queues/https%3A%2F%2Fsqs.${var.region}.amazonaws.com%2F${var.account_id}%2F${var.celery_queue_prefix}send-sms-medium), or [${var.celery_queue_prefix}send-sms-high](#/queues/https%3A%2F%2Fsqs.${var.region}.amazonaws.com%2F${var.account_id}%2F${var.celery_queue_prefix}send-sms-high) depending on the SMS priority. This task calls the SNS or Pinpoint API to send a text message.\n\n## SMS IDs\nAWS keeps track of SMS with a `messageId`, the value of SNS' `messageId` is stored in the `Notification` object in the `reference` column.\n\n## Logging\nCelery tasks output multiple messages when processing tasks/calling the SNS/Pinpoint API, take a look at the relevant Celery code to know more.\n\nAfter an SMS has been sent by SNS, the delivery details are stored in CloudWatch Log groups:\n\n- [sns/${var.region}/${var.account_id}/DirectPublishToPhoneNumber](#logsV2:log-groups/log-group/sns$252F${var.region}$252F${var.account_id}$252FDirectPublishToPhoneNumber) for successful deliveries\n- [sns/${var.region}/${var.account_id}/DirectPublishToPhoneNumber/Failure](#logsV2:log-groups/log-group/sns$252F${var.region}$252F${var.account_id}$252FDirectPublishToPhoneNumber$252FFailure) for failures\n\n## Phone numbers\n\nSMS sent in `${var.region}` use phone numbers reserved for Notify's use\n"
             }
         },
         {
@@ -539,6 +539,77 @@ resource "aws_cloudwatch_dashboard" "pinpoint" {
                 "stat": "Sum",
                 "period": 86400,
                 "title": "Carrier failures over the past day"
+            }
+        },
+        {
+            "type": "alarm",
+            "x": 0,
+            "y": 3,
+            "width": 24,
+            "height": 3,
+            "properties": {
+                "title": "Alarms - us-west-2",
+                "alarms": [
+                    "${aws_cloudwatch_metric_alarm.pinpoint-sms-success-rate-critical-us-west-2[0].arn}",
+                    "${aws_cloudwatch_metric_alarm.pinpoint-sms-success-rate-warning-us-west-2[0].arn}",
+                    "${aws_cloudwatch_metric_alarm.total-sms-spending-critical-us-west-2[0].arn}",
+                    "${aws_cloudwatch_metric_alarm.total-sms-spending-warning-us-west-2[0].arn}"
+                ]
+            }
+        },
+        {
+            "type": "metric",
+            "x": 9,
+            "y": 8,
+            "width": 9,
+            "height": 6,
+            "properties": {
+                "metrics": [
+                    [ "LogMetrics", "pinpoint-sms-successes" ]
+                ],
+                "view": "timeSeries",
+                "stacked": true,
+                "region": "${var.region_pinpoint_us}",
+                "stat": "Sum",
+                "period": 300,
+                "title": "SMS delivered per 5m - us-west-2"
+            }
+        },
+        {
+            "type": "metric",
+            "x": 9,
+            "y": 14,
+            "width": 9,
+            "height": 6,
+            "properties": {
+                "metrics": [
+                    [ "LogMetrics", "pinpoint-sms-failures" ]
+                ],
+                "view": "timeSeries",
+                "stacked": true,
+                "region": "${var.region_pinpoint_us}",
+                "stat": "Sum",
+                "period": 300,
+                "title": "SMS failures per 5m - us-west-2"
+            }
+        },
+        {
+            "type": "metric",
+            "x": 9,
+            "y": 48,
+            "width": 9,
+            "height": 6,
+            "properties": {
+                "metrics": [
+                    [ "AWS/Lambda", "Invocations", "FunctionName", "pinpoint_to_sqs_sms_callbacks_us_west_2" ],
+                    [ ".", "Errors", ".", ".", { "color": "#d62728", "yAxis": "right" } ]
+                ],
+                "view": "timeSeries",
+                "stacked": true,
+                "region": "${var.region_pinpoint_us}",
+                "stat": "Sum",
+                "period": 300,
+                "title": "Pinpoint Callback Lambda invocations per 5m - us-west-2"
             }
         }
     ]
