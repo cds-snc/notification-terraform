66ae2ecd775f293045897a76d7b76d56f39166c3
Andrew Leith <andrew.leith@gmail.com>
2026-06-01 15:54:19 -0300

    feat(aws/common/eks): Add CloudWatch query for tracking timed-out notifications in Celery

--- aws/eks/cloudwatch_queries.tf
+++ aws/eks/cloudwatch_queries.tf
@@ -477,3 +477,22 @@ fields @timestamp, @notification_id, @url, @error
 | limit 10000
 QUERY
 }
+
+resource "aws_cloudwatch_query_definition" "celery-notifications-timeouts" {
+  count = var.cloudwatch_enabled ? 1 : 0
+  name  = "Celery / Notifications timeout"
+
+  log_group_names = [
+    local.eks_application_log_group
+  ]
+
+  query_string = <<QUERY
+fields @timestamp, log, kubernetes.container_name as app, kubernetes.pod_name as pod_name, @logStream
+| filter kubernetes.container_name like /^${local.celery_name}/
+| filter @message like /Timeout period reached for/
+| parse @message "*stderr F [*,*: INFO/ForkPoolWorker-*] Timeout period reached for * notifications*" as @ignore, @date, @time_ms, @worker_id, @notification_count
+| sort @timestamp desc
+| display @date, @notification_count
+| limit 200
+QUERY
+}
