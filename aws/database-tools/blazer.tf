0064dc1b93391ec408ad6460ba8f9ad9bd533d3f
Jumana B <jzbahrai@users.noreply.github.com>
2026-09-14 09:58:37 -0400

    Add checks for blazer (#2755)
    * Add checks for blazer
    
    * change the daily scheduler
    
    * add a user so we can restrict checks to certain tables
    
    * add vars
    
    * made fixes
    
    * fixes from review
    
    * fix eventbridge
    
    * blazer checks
    
    * remove unused var
    
    * vars
    
    * renamed the user
    
    * renamed the var
    
    * renamed the var
    
    * fixing security gap
    
    ---------
    
    Co-authored-by: Ben Larabie <ben.larabie@cds-snc.ca>

--- aws/database-tools/blazer.tf
+++ aws/database-tools/blazer.tf
@@ -68,8 +68,14 @@ resource "aws_ecs_task_definition" "blazer" {
         "name" : "BLAZER_DATABASE_URL",
         "valueFrom" : "${aws_ssm_parameter.sqlalchemy_database_reader_uri.arn}"
         }, {
+        "name" : "BLAZER_CHECKS_DATABASE_URL",
+        "valueFrom" : "${aws_ssm_parameter.blazer_checks_database_url.arn}"
+        }, {
         "name" : "DATABASE_URL",
         "valueFrom" : "${aws_ssm_parameter.db_tools_environment_variables.arn}"
+        }, {
+        "name" : "BLAZER_SLACK_WEBHOOK_URL",
+        "valueFrom" : "${aws_ssm_parameter.blazer_slack_webhook_general_topic.arn}"
         }, {
         "name" : "GOOGLE_OAUTH_CLIENT_ID",
         "valueFrom" : "${aws_ssm_parameter.notify_o11y_google_oauth_client_id.arn}"
